e2c50c39027f41341403bcb5d07eeb9e192aba87
Ulzii Otgonbaatar <ulzii@returntocorp.com>
2020-01-29 16:12:24 -0800

    finalize the rule

--- tests/python/flask/send_static_file/test_send_file_without_path_sanitization.py
+++ tests/python/flask/send_static_file/test_send_file_without_path_sanitization.py
@@ -2,5 +2,6 @@ from flask import send_file
 
 app = Flask(__name__)
 
-def download_file():
-  return send_file()
+@app.route("/<path:filename>")
+def download_file(filename):
+  return send_file(filename)
