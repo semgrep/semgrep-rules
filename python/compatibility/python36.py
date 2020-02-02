20778bb421e1bf410a724d6c640c2c94be3d512a
Isaac Evans <ine@mit.edu>
2020-02-02 10:10:30 -0500

    fix python36 test

--- python/compatibility/python36.py
+++ python/compatibility/python36.py
@@ -8,3 +8,4 @@ def main():
     # ruleid:python36-compatibility-Popen2
     subprocess.Popen(cmd, encoding="utf-8")
     # ruleid:python36-compatibility-Popen1
+    subprocess.Popen(cmd, errors=None)
