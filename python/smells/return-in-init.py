c2e4ee64af9e6268eee1fda2569aedce9da2e551
Isaac Evans <409041+ievans@users.noreply.github.com>
2020-02-04 08:24:08 -0800

    Revert "Disable bad rules with return pattern"

--- python/smells/return-in-init.py
+++ python/smells/return-in-init.py
@@ -1,18 +1,18 @@
 class A:
 
     def __init__(a, b, c):
-        # todoruleid:return-in-init
+        # ruleid:return-in-init
         return A(a, b, c)
 
 
 class B:
 
     def __init__(a, b, c):
-        # todoruleid:yield-in-init
+        # ruleid:yield-in-init
         yield
 
 class C:
 
     def __init__():
-        # todoruleid:yield-in-init
+        # ruleid:yield-in-init
         yield 5
