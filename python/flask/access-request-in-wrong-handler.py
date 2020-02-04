eebcca4963839c0b3e3c949f3457b79cff991cb1
Ulzii Otgonbaatar <ulzii@returntocorp.com>
2020-02-04 09:40:23 -0800

    simply rule with pattern-inside

--- python/flask/access-request-in-wrong-handler.py
+++ python/flask/access-request-in-wrong-handler.py
@@ -3,38 +3,20 @@ from flask import request
 app = Flask(__name__)
 
 
-# ruleid:avoid-accessing-request-in-wrong-handler
 @app.route('/', method="GET")
 def handler_with_get_json(ff):
+  # ruleid:avoid-accessing-request-in-wrong-handler
   r = request.json
-  return
+  return r
 
-# ruleid:avoid-accessing-request-in-wrong-handler
 @app.route('/', method="GET")
 def handler_with_get_form(ff):
+  # ruleid:avoid-accessing-request-in-wrong-handler
   r = request.form
-  return
+  return r
 
-# ruleid:avoid-accessing-request-in-wrong-handler
 @app.route('/', method="GET")
 def handler_with_data(ff):
+  # ruleid:avoid-accessing-request-in-wrong-handler
   r = request.data
-  return
-
-# ruleid:avoid-accessing-request-in-wrong-handler
-@app.route('/', methods=["GET", "POST"])
-def handler_with_mupltiple_methods_json(ff):
-  r = request.json
-  return
-
-# ruleid:avoid-accessing-request-in-wrong-handler
-@app.route('/', methods=["GET", "POST"])
-def handler_with_mupltiple_methods_form(ff):
-  r = request.form
-  return
-
-# ruleid:avoid-accessing-request-in-wrong-handler
-@app.route('/', methods=["GET", "POST"])
-def handler_with_mupltiple_methods_data(ff):
-  r = request.data
-  return
+  return r
