aed5bdd08a4f82393937c97ccf8e040bf881742f
Ulzii Otgonbaatar <ulzii@returntocorp.com>
2020-02-03 18:52:53 -0800

    Check for Flask request access outside in GET requests

---
+++ python/flask/access-request-in-wrong-handler.py
@@ -0,0 +1,39 @@
+from flask import request
+
+app = Flask(__name__)
+
+# ruleid:avoid-accessing-request-in-wrong-handler
+@app.route('/', method="GET")
+def handler_with_get_json(ff):
+  r = request.json
+  return
+
+# ruleid:avoid-accessing-request-in-wrong-handler
+@app.route('/', method="GET")
+def handler_with_get_form(ff):
+  r = request.form
+  return
+
+# ruleid:avoid-accessing-request-in-wrong-handler
+@app.route('/', method="GET")
+def handler_with_data(ff):
+  r = request.data
+  return
+
+# ruleid:avoid-accessing-request-in-wrong-handler
+@app.route('/', methods=["GET", "POST"])
+def handler_with_mupltiple_methods_json(ff):
+  r = request.json
+  return
+
+# ruleid:avoid-accessing-request-in-wrong-handler
+@app.route('/', methods=["GET", "POST"])
+def handler_with_mupltiple_methods_form(ff):
+  r = request.form
+  return
+
+# ruleid:avoid-accessing-request-in-wrong-handler
+@app.route('/', methods=["GET", "POST"])
+def handler_with_mupltiple_methods_data(ff):
+  r = request.data
+  return
