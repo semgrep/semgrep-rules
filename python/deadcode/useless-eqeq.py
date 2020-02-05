a037a423b374859de0466e288b13b06d8937a005
Isaac Evans <409041+ievans@users.noreply.github.com>
2020-02-05 14:07:54 -0800

    enable more tests (#43)
    * enable more tests

--- python/deadcode/useless-eqeq.py
+++ python/deadcode/useless-eqeq.py
@@ -9,11 +9,10 @@ def __eq__(self, other):
 def sure(ofcourse):
     return 1 == 1
 
-# TODO
-#class A:
-#    def __eq__(self, other):
-#        # OK; skip most things that are inside eqs based on what we saw on platform 
-#        return self == self and self == other
+class A:
+    def __eq__(self, other):
+        # OK; skip most things that are inside eqs based on what we saw on platform 
+        return self == self and self == other
 
 
 assertTrue(x ==x)
