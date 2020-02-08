8336fcc1654c80ddcb93eacb3a03310af20c4bb2
Isaac Evans <ine@mit.edu>
2020-02-07 19:08:22 -0800

    correct a few false positives

--- python/deadcode/return.py
+++ python/deadcode/return.py
@@ -17,3 +17,27 @@ def alwaysblue():
     # todoruleid: code-after-unconditional-return
     return 'red'
     x = 5
+
+
+def resolve(key: str):
+    key = os.path.join(path, "keys", key)
+    # ok
+    return key
+
+
+def resolve(key: str) -> str:
+    key = os.path.join(path, "keys", key)
+    # ok
+    return key
+
+# ruleid: return-not-in-function
+return (a, b)
+
+# ruleid: return-not-in-function
+return (a, b)
+
+
+def resolve(key: str) -> str:
+    key = os.path.join(path, "keys", key)
+    # ok
+    return key, key
