74ca7909ef8eb4dfac02a56e7a8fde30c48c897e
Isaac Evans <409041+ievans@users.noreply.github.com>
2020-02-10 13:44:20 -0800

    add java tests (#53)
    * add java tests

--- java/basic-rules.java
+++ java/basic-rules.java
@@ -2,7 +2,35 @@ class Bar {
     void main() {
         boolean myBoolean; 
 
+        //myBoolean == myBoolean;
+
+        // ruleid:assignment-comparison
         if (myBoolean = true) {
+            continue;
+        }
+
+        // ruleid:hardcoded-eq-true-or-false
+        if (true) { 
+            continue;
+        }
+
+        // todoruleid:hardcoded-eq-true-or-false
+        if (true && false) { 
+            continue;
+        }
+
+        // ok
+        if (myBoolean) {
+
+        }
+
+        // ruleid:eqeq-is-bad
+        if (myBoolean == myBoolean) { 
+            continue;
+        }
+
+        // ruleid:eqeq-is-bad
+        if (myBoolean != myBoolean) { 
             continue;
         }
 
