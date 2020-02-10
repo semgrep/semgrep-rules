884487b19975cb4a0bc63e2b479f8502a487a363
Isaac Evans <409041+ievans@users.noreply.github.com>
2020-02-10 13:44:10 -0800

    more go tests (#54)
    * more go tests

--- go/deadcode/useless-eqeq.go
+++ go/deadcode/useless-eqeq.go
@@ -6,4 +6,11 @@ func main() {
     var y = 1;
     // ruleid:eqeq-is-bad
     fmt.Println(y == y)
+    // ok
+    assert(y == y)
+
+    // ruleid:hardcoded-eq-true-or-false
+    if (false) {
+        fmt.Println("never") 
+    }
 }
