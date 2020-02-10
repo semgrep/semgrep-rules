784a4a0e059067956bff0316634df8b703fb1b4b
Isaac Evans <409041+ievans@users.noreply.github.com>
2020-02-10 13:42:19 -0800

    tweak false positives found in python rules (#57)
    * correct a few false positives
    * pass in init is ok

--- python/deadcode/return.py
+++ python/deadcode/return.py
@@ -15,5 +15,22 @@ def alwaysblue():
     return 'red'
     x = 5
 
+
+def resolve(key: str):
+    key = os.path.join(path, "keys", key)
+    # ok
+    return key
+
+
+def resolve(key: str) -> str:
+    key = os.path.join(path, "keys", key)
+    # ok
+    return key
+
+def resolve(key: str) -> str:
+    key = os.path.join(path, "keys", key)
+    # ok
+    return key, key
+
 # ruleid: return-not-in-function
-return 5
+return (a, b)
