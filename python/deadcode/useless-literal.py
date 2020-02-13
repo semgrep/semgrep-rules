3522e7adfa45ebbc8898080c60c07444fe04a8b2
Isaac Evans <409041+ievans@users.noreply.github.com>
2020-02-13 09:36:07 -0800

    fix parse error in one python test (#65)

--- python/deadcode/useless-literal.py
+++ python/deadcode/useless-literal.py
@@ -1,7 +1,7 @@
 # todoruleid: useless-literal-dict
-d = dict(1: 'a', 2: 'b', 1: 'a')
+d = dict((1, 'a'), (2, 'b'), (1, 'a'))
 # todoruleid: useless-literal-set
-d = set(1: 'a', 2: 'b', 1: 'a')
+s = set((1, 'a'), (2, 'b'), (1, 'a'))
 
 # todoruleid: useless-literal-dict
 d = {1: 'a', 2: 'b', 1: 'a'}
