6a772653f1df56cb4147a873fcc322900179f8a5
Isaac Evans <ine@mit.edu>
2020-02-14 11:31:12 -0800

    add useless comparison check

---
+++ python/deadcode/useless-comparison.py
@@ -0,0 +1,21 @@
+
+
+# ruleid:useless-comparison
+if "detached HEAD" or "master" in expected:
+  pass
+
+# todoruleid:useless-comparison
+if ("detached HEAD" or "master" in expected):
+  pass
+
+# todoruleid:useless-comparison
+if ("detached HEAD" and ("master" in expected)):
+  pass
+
+# OK
+if ("detached HEAD" in expected) and ("master" in expected):
+  pass
+
+# ruleid:useless-comparison
+if "":
+  pass
