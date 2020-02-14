570019ee7fc0d6ca58f3d2ff0ff96ef7cc431514
Isaac Evans <ine@mit.edu>
2020-02-13 16:32:00 -0800

    add tests

---
+++ python/wtf-python/is-comparison-string.py
@@ -0,0 +1,21 @@
+x = 'foo'
+
+# ruleid:identical-is-comparison
+if x is x:
+  print('true')
+
+if x is None: # ok
+  pass
+
+if (type(X) is str):
+  pass # OK
+
+if x is True:
+  pass # OK
+
+if x is False:
+  pass # OK
+
+# ruleid: string-is-comparison
+if x is 'hello there':
+  pass
