bdd8f21194ca79fc10d05458e7659f2f30e23df2
Drew Dennison <drew@returntocorp.com>
2020-02-19 18:51:20 -0800

    pre-commit

--- python/smells/list-modify-iterating.py
+++ python/smells/list-modify-iterating.py
@@ -1,9 +1,9 @@
 l  = list(range(100))
-for i in l:                         
+for i in l:
     print(i),
-    # ruleid:list-pop-while-iterate                        
+    # ruleid:list-pop-while-iterate
     print(l.pop(0))
-    # ruleid:list-pop-while-iterate                 
+    # ruleid:list-pop-while-iterate
     x = l.pop(0)
     print(x)
 
