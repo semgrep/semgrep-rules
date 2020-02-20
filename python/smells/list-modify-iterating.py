7f143216cdf3a2794e7655347bea18b27bce67d8
Drew Dennison <drew@returntocorp.com>
2020-02-19 18:56:10 -0800

    update test comments

--- python/smells/list-modify-iterating.py
+++ python/smells/list-modify-iterating.py
@@ -1,19 +1,18 @@
 l  = list(range(100))
+# ruleid:list-pop-while-iterate
 for i in l:
     print(i),
-    # ruleid:list-pop-while-iterate
     print(l.pop(0))
-    # ruleid:list-pop-while-iterate
     x = l.pop(0)
     print(x)
 
 a = [1, 2, 3, 4]
+# ruleid:list-pop-while-iterate
 for i in a:
     print(i)
-    # ruleid:list-pop-while-iterate
     a.pop(0)
 
+# ruleid:list-pop-while-iterate
 for i in a:
     print(i)
-    # ruleid:list-pop-while-iterate
     a.append(0)
