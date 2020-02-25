d0376d620951ccc941ba459d35e298e39b056ae7
Drew Dennison <drew@returntocorp.com>
2020-02-25 10:09:44 -0800

    django: add model save missing super call rule
    Fixes #109

---
+++ python/django/db/model-save.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.template.defaultfilters import slugify
+
+# ok
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True, max_length=255)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super(Post, self).save(*args, **kwargs)
+
+# ruleid: django-db-model-save-super
+class Bad(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True, max_length=255)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
