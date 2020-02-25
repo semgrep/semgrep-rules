65413772e2e7802705da6b64ea02b73d9f3e354d
grayson <grayson@returntocorp.com>
2020-02-25 13:39:43 -0800

    [wip] Flask secure cookies checks. One for session cookies and one for response.set_cookie

---
+++ python/flask/session-secure-cookies-not-set.py
@@ -0,0 +1,6 @@
+import flask
+
+app = flask.Flask()
+
+if __name__ == "__main__":
+    app.run()
---
+++ python/flask/session-secure-cookies.py
@@ -0,0 +1,12 @@
+import flask
+
+app = flask.Flask()
+
+app.config.update(
+    SESSION_COOKIE_SECURE=True,
+    SESSION_COOKIE_HTTPONLY=True,
+    SESSION_COOKIE_SAMESITE='Lax',
+)
+
+if __name__ == "__main__":
+    app.run()
