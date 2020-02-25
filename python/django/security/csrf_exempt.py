3a213f6e8f6fdb004a20633ebca0c0a3fb228bd7
Drew Dennison <drew@returntocorp.com>
2020-02-25 15:34:47 -0800

    support fully qualified decorator

--- python/django/security/csrf_exempt.py
+++ python/django/security/csrf_exempt.py
@@ -5,3 +5,10 @@ from django.views.decorators.csrf import csrf_exempt
 @csrf_exempt
 def my_view(request):
     return HttpResponse('Hello world')
+
+import django
+
+# ruleid: django-csrf_exempt
+@django.views.decorators.csrf.csrf_exempt
+def my_view2(request):
+    return HttpResponse('Hello world')
