7fbb47cf10d5e39a4821a55bb7e0955f4e749ecf
grayson <grayson@returntocorp.com>
2020-02-28 11:08:47 -0800

    Updated flask/open-redirect to include ... in redirect calls

--- python/flask/open-redirect/open-redirect.py
+++ python/flask/open-redirect/open-redirect.py
@@ -6,5 +6,12 @@ app = Flask(__name__)
 def open_redirect():
     # ruleid: open-redirect
     url = request.args.get("url")
+    print("something")
+    return redirect(url)
+
+@app.route("not_open_redirect/")
+def not_open_redirect():
+    # ok
+    url = "/about/"
     return redirect(url)
 
