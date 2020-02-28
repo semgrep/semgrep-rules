6eca47860565352528b2e218c833740717745359
grayson <grayson@returntocorp.com>
2020-02-28 11:22:11 -0800

    Fixed a bug where I had forgotten to remove flask. from request. Added in cases for HttpResponseRedirect. Added test for HttpResponseRedirect

--- python/django/open-redirect/open-redirect.py
+++ python/django/open-redirect/open-redirect.py
@@ -1,12 +1,20 @@
 from django.shortcuts import redirect
+from django.http import HttpResponseRedirect
 
-def my_view(request):
+def unsafe(request):
     # ruleid: open-redirect
     url = request.headers.get('referrer')
     print("something")
     return redirect(url)
 
-def my_view(request):
+def safe(request):
     # ok
     url = "https://lmnop.qrs"
     return redirect(url)
+
+def unsafe2(request):
+    # ruleid: open-redirect
+    url = request.POST.get("url")
+    return HttpResponseRedirect(url)
+
+
