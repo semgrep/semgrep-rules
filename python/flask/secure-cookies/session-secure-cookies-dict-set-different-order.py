a4a3033b56c2b3983a61edef0aee494590a10d14
minusworld <grayson@returntocorp.com>
2020-03-02 16:05:05 -0800

    Flask secure cookies checks (#195)
    * [wip] Flask secure cookies checks. One for session cookies and one for response.set_cookie
    
    * [wip] Secure session cookie for flask check. Currently fires too many times.
    
    * Adding Flask secure cookie checks. Introduces two checks: 1) a check for setting secure session cookies in the application config, and 2) a check for setting secure user cookies (different from session cookies) in response.set_cookie(). (2) is an sgrep version of https://checks.bento.dev/en/latest/flake8-flask/secure-set-cookie/.
    
    * Remove *-2.py tests which are not supported yet
    
    * Update secure-session-cookies to use metavars instead of constants to account for the case when these are set using variables
    
    * Removed backup file
    
    * Added checks for setting secure session cookies in the case where the dev uses dictionary assignment to app.config instead of app.config.update(...). Was running into issues trying to make the pattern elegant, so I did the brute-force method.  Added two checks, *dict-set.py and *dict-some-set.py for testing.
    
    * Removed backup file
    
    * Formatting
    
    * Added more tests

--- python/flask/secure-cookies/session-secure-cookies-dict-set-different-order.py
+++ python/flask/secure-cookies/session-secure-cookies-dict-set-different-order.py
@@ -1,10 +1,12 @@
 import flask
 
+# ok
 app = flask.Flask()
 
-app.config["SESSION_COOKIE_SECURE"] = True
 app.config["SESSION_COOKIE_HTTPONLY"] = True
+print("statement")
 app.config["SESSION_COOKIE_SAMESITE"] = "Strict"
+app.config["SESSION_COOKIE_SECURE"] = True
 
 if __name__ == "__main__":
     app.run()
