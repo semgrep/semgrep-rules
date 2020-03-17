da67fd0e940c0eaad41cd6efb3fdec017cf08602
Nathan Brahms <nathan@returntocorp.com>
2020-03-17 13:50:37 -0700

    fix fp in js useless assignment

--- javascript/deadcode/useless-assign.js
+++ javascript/deadcode/useless-assign.js
@@ -4,3 +4,13 @@ var x = 1;
 x = 2;
 x = 3;
 console.log(x);
+
+// ok
+y = [1, 2];
+y = y.map(function(e) { return e * 2; });
+console.log(y);
+
+// ok
+z = [1, 2];
+z = z.map(e => e * 2);
+console.log(z);
