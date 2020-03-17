afdea42e5854e8c4b574e83116c09126e7ab92ec
Vasilii Ermilov <inkz@xakep.ru>
2020-03-17 12:30:57 +1000

    add jwt-hardcode rule for javascript (#243)

---
+++ javascript/jwt-hardcode/example4.js
@@ -0,0 +1,15 @@
+const $jwt = require('jsonwebtoken');
+
+const cert = 'hardcoded-secret';
+
+module.exports = (app) => {
+  app.post('/api/login', (req, res) => {
+    app.login(req.body.username, req.body.password).then((out) => {
+      out.token = $jwt.sign(out, cert, {expiresIn: '1d'});
+      res.send(out);
+    }, (err) => {
+      console.error(err);
+      res.status(400).send(err);
+    });
+  });
+};
