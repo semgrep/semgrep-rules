046264751466783c60c6a12d76cef206f5591369
Ulzii Otgonbaatar <ulzii@returntocorp.com>
2020-03-26 14:16:37 -0600

    move inline def

--- python/django/security/sql-injection-using-raw.py
+++ python/django/security/sql-injection-using-raw.py
@@ -7,30 +7,30 @@ class Person(models.Model):
 
 ##### raw() True Positives #########
 def get_user_age(request):
-  user_name = request.get('user_name')
   # ruleid: sql-injection-using-raw
+  user_name = request.get('user_name')
   user_age = Person.objects.raw('SELECT user_age FROM myapp_person where user_name = %s' % user_name)
   html = "<html><body>User Age %s.</body></html>" % user_age
   return HttpResponse(html)
 
 def get_user_age(request):
-  user_name = request.get('user_name')
   # pending https://github.com/returntocorp/sgrep/issues/326
   # tododruleid: sql-injection-using-raw
+  user_name = request.get('user_name')
   user_age = Person.objects.raw(f"SELECT user_age FROM myapp_person where user_name = {user_name}")
   html = "<html><body>User Age %s.</body></html>" % user_age
   return HttpResponse(html)
 
 def get_user_age(request):
-  user_name = request.get('user_name')
   # ruleid: sql-injection-using-raw
+  user_name = request.get('user_name')
   user_age = Person.objects.raw('SELECT user_age FROM myapp_person where user_name = %s'.format(user_name))
   html = "<html><body>User Age %s.</body></html>" % user_age
   return HttpResponse(html)
 
 def get_users(request):
-  client_id = request.headers.get('client_id')
   # ruleid: sql-injection-using-raw
+  client_id = request.headers.get('client_id')
   users = Person.objects.raw('SELECT * FROM myapp_person where client_id = %s' % client_id)
   html = "<html><body>Users %s.</body></html>" % users
   return HttpResponse(html)
--- python/django/security/sql_injection_rawsql.py
+++ python/django/security/sql_injection_rawsql.py
@@ -3,30 +3,30 @@ from django.http import HttpResponse
 
 ##### RawSQL() True Positives #########
 def get_user_age(request):
-  user_name = request.get('user_name')
   # ruleid: sql-injection-using-rawsql
+  user_name = request.get('user_name')
   user_age = RawSQL('SELECT user_age FROM myapp_person where user_name = %s' % user_name)
   html = "<html><body>User Age %s.</body></html>" % user_age
   return HttpResponse(html)
 
 def get_user_age(request):
-  user_name = request.get('user_name')
   # pending https://github.com/returntocorp/sgrep/issues/326
   # todoruleid: sql-injection-using-rawsql
+  user_name = request.get('user_name')
   user_age = RawSQL(f'SELECT user_age FROM myapp_person where user_name = {user_name}')
   html = "<html><body>User Age %s.</body></html>" % user_age
   return HttpResponse(html)
 
 def get_user_age(request):
-  user_name = request.get('user_name')
   # ruleid: sql-injection-using-rawsql
+  user_name = request.get('user_name')
   user_age = RawSQL('SELECT user_age FROM myapp_person where user_name = %s'.format(user_name))
   html = "<html><body>User Age %s.</body></html>" % user_age
   return HttpResponse(html)
 
 def get_users(request):
-  client_id = request.headers.get('client_id')
   # ruleid: sql-injection-using-rawsql
+  client_id = request.headers.get('client_id')
   users = RawSQL('SELECT * FROM myapp_person where client_id = %s'.format(client_id))
   html = "<html><body>Users %s.</body></html>" % users
   return HttpResponse(html)
