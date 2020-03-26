064595436e229e06f45b672ea20c2d881ded3122
grayson <grayson@returntocorp.com>
2020-03-25 17:57:23 -0700

    Added a test case for the previous commit

--- python/django/security/passwords/unvalidated-password.py
+++ python/django/security/passwords/unvalidated-password.py
@@ -34,3 +34,24 @@ def other(user_profile: UserProfile) -> None:
 	user_profile.save()
 
 	user_profile.assertIsNotNone(EmailAuthBackend().authenticate(username=user_profile.example_email('hamlet'), password=password))
+
+class ModelBackend(BaseBackend):
+    """
+    Authenticates against settings.AUTH_USER_MODEL.
+    """
+
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        if username is None:
+            username = kwargs.get(UserModel.USERNAME_FIELD)
+        if username is None or password is None:
+            return
+        try:
+            user = UserModel._default_manager.get_by_natural_key(username)
+        except UserModel.DoesNotExist:
+            # Run the default password hasher once to reduce the timing
+            # difference between an existing and a nonexistent user (#20760).
+			# ok
+            UserModel().set_password(password)
+        else:
+            if user.check_password(password) and self.user_can_authenticate(user):
+                return user
