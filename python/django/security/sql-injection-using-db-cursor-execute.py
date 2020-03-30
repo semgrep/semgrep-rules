d43d49043c3d6a70e266a0a2830ef1c3acfadb08
Ulzii Otgonbaatar <ulzii@returntocorp.com>
2020-03-30 16:23:21 -0600

    direct flows pattern and test

--- python/django/security/sql-injection-using-db-cursor-execute.py
+++ python/django/security/sql-injection-using-db-cursor-execute.py
@@ -5,6 +5,10 @@ def fetch_name_0(request):
   with connection.cursor() as cursor:
       # ruleid: sql-injection-db-cursor-execute
       cursor.execute(f"SELECT foo FROM bar WHERE baz = {request.data.get('baz')}")
+      # ruleid: sql-injection-db-cursor-execute
+      cursor.execute("SELECT foo FROM bar WHERE baz = %s" % request.data.get('baz'))
+      # ruleid: sql-injection-db-cursor-execute
+      cursor.execute("SELECT foo FROM bar WHERE baz = %s".format(request.data.get('baz')))
       row = cursor.fetchone()
   return row
 
