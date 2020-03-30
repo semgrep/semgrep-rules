3acd02deb525f43626632f2e9116410381268de8
Ulzii Otgonbaatar <ulzii@returntocorp.com>
2020-03-30 15:58:32 -0600

    Add SQLi detection for execute() sink

---
+++ python/django/security/sql-injection-using-db-cursor-execute.py
@@ -0,0 +1,35 @@
+from django.db import connection
+
+##### True Positives #########
+def fetch_name_1(request):
+  baz = request.data.get("baz") 
+  with connection.cursor() as cursor:
+      cursor.execute(f"UPDATE bar SET foo = 1 WHERE baz = {baz}")
+      cursor.execute(f"SELECT foo FROM bar WHERE baz = {baz}")
+      row = cursor.fetchone()
+  return row
+
+def fetch_name_2(request):
+  baz = request.data.get("baz") 
+  with connection.cursor() as cursor:
+      cursor.execute("SELECT foo FROM bar WHERE baz = %s" % baz)
+      row = cursor.fetchone()
+  return row
+
+def fetch_name_3(request):
+  baz = request.data.get("baz") 
+  with connection.cursor() as cursor:
+      cursor.execute("SELECT foo FROM bar WHERE baz = %s".format(baz))
+      row = cursor.fetchone()
+  return row
+
+
+##### True Negatives #########
+def fetch_name_4(request):
+  baz = request.data.get("baz") 
+  with connection.cursor() as cursor:
+      cursor.execute("UPDATE bar SET foo = 1 WHERE baz = %s", [baz])
+      cursor.execute("SELECT foo FROM bar WHERE baz = %s", [baz])
+      row = cursor.fetchone()
+
+  return row
