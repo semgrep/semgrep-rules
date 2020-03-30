8ae1d0c7851594dcc1b566cd77f9d4ad4fe5e6ca
Ulzii Otgonbaatar <ulzii@returntocorp.com>
2020-03-30 16:14:03 -0600

    precommit

--- python/django/security/sql-injection-using-db-cursor-execute.py
+++ python/django/security/sql-injection-using-db-cursor-execute.py
@@ -10,7 +10,7 @@ def fetch_name_0(request):
 
 def fetch_name_1(request):
   # ruleid: sql-injection-db-cursor-execute
-  baz = request.data.get("baz") 
+  baz = request.data.get("baz")
   with connection.cursor() as cursor:
       cursor.execute(f"UPDATE bar SET foo = 1 WHERE baz = {baz}")
       cursor.execute(f"SELECT foo FROM bar WHERE baz = {baz}")
@@ -19,7 +19,7 @@ def fetch_name_1(request):
 
 def fetch_name_2(request):
   # ruleid: sql-injection-db-cursor-execute
-  baz = request.data.get("baz") 
+  baz = request.data.get("baz")
   with connection.cursor() as cursor:
       cursor.execute("SELECT foo FROM bar WHERE baz = %s" % baz)
       row = cursor.fetchone()
@@ -27,7 +27,7 @@ def fetch_name_2(request):
 
 def fetch_name_3(request):
   # ruleid: sql-injection-db-cursor-execute
-  baz = request.data.get("baz") 
+  baz = request.data.get("baz")
   with connection.cursor() as cursor:
       cursor.execute("SELECT foo FROM bar WHERE baz = %s".format(baz))
       row = cursor.fetchone()
@@ -37,7 +37,7 @@ def fetch_name_3(request):
 ##### True Negatives #########
 def fetch_name_4(request):
   # using param list is ok
-  baz = request.data.get("baz") 
+  baz = request.data.get("baz")
   with connection.cursor() as cursor:
       cursor.execute("UPDATE bar SET foo = 1 WHERE baz = %s", [baz])
       cursor.execute("SELECT foo FROM bar WHERE baz = %s", [baz])
