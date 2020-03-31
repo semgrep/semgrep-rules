b98668657e497911899ebd3d2f9752f0f6e6a976
minusworld <grayson@returntocorp.com>
2020-03-31 11:04:09 -0700

    Updated test for no-csrf-exempt now that https://github.com/returntocorp/sgrep/issues/294 is working. Renamed check_id to 'no-csrf-exempt' and renamed files to 'csrf-exempt'. (#288)

--- python/django/security/csrf-exempt.py
+++ python/django/security/csrf-exempt.py
@@ -1,14 +1,15 @@
 from django.http import HttpResponse
-# ruleid: django-csrf_exempt
+# ruleid: no-csrf-exempt
 from django.views.decorators.csrf import csrf_exempt
 
+# ruleid: no-csrf-exempt
 @csrf_exempt
 def my_view(request):
     return HttpResponse('Hello world')
 
 import django
 
-# ruleid: django-csrf_exempt
+# ruleid: no-csrf-exempt
 @django.views.decorators.csrf.csrf_exempt
 def my_view2(request):
     return HttpResponse('Hello world')
