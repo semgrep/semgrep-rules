297d8f22f7ee3ec03106f7150e22e1ea0b0b2359
minusworld <grayson@returntocorp.com>
2020-03-31 14:53:25 -0700

    Added checks for user data going into eval and exec. (#272)
    * Updated test for no-csrf-exempt now that https://github.com/returntocorp/sgrep/issues/294 is working. Renamed check_id to 'no-csrf-exempt' and renamed files to 'csrf-exempt'.
    
    * Added checks for user data going into eval and exec.
    
    * Added new checks to account for format string cases of user data flowing into exec and eval.
    
    * Fix tests
    
    * Updated check message for user-eval* and user-exec checks. Added f-string cases to user-eval-format-string and user-exec-format-string. Added test cases for these.

---
+++ python/django/security/user-eval.py
@@ -0,0 +1,22 @@
+from textwrap import dedent
+
+def unsafe(request):
+    # ruleid: user-eval
+    code = request.POST.get('code')
+    print("something")
+    eval(code)
+
+def unsafe_inline(request):
+    # ruleid: user-eval
+    eval(request.GET.get('code'))
+
+def unsafe_dict(request):
+    # ruleid: user-eval
+    eval(request.POST['code'])
+
+def safe(request):
+    # ok
+    code = """
+    print('hello')
+    """
+    eval(dedent(code))
