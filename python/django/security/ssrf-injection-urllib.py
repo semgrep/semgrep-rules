14b2db4875cd69e78456b7d764e23a28ce62e31f
Nathan Brahms <nathan@returntocorp.com>
2020-03-31 16:55:56 -0700

    fixup! Add python/django/security/ssrf-injection-requests

--- python/django/security/ssrf-injection-urllib.py
+++ python/django/security/ssrf-injection-urllib.py
@@ -13,7 +13,7 @@ def test2():
     from django.http import HttpResponse
 
     def send_to_redis(request):
-        # ruleid: ssrf-injection-requests
+        # ruleid: ssrf-injection-urllib
         bucket = request.GET.get("bucket")
         inner_response = urlopen("http://my.redis.foo/{}".format(bucket), data=3)
         return HttpResponse(body = {"response_code": inner_response.status_code})
