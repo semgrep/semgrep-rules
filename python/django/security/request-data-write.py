5fff94c9ddbc8a5ed79a07d63b6e94cd2e0a3edc
grayson <grayson@returntocorp.com>
2020-04-01 23:37:18 -0700

    New check for request data going into .write

---
+++ python/django/security/request-data-write.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+from . import settings as USettings
+
+def save_scrawl_file(request, filename):
+    import base64
+    try:
+        content = request.POST.get(USettings.UEditorUploadSettings.get("scrawlFieldName", "upfile"))
+        f = open(filename, 'wb')
+        f.write(base64.decodestring(content))
+        f.close()
+        state = "SUCCESS"
+    except Exception as e:
+        state = u"写入图片文件错误:%s" % e
+    return state
