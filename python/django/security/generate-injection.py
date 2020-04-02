48c9f2d42e15189179b7a50d01dd91bffc9cbc42
grayson <grayson@returntocorp.com>
2020-04-02 00:22:21 -0700

    Updated injection template script and template. Now utilized for loops in the template to make it easy to enumerate new sources, sinks, and 'format trajectories.'

--- python/django/security/generate-injection.py
+++ python/django/security/generate-injection.py
@@ -1,29 +1,60 @@
 #!/usr/bin/env python3
 
-import argparse
+import logging
 import os
+import subprocess
 import sys
-from jinja2 import Environment, FileSystemLoader
 
+from jinja2 import Template
 
-def main():
-    parser = argparse.ArgumentParser("CLI for injection templates")
+logger = logging.getLogger(__file__)
+logger.setLevel(logging.INFO)
+handler = logging.StreamHandler(stream=sys.stderr)
+handler.setFormatter(
+    logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+)
+logger.addHandler(handler)
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("template_file", type=str, help="The template file to use.")
     parser.add_argument("--id", type=str, help="Check ID")
     parser.add_argument("--message", type=str, help="Check message")
     parser.add_argument("--sink", type=str, help="Sink")
 
-    options = parser.parse_args(sys.argv[1:])
+    args = parser.parse_args()
 
-    env = Environment(
-        loader=FileSystemLoader(os.path.join(os.getcwd(), "templates/"))
+    request_data_patterns = (
+        "request.$W.get(...)",
+        "request.$W(...)",
+        "request.$W[...]",
+        "request.$W"
     )
-    injection_template = env.get_template("injection-template.yaml.j2")
-    print(injection_template.render(
-        id=options.id,
-        message=options.message,
-        sink=options.sink,
-    ))
+    format_patterns = (
+        "$DATA",
+        "$B.$C(..., $DATA, ...)",
+        "$STR % $DATA",
+        "f\"...{$DATA}...\"",
+        "$STR + $DATA",
+    )
+    left_patterns = (
+        "",
+        "$A = ",
+        "return "
+    )
 
+    context = {
+        "sink": args.sink,
+        "check_id": args.id,
+        "message": args.message,
+        "request_data_patterns": request_data_patterns,
+        "format_patterns": format_patterns,
+        "left_patterns": left_patterns,
+    }
 
-if __name__ == "__main__":
-    main()
+    with open(args.template_file, "r") as fin:
+        template_file_contents = fin.read()
+    t = Template(template_file_contents)
+    print(t.render(context))
