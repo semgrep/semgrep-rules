c0d56042214c38b42691c1260ea04c8032ef2554
minusworld <grayson@returntocorp.com>
2020-04-03 10:10:27 -0700

    Added reference comment for test cases

--- python/django/security/mass-assignment.py
+++ python/django/security/mass-assignment.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from myapp.models import Whatzit
 
+# Test cases borrowed from https://gist.github.com/jsocol/3217262
+
 def create_whatzit(request):
     # ruleid: mass-assignment
     Whatzit.objects.create(**request.POST)
