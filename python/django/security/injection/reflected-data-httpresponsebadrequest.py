97be28b406bb6a19f57ba1ca5dc050de5203ad94
grayson <grayson@returntocorp.com>
2020-04-06 16:43:09 -0700

    Add inline patterns to reflected-data-httpresponsebadrequest. Added test case too

--- python/django/security/injection/reflected-data-httpresponsebadrequest.py
+++ python/django/security/injection/reflected-data-httpresponsebadrequest.py
@@ -15,3 +15,24 @@ def search_certificates(request):
     user = User.objects.get(Q(email=user_filter) | Q(username=user_filter))
     if user.DoesNotExist:
         return HttpResponseBadRequest(_("user '{user}' does not exist").format(user_filter))
+
+def previewNode(request, uid):
+    """Preview evaluante node"""
+    try:
+        if uid in engines:
+            # ok
+            _nodeId = request.data.get('nodeId')
+            engines[uid].stoppable = True
+            _res = engines[uid].model.previewNode(_nodeId)
+            if _res is None:
+                return HttpResponseBadRequest('', status=204)
+            return HttpResponseBadRequest(_res)
+        return manageNoEngine()
+    except Exception as e:
+        return genericApiException(e, engines[uid])
+    finally:
+        engines[uid].stoppable = False
+
+def inline_test(request):
+    # ruleid: reflected-data-httpresponsebadrequest
+    return HttpResponseBadRequest("Received {}".format(request.POST.get('message')))
