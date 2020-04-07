d574593153ad0f1b1ed7ee9958d9575b9531e46b
grayson <grayson@returntocorp.com>
2020-04-06 17:41:23 -0700

    Updated path-traversal-join with new patterns based on a real-world false negative. Added a test case for this.

--- python/django/security/injection/path-traversal-join.py
+++ python/django/security/injection/path-traversal-join.py
@@ -15,3 +15,14 @@ def foo_2(request):
   file_path = os.path.abspath(file_path)
   f = open(file_path, 'r')
   return HttpResponse(content=f, content_type="text/plain")
+
+def user_pic(request):
+    """A view that is vulnerable to malicious file access."""
+
+    base_path = os.path.join(os.path.dirname(__file__), '../../badguys/static/images')
+    # ruleid: path-traversal-join
+    filename = request.GET.get('p')
+
+    data = open(os.path.join(base_path, filename), 'rb').read()
+
+    return HttpResponse(data, content_type=mimetypes.guess_type(filename)[0])
