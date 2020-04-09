0879c7c77b661844d35afdab17abd01639c5548f
Drew Dennison <drew@returntocorp.com>
2020-04-08 20:14:00 -0700

    fix tests

--- python/bokeh/deprecated_apis.py
+++ python/bokeh/deprecated_apis.py
@@ -1,8 +1,8 @@
+# ruleid:bokeh-deprecated-apis
 from bokeh.layouts import widgetbox
+# ruleid:bokeh-deprecated-apis
 from bokeh.models.graphs import from_networkx
 
-# ruleid:bokeh-deprecated-apis
 widgetbox(children=[slider], sizing_mode='scale_width')
 
-# ruleid:bokeh-deprecated-apis
 graph_renderer = from_networkx(G, nx.spring_layout, scale=0.5, center=(0,0))
