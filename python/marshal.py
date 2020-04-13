55c19eef2e4d00d63aa6357fd9507fb6122c5da6
Isaac Evans <409041+ievans@users.noreply.github.com>
2020-04-13 11:30:28 -0700

    Update marshal.py

--- python/marshal.py
+++ python/marshal.py
@@ -4,7 +4,7 @@ fin = open('index.mar')
 for line in fin:
     # ruleid: marshal-usage
     marshal.dumps(line)
-    
+
 for line in fin:
     # ok
-    marshal.someokfunc(line)    
+    marshal.someokfunc(line)
