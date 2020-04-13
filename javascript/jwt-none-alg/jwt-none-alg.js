cfba5921b99e5c4fb43a0574b2b2af220eb6ab33
inkz <inkz@xakep.ru>
2020-04-13 20:54:49 +1000

    Update jwt-none-alg examples

--- javascript/jwt-none-alg/jwt-none-alg.js
+++ javascript/jwt-none-alg/jwt-none-alg.js
@@ -2,3 +2,17 @@
 const jose = require("jose");
 const { JWK, JWT } = jose;
 const token = JWT.verify('token-here', JWK.None);
+
+function verifyJwt() {
+	// ruleid: jwt-none-alg
+	let jwt = require("jsonwebtoken");
+	let secret = 'some-secret';
+	jwt.verify('token-here', secret, { algorithms: ['RS256', 'none'] }, function(err, payload) {
+		console.log(payload);
+	});
+}	
+
+// ok
+const jwt = require("jsonwebtoken");
+const secret = 'some-secret';
+const payload = jwt.verify('token-here', secret, { algorithms: ['RS256', 'HS256'] });
