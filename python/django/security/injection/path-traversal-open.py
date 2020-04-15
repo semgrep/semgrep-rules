86373c146bff6b6dd664b1b83a6e2ccd6e4a32d1
grayson <grayson@returntocorp.com>
2020-04-15 16:52:29 -0700

    Added FN case for path-traversal-open

--- python/django/security/injection/path-traversal-open.py
+++ python/django/security/injection/path-traversal-open.py
@@ -1,4 +1,6 @@
+import re, os
 from django.http import HttpResponse
+from somewhere import APIView
 
 def unsafe(request):
     # ruleid: path-traversal-open
@@ -58,3 +60,25 @@ def download_doc(request):
     response["Content-Type"] = "application/octet-stream"
     response["Content-Disposition"] = "attachment;filename=\"{0}\"".format(file_name)
     return response
+
+class GenerateUserAPI(APIView):
+    def get(self, request):
+        """
+        download users excel
+        """
+        # ruleid: path-traversal-open
+        file_id = request.GET.get("file_id")
+        if not file_id:
+            return self.error("Invalid Parameter, file_id is required")
+        if not re.match(r"^[a-zA-Z0-9]+$", file_id):
+            return self.error("Illegal file_id")
+        file_path = f"/tmp/{file_id}.xlsx"
+        if not os.path.isfile(file_path):
+            return self.error("File does not exist")
+        with open(file_path, "rb") as f:
+            raw_data = f.read()
+        os.remove(file_path)
+        response = HttpResponse(raw_data)
+        response["Content-Disposition"] = f"attachment; filename=users.xlsx"
+        response["Content-Type"] = "application/xlsx"
+        return response
