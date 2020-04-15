798998dfbb4eb7d9ae9f9ddf5274d36ff6d7abdc
grayson <grayson@returntocorp.com>
2020-04-15 16:43:34 -0700

    Added todoruleid test for tuple % format case

--- python/django/security/injection/sql-injection-using-db-cursor-execute.py
+++ python/django/security/injection/sql-injection-using-db-cursor-execute.py
@@ -37,6 +37,25 @@ def fetch_name_3(request):
       row = cursor.fetchone()
   return row
 
+def upload(request, project_id):
+
+    if request.method == 'POST':
+
+        proj = Project.objects.get(pk=project_id)
+        form = ProjectFileForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            # Dependent on feature in develop
+            # todoruleid: sql-injection-db-cursor-execute
+            name = request.POST.get('name', False)
+            upload_path = store_uploaded_file(name, request.FILES['file'])
+
+            other_name = "{}".format(name)
+            curs = connection.cursor()
+            curs.execute(
+                "insert into taskManager_file ('name','path','project_id') values ('%s','%s',%s)" %
+                (other_name, upload_path, project_id))
+
 
 ##### True Negatives #########
 def fetch_name_4(request):
