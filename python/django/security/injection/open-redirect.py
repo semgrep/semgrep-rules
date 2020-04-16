98cdb271f82575ecbe2f68f7f43e7d0aae4e0c52
grayson <grayson@returntocorp.com>
2020-04-15 17:33:12 -0700

    Added FN cases: one deep in an if statement (can't find this one yet) and one inline.

--- python/django/security/injection/open-redirect.py
+++ python/django/security/injection/open-redirect.py
@@ -1,5 +1,6 @@
 from django.shortcuts import redirect
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
+from django.utils.http import is_safe_url
 
 def unsafe(request):
     # ruleid: open-redirect
@@ -17,4 +18,14 @@ def unsafe2(request):
     url = request.POST.get("url")
     return HttpResponseRedirect(url)
 
+def fine(request):
+    # ok
+    return HttpResponseRedirect(request.get_full_path())
 
+def url_validation(request):
+    # this will fire for now until we can filter out is_safe_url
+    next = request.POST.get('next', request.GET.get('next'))
+    if (next or not request.is_ajax()) and not is_safe_url(url=next, allowed_hosts=request.get_host()):
+        next = "/index"
+    response = HttpResponseRedirect(next) if next else HttpResponse(status=204)
+    return response
