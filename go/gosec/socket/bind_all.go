9cf49bc76836ce04b3685aba86f7d9a7f89d3e0b
minusworld <grayson@returntocorp.com>
2020-05-05 16:59:37 -0700

    Added bind_default test case to bind_all.go to run test in CI

--- go/gosec/socket/bind_all.go
+++ go/gosec/socket/bind_all.go
@@ -14,6 +14,15 @@ func bind_all() {
 	defer l.Close()
 }
 
+func bind_default() {
+	// ruleid: avoid-bind-to-all-interfaces
+	l, err := net.Listen("tcp", ":2000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer l.Close()
+}
+
 func main() {
 	// ok
 	l, err := net.Listen("tcp", "192.168.1.101:2000")
