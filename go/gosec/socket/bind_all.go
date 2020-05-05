a2b29c1b044418a632e39ecf76bbb45bfdd48841
minusworld <grayson@returntocorp.com>
2020-05-05 16:58:39 -0700

    Added True Negative test

--- go/gosec/socket/bind_all.go
+++ go/gosec/socket/bind_all.go
@@ -5,9 +5,18 @@ import (
 	"net"
 )
 
-func main() {
+func bind_all() {
 	// ruleid: avoid-bind-to-all-interfaces
 	l, err := net.Listen("tcp", "0.0.0.0:2000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer l.Close()
+}
+
+func main() {
+	// ok
+	l, err := net.Listen("tcp", "192.168.1.101:2000")
 	if err != nil {
 		log.Fatal(err)
 	}
