fc3a7b48710c9503e710c36075dbea93efdbbc6a
Ulzii Otgonbaatar <ulzii@returntocorp.com>
2020-05-07 11:21:30 -0600

    adding tempfile tn

--- go/gosec/bad_temp_file/bad_tmp.go
+++ go/gosec/bad_temp_file/bad_tmp.go
@@ -12,3 +12,10 @@ func main() {
 		fmt.Println("Error while writing!")
 	}
 }
+func main_good() {
+	// ok
+	err := ioutil.Tempfile("/tmp", "my_temp")
+	if err != nil {
+		fmt.Println("Error while writing!")
+	}
+}
