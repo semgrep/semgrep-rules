45589e6568591740e23ab8893fdb8211ae8501a7
Ulzii Otgonbaatar <ulzii@returntocorp.com>
2020-05-07 11:24:55 -0600

    use hyphen and add inline ruleid for CI test

--- go/gosec/unsafe.go
+++ go/gosec/unsafe.go
@@ -1,19 +1,25 @@
 package main
+
 import (
-    "fmt"
+	"fmt"
+	"unsafe"
+
+	foobarbaz "unsafe"
 )
-import foobarbaz "unsafe"
 
 type Fake struct{}
+
 func (Fake) Good() {}
 func main() {
-    unsafeM := Fake{}
-    unsafeM.Good()
-    intArray := [...]int{1, 2}
-    fmt.Printf("\nintArray: %v\n", intArray)
-    intPtr := &intArray[0]
-    fmt.Printf("\nintPtr=%p, *intPtr=%d.\n", intPtr, *intPtr)
-    addressHolder := uintptr(foobarbaz.Pointer(intPtr)) + unsafe.Sizeof(intArray[0])
-    intPtr = (*int)(foobarbaz.Pointer(addressHolder))
-    fmt.Printf("\nintPtr=%p, *intPtr=%d.\n\n", intPtr, *intPtr)
+	unsafeM := Fake{}
+	unsafeM.Good()
+	intArray := [...]int{1, 2}
+	fmt.Printf("\nintArray: %v\n", intArray)
+	intPtr := &intArray[0]
+	fmt.Printf("\nintPtr=%p, *intPtr=%d.\n", intPtr, *intPtr)
+	// ruleid: use-of-unsafe-block
+	addressHolder := uintptr(foobarbaz.Pointer(intPtr)) + unsafe.Sizeof(intArray[0])
+	// ruleid: use-of-unsafe-block
+	intPtr = (*int)(foobarbaz.Pointer(addressHolder))
+	fmt.Printf("\nintPtr=%p, *intPtr=%d.\n\n", intPtr, *intPtr)
 }
