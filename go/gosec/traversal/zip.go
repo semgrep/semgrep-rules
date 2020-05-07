70e915d9a20ba75194ab2beb0384ddb3af627a98
Ulzii Otgonbaatar <ulzii@returntocorp.com>
2020-05-07 11:18:25 -0600

    add true negative

--- go/gosec/traversal/zip.go
+++ go/gosec/traversal/zip.go
@@ -2,7 +2,9 @@ package unzip
 
 import (
 	"archive/zip"
+	"fmt"
 	"io"
+	"log"
 	"os"
 	"path/filepath"
 )
@@ -43,4 +45,31 @@ func unzip(archive, target string) error {
 	}
 
 	return nil
+}
+
+func unzip_good() {
+	// Open a zip archive for reading.
+	r, err := zip.OpenReader("testdata/readme.zip")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer r.Close()
+	// Iterate through the files in the archive,
+	// printing some of their contents.
+	for _, f := range r.File {
+		fmt.Printf("Contents of %s:\n", f.Name)
+		rc, err := f.Open()
+		if err != nil {
+			log.Fatal(err)
+		}
+		_, err = io.CopyN(os.Stdout, rc, 68)
+		if err != nil {
+			log.Fatal(err)
+		}
+		rc.Close()
+		fmt.Println()
+	}
+	// Output:
+	// Contents of README:
+	// This is the source code repository for the Go programming language.
 }
