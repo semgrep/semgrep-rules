0cba6098949f35b940c40cdb170cc51d02e443ba
Grayson <grayson@returntocorp.com>
2020-05-08 17:59:56 -0700

    Add test annotations

--- java/security/audit/blowfish-insufficient-key-size.java
+++ java/security/audit/blowfish-insufficient-key-size.java
@@ -6,16 +6,19 @@ import javax.servlet.http.HttpServletRequestWrapper;
 public class Cls {
 
     public void unsafeKeySize() {
+        // ruleid: blowfish-insufficient-key-size
         KeyGenerator keyGen = KeyGenerator.getInstance("Blowfish");
         keyGen.init(64);
     }
 
     public void safeKeySize() {
+        // ok
         KeyGenerator keyGen = KeyGenerator.getInstance("Blowfish");
         keyGen.init(128);
     }
 
     public void superSafeKeySize() {
+        // ok
         KeyGenerator keyGen = KeyGenerator.getInstance("Blowfish");
         keyGen.init(448);
     }
