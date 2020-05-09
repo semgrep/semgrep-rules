a79235bbaa1c84259711b31f9a96f1efecc094fc
grayson <grayson@starnomaddigital.com>
2020-05-08 19:01:04 -0700

    Fix conn_recv

--- python/lang/security/audit/conn_recv.py
+++ python/lang/security/audit/conn_recv.py
@@ -1,6 +1,10 @@
 import multiprocessing
 import multiprocessing.connection
 
+
+rx = multiprocessing.connection.Client(('localhost', 12345)).recv()
+
+# ruleid: multiprocessing-recv
 connection = multiprocessing.connection.Client(
     ('localhost', 12345),
 )
