48bb3c57dab5a7d1415aea4822961c0c444a0249
Emma Jin <emjin@users.noreply.github.com>
2020-05-22 19:28:45 -0400

    Modified to match request pased either through an intermediate variable or directly. Added test cases accordingly.
    Used deep expression matching to match through layers of function calls.

--- python/django/security/audit/avoid-insecure-deserialization.py
+++ python/django/security/audit/avoid-insecure-deserialization.py
@@ -9,18 +9,74 @@ def current_datetime(request):
     
     return "Hey there! {}!".format(pickle.loads(b64decode(html)))
 
+# pickle tests
+
 def current_datetime(request):
-    # ruleid: avoid-insecure-deserialization
-    user_obj = request.cookies.get('uuid')
+    # ruleid:avoid-insecure-deserialization
+    user_obj = b64decode(request.cookies.get('uuid'))
     now = datetime.datetime.now()
     html = "<html><body>It is now %s.</body></html>" % now
     
     return "Hey there! {}!".format(pickle.loads(user_obj))
 
 def current_datetime(request):
-    # ok (TODO: should fail)
-    user_obj = request.cookies.get('uuid')
+    # ruleid:avoid-insecure-deserialization
+    user_obj = request.cookies.get('uuid') 
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now
+
+    return "Hey there! {}!".format(pickle.loads(user_obj))
+
+def current_datetime(request):
+    # ruleid:avoid-insecure-deserialization
+    user_obj = request.cookies.get('uuid')    
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now
+
+    return "Hey there! {}!".format(pickle.loads(b64decode(user_obj)))
+
+def current_datetime(request):
+    # ruleid:avoid-insecure-deserialization
+    return "Hey there! {}!".format(pickle.loads(b64decode(request.cookies.get('uuid'))))
+
+# Other libraries
+
+def current_datetime(request):
+    # ruleid:avoid-insecure-deserialization
+    user_obj = b64decode(request.cookies.get('uuid'))
     now = datetime.datetime.now()
     html = "<html><body>It is now %s.</body></html>" % now
     
-    return "Hey there! {}!".format(pickle.loads(b64decode(user_obj)))
+    return "Hey there! {}!".format(_pickle.loads(user_obj))
+
+def current_datetime(request):
+    # ruleid:avoid-insecure-deserialization
+    user_obj = request.cookies.get('uuid') 
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now
+
+    return "Hey there! {}!".format(cPickle.loads(user_obj))
+
+def current_datetime(request):
+    # ruleid:avoid-insecure-deserialization
+    user_obj = request.cookies.get('uuid')    
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now
+
+    return "Hey there! {}!".format(dill.loads(b64decode(user_obj)))
+    
+def current_datetime(request):
+    # ruleid:avoid-insecure-deserialization
+    user_obj = request.cookies.get('uuid') 
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now
+
+    return "Hey there! {}!".format(shelve.loads(user_obj))
+
+def current_datetime(request):
+    # ruleid:avoid-insecure-deserialization
+    user_obj = request.cookies.get('uuid')    
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now
+
+    return "Hey there! {}!".format(yaml.loads(b64decode(user_obj)))
