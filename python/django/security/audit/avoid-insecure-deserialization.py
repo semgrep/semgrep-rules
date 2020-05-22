bdc2d9400a659301e1e13a0219c98fa4a3fe3fba
Emma Jin <emjin@users.noreply.github.com>
2020-05-22 17:39:16 -0400

    Actually committed the modified file and deleted the old locations of avoid-insecure-deserialization.py/yaml

--- python/django/security/audit/avoid-insecure-deserialization.py
+++ python/django/security/audit/avoid-insecure-deserialization.py
@@ -2,27 +2,25 @@ from django.http import HttpResponse
 import datetime
 
 def current_datetime(request):
+    # ok
     user_obj = request.cookies.get('uuid')
     now = datetime.datetime.now()
     html = "<html><body>It is now %s.</body></html>" % now
     
-    # ok
     return "Hey there! {}!".format(pickle.loads(b64decode(html)))
 
 def current_datetime(request):
+    # ruleid: avoid-insecure-deserialization
     user_obj = request.cookies.get('uuid')
     now = datetime.datetime.now()
     html = "<html><body>It is now %s.</body></html>" % now
     
-    # ruleid: avoid-insecure-deserialization (TODO)
     return "Hey there! {}!".format(pickle.loads(user_obj))
-    #return HttpResponse(html)
 
 def current_datetime(request):
+    # ok (TODO: should fail)
     user_obj = request.cookies.get('uuid')
     now = datetime.datetime.now()
     html = "<html><body>It is now %s.</body></html>" % now
     
-    # ruleid: avoid-insecure-deserialization
     return "Hey there! {}!".format(pickle.loads(b64decode(user_obj)))
-    #return HttpResponse(html)
