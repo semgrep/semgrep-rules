1c4842cbe3742a94f336e21ec6201d3d6bc66f0f
minusworld <grayson@returntocorp.com>
2020-05-26 18:54:45 -0700

    Fix tests

--- go/grpc/security/grpc-client-insecure-connection.go
+++ go/grpc/security/grpc-client-insecure-connection.go
@@ -6,7 +6,7 @@ import (
 
 // cf. https://blog.gopheracademy.com/advent-2019/go-grps-and-tls/#connection-without-encryption
 func unsafe() {
-    // ruleid:grpc-insecure-connection
+    // ruleid:grpc-client-insecure-connection
     conn, err := grpc.Dial(address, grpc.WithInsecure())
     if err != nil {
         log.Fatalf("did not connect: %v", err)
