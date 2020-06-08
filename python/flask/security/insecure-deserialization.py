f4e10c4dcca4948021e2218a91c8a8e3ebea7bc9
minusworld <grayson@returntocorp.com>
2020-06-08 12:59:20 -0700

    Reduce FP rate for some Flask Security rules; port some injection rules from Django (#584)
    * Rename python.flask.security.render-template-string to dangerous-template-string to distinguish from the audit check of the same name.
    
    * For Flask open-redirect check, add in inline checks and filter out FPs when using url_parse
    
    * Rename unsanitized_input to unsanitized-input
    
    * Cleanup unsanitized-input
    
    * Add rule for user data injected into eval
    
    * Add rule for user data injection into os.system
    
    * Add rule for user data injection into open(...)
    
    * Add rule for user data injection into requests., which could be ssrf
    
    * Add rule for user data injection into exec
    
    * Add scripts and jinja template for generating Flask injection rules
    
    * Fix unsanitized-input by namespacing properly.
    
    * Fix insecure-deserialization test annotations

--- python/flask/security/insecure-deserialization.py
+++ python/flask/security/insecure-deserialization.py
@@ -22,12 +22,17 @@ def index():
         msg = "Seems like you didn't have a cookie. No worries! I'll set one now!"
         response = make_response(msg)
         user_obj = UserID()
-        # ruleid:avoid-insecure-deserialization
+        # ruleid:insecure-deserialization
         response.set_cookie('uuid', b64encode(pickle.dumps(user_obj)))
         return response
     else:
-        # ruleid:avoid-insecure-deserialization
+        # ruleid:insecure-deserialization
         return "Hey there! {}!".format(pickle.loads(b64decode(user_obj)))
+
+@app.route("/ok")
+def ok():
+    # ok
+    novellist = pickle.load(open('./novel/list.dat', "rb"))
 
 if __name__ == "__main__":
     # Using host='0.0.0.0' to accept connections from all IPs
