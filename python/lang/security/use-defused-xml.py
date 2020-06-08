b2aed959d195d5eb2e2c5d72166c470c00142561
minusworld <grayson@returntocorp.com>
2020-06-08 10:12:59 -0700

    add tests

--- python/lang/security/use-defused-xml.py
+++ python/lang/security/use-defused-xml.py
@@ -1,4 +1,13 @@
-import xml
-from xml.etree import ElementTree 
-tree = ElementTree.parse('country_data.xml')
-root = tree.getroot()
+def bad():
+    # ruleid: use-defused-xml
+    import xml
+    from xml.etree import ElementTree 
+    tree = ElementTree.parse('country_data.xml')
+    root = tree.getroot()
+    
+def ok():
+    # ok
+    import xml
+    from xml.etree import ElementTree 
+    tree = ElementTree.parse('country_data.xml')
+    root = tree.getroot()
