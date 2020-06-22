b67d0ab4c6e8ee3b24f4f68152a7856f09997bee
inkz <inkz@xakep.ru>
2020-06-22 16:48:26 +1000

    update require-request rule

--- javascript/express/security/require-request.js
+++ javascript/express/security/require-request.js
@@ -13,19 +13,40 @@ function testController1(req, res) {
     }
     res.end('ok')
 };
-
 app.get('/test1', testController1)
 
-app.get('/test2', (req, res) => {
+let testController2 = function (req, res) {
 // ruleid: require-request
     const func = require(req.body)
     return res.send(func())
-})
+}
+app.get('/test2', testController2)
+
+var testController3 = null;
+testController3 = function (req, res) {
+// ruleid: require-request
+    const func = require(req.body)
+    return res.send(func())
+}
+app.get('/test3', testController3)
+
+(function (req, res) {
+// ruleid: require-request
+    const func = require(req.body)
+    return res.send(func())
+})(req, res)
 
 app.get('/ok-test', (req, res) => {
 // ok
     const func = require(hardcodedPath)
     return res.send(func())
 })
+
+let okController = function (req, res) {
+// ok
+    const func = require('lib/func.js')
+    return res.send(func())
+}
+app.get('/ok-test2', okController)
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
