d3efe2e2db00e618aa6f904fe6b12dbeee8923f0
grayson <grayson@returntocorp.com>
2020-06-26 11:53:23 -0700

    Add in positional argument cases for rsa and dsa rules

--- python/cryptography/security/insufficient-dsa-key-size.py
+++ python/cryptography/security/insufficient-dsa-key-size.py
@@ -5,6 +5,14 @@ from cryptography.hazmat.primitives.asymmetric import dsa
 dsa.generate_private_key(key_size=2048,
                          backend=backends.default_backend())
 
+# ok
+dsa.generate_private_key(2048,
+                         backend=backends.default_backend())
+
 # ruleid: insufficient-dsa-key-size
 dsa.generate_private_key(key_size=1024,
+                         backend=backends.default_backend())
+
+# ruleid: insufficient-dsa-key-size
+dsa.generate_private_key(1024,
                          backend=backends.default_backend())
