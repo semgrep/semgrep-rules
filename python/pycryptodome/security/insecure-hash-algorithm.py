70d4ecff4f4663bf81038df42b50dcd0d8c15627
grayson <grayson@returntocorp.com>
2020-06-26 12:59:09 -0700

    Fix tests

--- python/pycryptodome/security/insecure-hash-algorithm.py
+++ python/pycryptodome/security/insecure-hash-algorithm.py
@@ -16,20 +16,20 @@ h_obj = SHA3_256.new()
 h_obj.update(b'Some data')
 print(h_obj.hexdigest())
 
-# ruleid:insecure-cipher-algorithm-md2
+# ruleid:insecure-hash-algorithm-md2
 pycrypto_md2.new()
-# ruleid:insecure-cipher-algorithm-md4
+# ruleid:insecure-hash-algorithm-md4
 pycrypto_md4.new()
-# ruleid:insecure-cipher-algorithm-md5
+# ruleid:insecure-hash-algorithm-md5
 pycrypto_md5.new()
-# ruleid:insecure-cipher-algorithm-sha1
+# ruleid:insecure-hash-algorithm-sha1
 pycrypto_sha.new()
 
-# ruleid:insecure-cipher-algorithm-md2
+# ruleid:insecure-hash-algorithm-md2
 pycryptodomex_md2.new()
-# ruleid:insecure-cipher-algorithm-md4
+# ruleid:insecure-hash-algorithm-md4
 pycryptodomex_md4.new()
-# ruleid:insecure-cipher-algorithm-md5
+# ruleid:insecure-hash-algorithm-md5
 pycryptodomex_md5.new()
-# ruleid:insecure-cipher-algorithm-sha1
+# ruleid:insecure-hash-algorithm-sha1
 pycryptodomex_sha.new()
