55983bee3b8561ac95f0b304c29a2bd0019235fa
grayson <grayson@returntocorp.com>
2020-06-26 11:56:35 -0700

    Handle non-int case for rules

--- python/cryptography/security/insufficient-rsa-key-size.py
+++ python/cryptography/security/insufficient-rsa-key-size.py
@@ -1,9 +1,20 @@
+import os
 from cryptography.hazmat import backends
 from cryptography.hazmat.primitives.asymmetric import rsa
 
 # ok
 rsa.generate_private_key(public_exponent=65537,
                          key_size=2048,
+                         backend=backends.default_backend())
+
+# ok
+rsa.generate_private_key(65537,
+                         2048,
+                         backends.default_backend())
+
+# ok
+rsa.generate_private_key(public_exponent=65537,
+                         key_size=os.getenv("KEY_SIZE"),
                          backend=backends.default_backend())
 
 # ok
