42cb924b53325b8641c4dc4740633983e8e82316
minusworld <grayson@returntocorp.com>
2020-07-13 10:15:44 -0700

    Update test annotations

--- python/requests/best-practice/use-raise-for-status.py
+++ python/requests/best-practice/use-raise-for-status.py
@@ -1,66 +1,70 @@
 import requests
 
-# ruleid:always-use-raise-for-status
+# ruleid:use-raise-for-status
 requests.put("")
 
 requests.put
 requests.put("").raise_for_status("")
 
 def f():
-    # ruleid:always-use-raise-for-status
+    # ruleid:use-raise-for-status
     return requests.get("")
 
 def f():
-    # ruleid:always-use-raise-for-status
+    # ruleid:use-raise-for-status
     return requests.post("")
 
 def f():
-    # ruleid:always-use-raise-for-status
+    # ruleid:use-raise-for-status
     return requests.put("")
 
 def f():
-    # ruleid:always-use-raise-for-status
+    # ruleid:use-raise-for-status
     return requests.patch("")
 
 def f():
-    # ruleid:always-use-raise-for-status
+    # ruleid:use-raise-for-status
     return requests.delete("")
 
 def f():
-    # ruleid:always-use-raise-for-status
+    # ruleid:use-raise-for-status
     requests.put("")
 
 def f():
-    # ruleid:always-use-raise-for-status
+    # ruleid:use-raise-for-status
     r = requests.get("")
     return r
 
 def f():
-    # ruleid:always-use-raise-for-status
+    # ruleid:use-raise-for-status
     r = requests.get("")
     r.raise_for_status
     return r
 
 def f():
+    # ok
     r = requests.get("")
     r.raise_for_status()
-    # ruleid:always-use-raise-for-status
+    # ruleid:use-raise-for-status
     r = requests.get("")
     r.raise_for_status
     return r
 
 def f():
-    # ruleid:always-use-raise-for-status
+    # ruleid:use-raise-for-status
     r = requests.get("")
     r.raise_for_status
+    # ok
     r = requests.get("")
     r.raise_for_status()
     return r
 
 def f():
+    # ok
     r = requests.get("")
     r.raise_for_status()
     return r
 
 def f():
+    # ok
     requests.put("").raise_for_status()
