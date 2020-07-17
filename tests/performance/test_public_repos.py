eacd4a60c58f7cd75ac720c7584869209cb84f82
grayson <grayson@returntocorp.com>
2020-07-17 15:17:40 -0700

    Benchmark public repos are now configurable from from a YAML file. Currently only supports one repo.

--- tests/performance/test_public_repos.py
+++ tests/performance/test_public_repos.py
@@ -1,3 +1,4 @@
+import ast
 import logging
 import os
 import subprocess
@@ -6,7 +7,8 @@ import tempfile
 import yaml
 from pathlib import Path
 from collections import defaultdict
-from typing import Generator, Tuple, Dict, Optional, List
+from textwrap import dedent
+from typing import Generator, Tuple, Dict, Optional, List, Set
 
 import pytest
 
@@ -20,59 +22,78 @@ Config = Dict[Language, RepoTarget]
 
 CONFIG_PATH = (Path(__file__).parent) / "test_public_repos.yaml"
 
-language_to_rules: Dict[Language, Path] = defaultdict(list)
-for rule_path in rule_paths():
-    language_to_rules[os.path.relpath(rule_path, os.getcwd()).split(os.path.sep)[0]].append(rule_path)
-
-def read_config(config_path: Path) -> Config:
+def _read_config(config_path: Path) -> Config:
     with open(config_path, 'r') as fin:
         return yaml.safe_load(fin)
 
-config = read_config(CONFIG_PATH)
+config = _read_config(CONFIG_PATH)
 
-def repo_params(config: Config) -> List[Tuple[Language, RepoTarget]]:
-    targets = []
-    for language in config:
-        target = ArchList(config[language])
-        targets.append(
-            (target.get(0), target.get(1))
-        )
-    return targets
-    
-@pytest.fixture(scope="module", params=repo_params(config))
-def setup_repo(tmp_path_factory, request) -> Path:
-    temp_dir = tmp_path_factory.mktemp("repos")
-    target: RepoTarget = request.param
-    repo_url = target[0]
-    commit_hash = target[1]
+language_to_rules: Dict[Language, List[Path]] = defaultdict(list)
+for rule_path in rule_paths():
+    language_to_rules[os.path.relpath(rule_path, os.getcwd()).split(os.path.sep)[0]].append(rule_path)
 
-    name = repo_url.split('/')[-1]
-    target_dir = os.path.join(temp_dir, name)
-    subprocess.run(
-        ["git", "clone", repo_url, target_dir]
-    )
-    if commit_hash:
+def generate_setup_repo_fixture(repo_target):
+    @pytest.fixture(scope="module")
+    def _setup_repo(tmp_path_factory):
+        repo_url = repo_target.get('url')
+        commit_hash = repo_target.get('commit')
+
+        temp_dir = tmp_path_factory.mktemp("repos")
+
+        name = repo_url.split('/')[-1]
+        target_dir = os.path.join(temp_dir, name)
         subprocess.run(
-            ["git", "checkout", commit_hash],
+            ["git", "clone", repo_url, target_dir]
+        )
+        if commit_hash:
+            subprocess.run(
+                ["git", "checkout", commit_hash],
+                cwd=target_dir
+            )
+        subprocess.run(
+            ["git", "clean", "-xdf"],
             cwd=target_dir
         )
-    subprocess.run(
-        ["git", "clean", "-xdf"],
-        cwd=target_dir
-    )
 
-    yield target_dir
+        return target_dir
+    return _setup_repo
 
+# cf. https://github.com/pytest-dev/pytest/issues/2424#issuecomment-333387206
+def inject_fixture(name, repo_target):
+    globals()[name] = generate_setup_repo_fixture(repo_target)
+
+def get_language(rule_path: Path) -> Optional[str]:
+    languages = config.keys()
+    intersect = set(list(str(rule_path).split(os.path.sep))).intersection(set(languages))
+    if len(intersect):
+        return intersect.pop()
+    return None
+
+for language in config.keys():
+    inject_fixture(f"setup_repo_{language}", config.get(language).pop()) # Only do one repo for now
+
+all_rule_paths = list(rule_paths()) # Consume generator to produce list
+
+# This is pure black magic, and I hate it. T_T
+# Doing this because I want to parametrize the test functions.
+# pytest's native parametrization only lets me do the cross product
+# of repo x rules.
+# So here's why I'm doing this:
+## 1) I need different fixtures for each rule set. With the cross product I have to run every rule on every repo.
+##      I also need the repo fixtures so that we don't waste setup and teardown time.
+## 2) I need different rule sets for each langauge.
+##      I really struggled to get parametrization or fixtures to work this way. All I really wanted was a way
+##      to select a subset of the rules based on the setup repo fixture. However, that proved nigh impossible.
+## 3) I want this to be configurable from the YAML file so that it's easy to add a new language.
+testing_template = """
 @pytest.mark.parametrize(
     "rule_path",
-    rule_paths(),
+    list(filter(lambda path: get_language(path) == "{language}", all_rule_paths)),
     ids=lambda path: os.path.relpath(str(path), os.getcwd())
 )
-def test_benchmark_on_repository(
-    setup_repo, rule_path, benchmark
-):
+def test_{language}_rules(setup_repo_{language}, rule_path, benchmark):
     benchmark(
-        subprocess.check_output,
+        subprocess.run,
         [
             "python3",
             "-m",
@@ -83,6 +104,11 @@ def test_benchmark_on_repository(
             "--strict",
             "--config",
             rule_path,
-            setup_repo 
+            setup_repo_{language}
         ],
     )
+"""
+
+for language in config.keys():
+    language = ''.join([c for c in language if c.isalpha()]) # Only include alphanum chars because I'm paranoid
+    exec(testing_template.format(language=language))
