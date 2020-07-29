95a406a3f00113316a81da8bbb0b3e31df2c2893
Artem Skoretskiy <artem.skoretskiy@gmail.com>
2020-07-29 18:59:11 +0200

    Bugfix/django blank URLField and ManyToManyField (#745)
    * Extend tests to cover all text fields
    
    * Allow `URLField` to have `blank=True` without `null=True`
    
    * Sort imports and wrap into 88 chars
    
    * Add ManyToManyField
    
    * Import module instead of importing all classes

--- python/django/correctness/nontext-field-must-set-null-true.py
+++ python/django/correctness/nontext-field-must-set-null-true.py
@@ -1,18 +1,30 @@
 from django.db import models
-from django.db.models import Model, CharField, IntegerField
 
-class FakeModel(Model):
+
+class FakeModel(models.Model):
     # ok
-    fieldOne = CharField(
+    fieldChar = models.CharField(
         max_length=200,
         blank=True)
+    # ok
+    fieldText = models.TextField(blank=True)
+    # ok
+    fieldSlug = models.SlugField(blank=True)
+    # ok
+    fieldEmail = models.EmailField(blank=True)
+    # ok
+    fieldURL = models.URLField(blank=True)
+    # ok
+    fieldUUID = models.UUIDField(blank=True)
+    # ok
+    fieldManyToMany = models.ManyToManyField("self", blank=True)
     # ruleid: nontext-field-must-set-null-true
-    fieldTwo = IntegerField(
+    fieldInt = models.IntegerField(
         blank=True,
         max_value=30
     )
     # ok
-    fieldThree = IntegerField(
+    fieldIntNull = models.IntegerField(
         null=True,
         blank=True,
         max_value=100
