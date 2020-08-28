413cc29ceddb9b6e97159e4c98a87df5ae276d26
colleend <23181670+colleend@users.noreply.github.com>
2020-08-28 17:18:21 -0500

    Http request (#816)
    * rules for ruby model attr accessible (brakeman)
    
    * used rule-id instead of ruleid
    
    * add check for attr-accessible use in models. May be more specific than brakeman check.
    
    * brakeman rules to prevent use of attr-protected.
    
    * brakeman rules to detected nested attributes
    
    * rule for nested attributes bypass brakeman
    
    * forgot to add ruleid
    
    * Delete nested-attributes.rb
    
    * Delete nested-attributes-bypass.rb
    
    * Delete nested-attributes-bypass.yaml
    
    * Delete nested-attributes.yaml
    
    * added more info in the yaml
    
    * rules to find http requests
    
    * updating names and message
    
    * Fix typo in CWE
    
    * updated to say https is ok
    
    * oops
    
    * ftp request checks
    
    * i cannot
    
    Co-authored-by: Colleen Dai <chai@Colleens-MBP.attlocal.net>
    Co-authored-by: minusworld <grayson@returntocorp.com>

---
+++ problem-based-packs/insecure-transport/js-node/http-request.js
@@ -0,0 +1,79 @@
+function bad_http() {
+    // ruleid: http-request
+    http.get('http://nodejs.org/dist/index.json', (res) => {
+    const { statusCode } = res;})
+
+    // ok: http-request
+    https.get('http://nodejs.org/dist/index.json', (res) => {
+    const { statusCode } = res;})
+
+    // ruleid: http-request
+    const options = {
+        port: 80,
+        hostname: 'www.google.com',
+        path: '/upload'
+    }
+
+    const req = http.request(options, (res) => {
+    console.log(`STATUS: ${res.statusCode}`);})
+
+    // ok: http-request
+    const options = {
+        port: 80,
+        hostname: 'www.google.com',
+        path: '/upload'
+    }
+
+    const req = https.request(options, (res) => {
+    console.log(`STATUS: ${res.statusCode}`);})
+
+    // ruleid: http-request
+    const options = new URL('http://abc:xyz@example.com');
+
+    const req = http.request(options, (res) => {
+    });
+
+    // ok: http-request
+    const options = new URL('http://abc:xyz@example.com');
+
+    const req = https.request(options, (res) => {
+    });
+};
+
+function more_bad_http() {
+    console.log("what");
+    // ok: http-request
+    const req = https.request('http://google.com', options, (res) => {
+    });
+
+    // ruleid: http-request
+    const req = http.request('http://google.com', options, (res) => {
+    });
+};
+
+function ok_http() {
+    // ok
+    const options = {
+        port: 80,
+        hostname: 'www.google.com',
+        path: '/upload',
+        protocol: 'https'
+    }
+
+    const req = http.request(options, (res) => {
+    console.log(`STATUS: ${res.statusCode}`);})
+
+    // ok
+    const options = {
+        port: 80,
+        hostname: 'www.google.com',
+        path: '/upload',
+        protocol: 'https'
+    }
+
+    const req = https.request(options, (res) => {
+    console.log(`STATUS: ${res.statusCode}`);})
+}
+
+
+
