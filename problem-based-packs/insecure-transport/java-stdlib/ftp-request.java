a9306534c88cf40dcf8c6d3b0c854406b743d7ee
Colleen Dai <chai@Colleens-MBP.attlocal.net>
2020-09-09 16:25:33 -0500

    oops

--- problem-based-packs/insecure-transport/java-stdlib/ftp-request.java
+++ problem-based-packs/insecure-transport/java-stdlib/ftp-request.java
@@ -2,13 +2,13 @@ class Bad {
     public static void badftp1() {
         String server = "www.yourserver.net";
         int port = 21;
-        // ruleid: ftprequest
+        // ruleid: ftp-request
         FTPClient ftpClient = new FTPClient();
         ftpClient.connect(server, port);
     }
 
     public static void badftp2() {
-        // ruleid: ftprequest
+        // ruleid: ftp-request
         URL url = new URL("ftp://user01:pass1234@ftp.foo.com/README.txt;type=i");
         URLConnection urlc = url.openConnection();
         InputStream is = urlc.getInputStream(); // To download
