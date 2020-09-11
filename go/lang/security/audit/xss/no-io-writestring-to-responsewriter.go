750d2a0aec323f294a1cfc513c9906c3184d0a89
minusworld <grayson@returntocorp.com>
2020-09-11 14:58:32 -0700

    Add rule detecting direclty writing to ResponseWriter using io.WriteString() (#851)
    * Add rule detecting direclty writing to ResponseWriter using io.WriteString()
    
    * Fix test file Sprintf call with proper syntax
    
    Co-authored-by: Slava Semushin <slava.semushin@gmail.com>
    
    Co-authored-by: Slava Semushin <slava.semushin@gmail.com>

---
+++ go/lang/security/audit/xss/no-io-writestring-to-responsewriter.go
@@ -0,0 +1,23 @@
+// cf. https://blogtitle.github.io/robn-go-security-pearls-cross-site-scripting-xss/
+
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+
+func isValid(token string) bool {
+	return true
+}
+
+func vulnerableHandler(w http.ResponseWriter, r *http.Request) {
+  r.ParseForm()
+  tok := r.FormValue("token")
+  if !isValid(tok) {
+    // ruleid:no-io-writestring-to-responsewriter
+    io.WriteString(w, fmt.Sprintf("Invalid token: %q", tok))
+  }
+  // ...
+}
