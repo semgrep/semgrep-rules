2f1658f06ebc8e09f5deed521d968842483e1dec
minusworld <grayson@returntocorp.com>
2020-09-17 15:13:22 -0700

    Secure defaults XSS rules for Express.js (#864)
    * Add Pug template rules for 'and attributes' and explicit unescape operators.
    
    * Add Pug rule for href variable
    
    * Add Express rule for directly writing to Response object
    
    * Add rule for detecting Pug templates with variables in script tags
    
    * Add rules for Mustache
    
    * Add rule for var in script tag for Mustache
    
    * More rules. Rules for var-in-href, var-in-script-tag, and new EJS rules.

---
+++ javascript/express/security/audit/xss/direct-response-write.js
@@ -0,0 +1,119 @@
+
+const express = require('express')
+const router = express.Router()
+
+router.get('/greeting', (req, res) => {
+    const { name } = req.query;
+    // ruleid: direct-response-write
+    res.send('<h1> Hello :' + name + "</h1>")
+})
+
+//template handle escaping 
+router.get('/greet-template', (req, res) => {
+    name = req.query.name
+    // ok: direct-response-write
+    res.render('index', { user_name: name });
+})
+
+module.exports = router
+
+
+app.get('/', function (req, res) {
+    var user = req.query.name;
+
+    msg = "Hi " + user
+    // ruleid: direct-response-write
+    res.send('Response</br>' + msg);
+});
+
+
+var msg = '';
+app.get('/3', function (req, res) {
+    var user = req.query.name;
+
+    msg = "Hi " + user
+    // ruleid: direct-response-write
+    res.send('Response</br>' + msg);
+});
+
+app.get('/2', function (req, res) {
+    var user = { user: req.query.name };
+    // ruleid: direct-response-write
+    res.send('Response</br>' + user.name);
+});
+
+app.get('/1', function (req, res) {
+    var user = req.query.name;
+    var msg = [];
+    msg.push(user);
+    // ruleid: direct-response-write
+    res.send('Response</br>' + msg[0]);
+});
+
+app.get('/4', function (req, res) {
+    var user = req.query.name;
+    var header = "<html>";
+    var msg = 'Hi ' + user;
+    var footer = "</html>";
+    var output = header + msg + footer;
+    // ruleid: direct-response-write
+    res.send(output);
+});
+
+
+
+
+
+var express = require('express');
+var app = express();
+app.get('/', function (req, res) {
+    var resp = req.query.name;
+    // ruleid: direct-response-write
+    res.send('Response</br>' + resp);
+});
+app.get('/3', function (req, res) {
+    var resp = req.query.name;
+    // ruleid: direct-response-write
+    res.write('Response</br>' + resp);
+});
+
+app.get('/3', function (req, res) {
+    var resp = req.foo;
+    var x = 1;
+    // ruleid: direct-response-write
+    res.write('Response</br>' + resp);
+});
+
+app.get('/xss', function (req, res) {
+    var html = "ASadad" + req.query.name + "Asdadads"
+    // ruleid: direct-response-write
+    res.write('Response</br>' + html);
+});
+app.get('/xss', function (req, res) {
+    // ruleid: direct-response-write
+    res.write('Response</br>' + req.query('doo'));
+});
+app.get('/xss', function (req, res) {
+    // ruleid: direct-response-write
+    res.write('Response</br>' + req.query.name);
+});
+
+app.get('/noxss', function (req, res) {
+    var resp = req.query.name;
+    // ok: direct-response-write
+    res.write('Response</br>');
+});
+
+app.get('/noxs2s', function (req, res) {
+    var resp = req.query.name;
+    // ruleid: direct-response-write
+    res.write('Response</br>' + foo);
+});
+
+app.get('/xss', function (req, res) {
+    var resp = req.query.name;
+    var html = "ASadad" + resp + "Asdadads"
+    // ruleid: direct-response-write
+    res.write('Response</br>' + html);
+});
+app.listen(8000);
