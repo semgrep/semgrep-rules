8392aebcc1a3d9bb8dea3d34cf55bf337b8b3525
grayson <grayson@returntocorp.com>
2020-09-29 11:22:04 -0700

    Add rule for potential command injection for 'sh' module in Python

---
+++ python/sh/security/string-concat.py
@@ -0,0 +1,18 @@
+import sh
+
+long = os.environ.get("LONG", "")
+# ruleid: string-concat
+sh.ls("-a" + long)
+
+# ok: string-concat
+sh.ls("-al")
+
+# ok: string-concat
+sh.semgrep("--config", "https://semgrep.dev/p/r2c-CI")
+
+confurl = os.environ.get("SEMGREP_CONFIG_URL", "")
+# ruleid: string-concat
+sh.semgrep("--config {}".format(confurl))
+
+# ruleid: string-concat
+sh.semgrep(f"--config {confurl}")
