3cb0da93a5e0b43f2dc5a36d9cacf98252c90c57
Colleen Dai <chai@Colleens-MacBook-Pro.local>
2020-10-05 18:15:33 -0500

    fixes

--- python/sqlalchemy/security/sqlalchemy-filter-sql-injection.py
+++ python/sqlalchemy/security/sqlalchemy-filter-sql-injection.py
@@ -1,33 +1,33 @@
+# ruleid: sqlalchemy-filter-sql-injection
 def bad1(var):
-    # ruleid: sqlalchemy-filter-sql-injection
     session.query(MyClass).filter("foo={}".format(var))
 
+# ruleid: sqlalchemy-filter-sql-injection
 def bad2(var):
-    # ruleid: sqlalchemy-filter-sql-injection
     query = cls.query.join(DeploymentPermission).filter(
         "oops{}".format(var)
     )
 
+# ruleid: sqlalchemy-filter-sql-injection
 def bad3(var):
-    # ruleid: sqlalchemy-filter-sql-injection
     query = cls.query.filter(
         "oops{}".format(var)
     )
 
+# ok: sqlalchemy-filter-sql-injection
 def ok1(cls, deployment: "Deployment", token_name: str) -> str:
-    # ok: sqlalchemy-filter-sql-injection
     query = cls.query(DeploymentPermission).filter(
         cls.id == DeploymentPermission.token_id,
     )
 
+# ok: sqlalchemy-filter-sql-injection
 def ok2(cls, deployment: "Deployment", token_name: str) -> str:
-    # ok: sqlalchemy-filter-sql-injection
     query = cls.query.join(DeploymentPermission).filter(
         cls == hello
     )
 
+# ok: sqlalchemy-filter-sql-injection
 def ok3(var):
-    # ok: sqlalchemy-filter-sql-injection
     query = cls.query.filter(
         "oops{}".format(var=3)
     )
--- python/sqlalchemy/security/sqlalchemy-orderby-sql-injection.py
+++ python/sqlalchemy/security/sqlalchemy-orderby-sql-injection.py
@@ -1,42 +1,42 @@
+# ruleid: sqlalchemy-orderby-sql-injection
 def bad1(var):
-    # ruleid: sqlalchemy-orderby-sql-injection
     session.query(MyClass).order_by("foo={}".format(var))
 
+# ruleid: sqlalchemy-orderby-sql-injection
 def bad2(var):
-    # ruleid: sqlalchemy-orderby-sql-injection
     query = cls.query.join(DeploymentPermission).order_by(
         "oops{}".format(var)
     )
 
+# ruleid: sqlalchemy-orderby-sql-injection
 def bad3(var):
-    # ruleid: sqlalchemy-orderby-sql-injection
     query = cls.query.order_by(
         "oops{}".format(var)
     )
 
+# ruleid: sqlalchemy-orderby-sql-injection
 def bad4(var):
-    # ruleid: sqlalchemy-orderby-sql-injection
     query = query.order_by("oops{}".format(var)).limit(limit)
 
+# ok: sqlalchemy-orderby-sql-injection
 def ok1(cls, deployment: "Deployment", token_name: str) -> str:
-    # ok: sqlalchemy-orderby-sql-injection
     query = cls.query(DeploymentPermission).order_by(
         cls.id == DeploymentPermission.token_id,
     )
 
+# ok: sqlalchemy-orderby-sql-injection
 def ok2(cls, deployment: "Deployment", token_name: str) -> str:
-    # ok: sqlalchemy-orderby-sql-injection
     query = cls.query.join(DeploymentPermission).order_by(
         cls == hello
     )
 
+# ok: sqlalchemy-orderby-sql-injection
 def ok3(var):
-    # ok: sqlalchemy-orderby-sql-injection
     query = cls.query.order_by(
         "oops{}".format(var=3)
     )
 
+# ok: sqlalchemy-orderby-sql-injection
 def ok4():
-    # ok: sqlalchemy-orderby-sql-injection
     query = query.order_by(desc(Scan.started_at)).limit(limit)
 
