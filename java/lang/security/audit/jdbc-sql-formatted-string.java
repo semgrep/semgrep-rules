712e863283f2fe1df04596e8bcfe6596f0fdc13a
Abdessamad TEMMAR <temmar.abdessamad@gmail.com>
2020-10-05 08:13:54 +0200

    Update jdbc-sql-formatted-string rule to cover more JdbcTemplate methods (#893)
    * Detect Unsafe deserialization of a JMS ObjectMessage
    
    * update jdbc-sql-formatted-string to cover all JdbcTemplate methods
    
    * Update insecure-jms-deserialization.java
    
    * Update jdbc-sql-formatted-string.java
    
    Co-authored-by: Vasilii Ermilov <inkz@xakep.ru>

--- java/lang/security/audit/jdbc-sql-formatted-string.java
+++ java/lang/security/audit/jdbc-sql-formatted-string.java
@@ -8,14 +8,14 @@ class TestClass {
         System.out.println("Hello");
     }
     
-    public void unsafe(String paramName) {
+    public void unsafe_jdbc_queryForObject_1(String paramName) {
         JdbcTemplate jdbc = new JdbcTemplate();
         System.out.println("Hello");
         // ruleid:jdbc-sql-formatted-string
         int count = jdbc.queryForObject("select count(*) from Users where name = '"+paramName+"'", Integer.class);
     }
     
-    public void unsafe2(String paramName) {
+    public void unsafe_jdbc_queryForObject_2(String paramName) {
         JdbcTemplate jdbc = new JdbcTemplate();
         System.out.println("Hello");
         // ruleid:jdbc-sql-formatted-string
@@ -23,6 +23,39 @@ class TestClass {
         int count = jdbc.queryForObject(query, Integer.class);
     }
 
+    public void unsafe_jdbc_queryForObject_3(String paramName) {
+        JdbcTemplate jdbc = new JdbcTemplate();
+        System.out.println("Hello");
+        // ruleid:jdbc-sql-formatted-string
+        StringBuilder query = new StringBuilder("select count(*) from Users");
+        query.append( "where name = '"+paramName+"'");
+        int count = jdbc.queryForObject(query, Integer.class);
+    }
+
+    public void unsafe_jdbc_queryForList_1(String paramName) {
+        JdbcTemplate jdbc = new JdbcTemplate();
+        System.out.println("Hello");
+        List<Users> users = new ArrayList<>();
+        // ruleid:jdbc-sql-formatted-string
+        String query = "select count(*) from Users where name = '"+paramName+"'";
+        List<Map<String, Object>> rows =  jdbc.queryForList(query);
+    }    
+
+    public void unsafe_jdbc_queryForList_2(String paramName) {
+        JdbcTemplate jdbc = new JdbcTemplate();
+        System.out.println("Hello");
+        List<Users> users = new ArrayList<>();
+        // ruleid:jdbc-sql-formatted-string
+        List<Map<String, Object>> rows =  jdbc.queryForList("select count(*) from Users where name = '"+paramName+"'");
+    }
+
+    public void unsafe_jdbc_update(String paramName, String paramSalary) {
+        JdbcTemplate jdbc = new JdbcTemplate();
+        System.out.println("Hello");
+        // ruleid:jdbc-sql-formatted-string
+        String updateQuery = "update Users set salary = '"+paramSalary+"' where name = '"+paramName+"'";
+        jdbc.update(updateQuery);
+    }
     public void safe(String paramName) {
         JdbcTemplate jdbc = new JdbcTemplate();
         // ok
