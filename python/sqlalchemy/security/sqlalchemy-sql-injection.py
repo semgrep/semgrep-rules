613703d2f40998baf214f8b298d39dbc13d3d2cf
Colleen Dai <chai@Colleens-MacBook-Pro.local>
2020-10-07 14:05:30 -0500

    mispells

--- python/sqlalchemy/security/sqlalchemy-sql-injection.py
+++ python/sqlalchemy/security/sqlalchemy-sql-injection.py
@@ -8,46 +8,46 @@ def bad2(var):
         "oops{}".format(var)
     )
 
-# ruleid: ssqlalchemy-sql-injection
+# ruleid: sqlalchemy-sql-injection
 def bad3(var):
     query = cls.query.group_by(
         "oops{}".format(var)
     )
 
-# ruleid: ssqlalchemy-sql-injection
+# ruleid: sqlalchemy-sql-injection
 def bad4(var):
     query = query.order_by("oops{}".format(var)).limit(limit)
 
-# ruleid: ssqlalchemy-sql-injection
+# ruleid: sqlalchemy-sql-injection
 def bad5(var):
     query = query.filter("oops{}".format(var)).limit(limit)
 
-# ok: ssqlalchemy-sql-injection
+# ok: sqlalchemy-sql-injection
 def ok1(cls, deployment: "Deployment", token_name: str) -> str:
     query = cls.query(DeploymentPermission).distinct(
         cls.id == DeploymentPermission.token_id,
     )
 
-# ok: ssqlalchemy-sql-injection
+# ok: sqlalchemy-sql-injection
 def ok2(cls, deployment: "Deployment", token_name: str) -> str:
     query = cls.query.join(DeploymentPermission).having(
         cls == hello
     )
 
-# ok: ssqlalchemy-sql-injection
+# ok: sqlalchemy-sql-injection
 def ok3(var):
     query = cls.query.group_by(
         var=3)
 
-# ok: ssqlalchemy-sql-injection
+# ok: sqlalchemy-sql-injection
 def ok4(var):
     query = query.order_by(desc(Scan.started_at)).limit(limit)
 
-# ok: ssqlalchemy-sql-injection
+# ok: sqlalchemy-sql-injection
 def ok5(var):
     query = query.filter(var==5).limit(limit)
 
-# ok: ssqlalchemy-sql-injection
+# ok: sqlalchemy-sql-injection
 def ok6(var):
     query = query.filter("oops{}".bindparams(var)).limit(limit)
 
