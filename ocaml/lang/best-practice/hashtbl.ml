caecdfd13fc6e476438b3e516452f4eeba28007c
pad <yoann.padioleau@gmail.com>
2020-10-13 22:47:56 +0200

    addressing martin's comments

---
+++ ocaml/lang/best-practice/hashtbl.ml
@@ -0,0 +1,13 @@
+let test1 xs =
+  (* ruleid:hashtbl-find-outside-try *)
+  if Hashtbl.find h 1
+  then 1
+  else 2
+
+let test2 xs =
+ (* ok *)
+ try 
+   if Hashtbl.find h 1
+   then 1
+   else 2
+ with Not_found -> 3
--- ocaml/lang/correctness/physical_vs_structural.ml
+++ ocaml/lang/correctness/physical_vs_structural.ml
@@ -3,3 +3,9 @@ let test xs ys =
   if xs != ys
   then 1
   else 2
+
+let test2 xs ys = 
+  (* ruleid:physical-equal *)
+  if xs == ys
+  then 1
+  else 2
