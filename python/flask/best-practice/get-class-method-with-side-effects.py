3b254ef2792d67ad9384aca1e8565d7df0767db9
Kurt Boberg <kboberg@chegg.com>
2020-10-14 13:34:25 -0700

    fix broken tests (language tags are important, yo)

--- python/flask/best-practice/get-class-method-with-side-effects.py
+++ python/flask/best-practice/get-class-method-with-side-effects.py
@@ -2,19 +2,19 @@ import flask
 
 class SomeClass:
     #violation - CRUD operation
+    # ruleid: flask-class-method-get-side-effects
     def get(self):
-        # ruleid: flask-class-method-get-side-effects
         createRecord(someVar)
 
     #violation - CRUD operation
+    # ruleid: flask-class-method-get-side-effects
     def get(self, arg1):
         print("foo")
-        # ruleid: flask-class-method-get-side-effects
         var = updateBar(somearg)
 
+    # ruleid: flask-class-method-get-side-effects
     def get(self,arg1,arg2):
         someFunction()
-        # ruleid: flask-class-method-get-side-effects
         DeleteRecord(arg2)
         
 class OtherClass:
