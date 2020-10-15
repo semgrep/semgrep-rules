c355c99830ca2b956504ee7b497731e6002cc99a
colleend <23181670+colleend@users.noreply.github.com>
2020-10-15 13:14:41 -0500

    Add examples (#922)
    * no js, should only be javascript
    
    * updating to javascript
    
    * one rule
    
    * python 3.7 examples
    
    * return-in-init refactoring
    
    * necessary renames
    
    * fixing return-in-init
    
    * add neg test case
    
    * fixes
    
    Co-authored-by: Colleen Dai <chai@Colleens-MBP.attlocal.net>
    Co-authored-by: Colleen Dai <chai@Colleens-MacBook-Pro.local>

--- python/lang/compatibility/python37.py
+++ python/lang/compatibility/python37.py
@@ -1,7 +1,56 @@
 import os
 
+# ruleid: python37-compatibility-importlib2
+import importlib.resources
+
+# ruleid: python37-compatibility-importlib3
+import importlib.abc.ResourceReader
+
 # ruleid:python37-compatability-os-module
 os.pwrite('a')
+
+# ruleid:python37-compatibility-importlib
+importlib.source_hash()
+
+# ruleid: python37-compatibility-httpconn
+http.client.HTTPConnection(blocksize=5,var, etc)
+
+# ruleid: python37-compatibility-textiowrapper
+TextIOWrapper.reconfigure(var)
+
+# ruleid: python37-compatibility-ipv6network1
+ipaddress.IPv6Network.subnet_of(ip)
+
+# ruleid: python37-compatibility-ipv6network2
+ipaddress.IPv6Network.supernet_of(ip)
+
+# ruleid: python37-compatibility-ipv4network1
+ipaddress.IPv4Network.subnet_of(ip)
+
+# ruleid: python37-compatibility-ipv4network2
+ipaddress.IPv4Network.supernet_of(ip)
+
+# ruleid: python37-compatibility-locale1
+locale.format_string(monetary=var, extravars)
+
+# ruleid: python37-compatibility-math1
+math.remainder(24, 3)
+
+# ruleid: python37-compatibility-multiprocess1
+multiprocessing.Process.close()
+
+# ruleid: python37-compatibility-multiprocess2
+multiprocessing.Process.kill()
+
+# ruleid: python37-compatibility-os1
+os.preadv(var)
+
+# ruleid: python37-compatability-os-module
+os.pwrite('c')
+
+# ruleid: python37-compatibility-pdb
+pdb.set_trace(header=header, stuffa)
+
 
 if hasattr(os, 'pwrite'):
     # OK
@@ -15,3 +64,4 @@ if hasattr(os, 'pwritev'):
 
 # ruleid:python37-compatibility-os2-ok2
 os.pwritev('b')
+
