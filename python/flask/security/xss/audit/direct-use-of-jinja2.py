823b82922a075ab77e54c04eae675662cf8e4587
minusworld <grayson@returntocorp.com>
2020-10-19 14:43:35 -0700

    Fix test cases

--- python/flask/security/xss/audit/direct-use-of-jinja2.py
+++ python/flask/security/xss/audit/direct-use-of-jinja2.py
@@ -1,7 +1,6 @@
 # ruleid: direct-use-of-jinja2
 import jinja2
 
-# ruleid: direct-use-of-jinja2
 template = jinja2.Template("""
 <html>
 <body>
@@ -15,10 +14,13 @@ rendered = template.render(body=input())
 
 # ruleid: direct-use-of-jinja2
 from jinja2 import Environment, PackageLoader, select_autoescape
+# ruleid: direct-use-of-jinja2
 env = Environment(
     loader=PackageLoader('yourapplication', 'templates'),
     autoescape=select_autoescape(['html', 'xml'])
 )
 
-# ruleid: direct-use-of-jinja2
 t = env.get_template('mytemplate.html')
+
+# ruleid: direct-use-of-jinja2
+rendered2 = t.render(body=input())
