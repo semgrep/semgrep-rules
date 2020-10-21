57befbfcfe924e8ec3c9da535a61d694d72294ae
Vasilii Ermilov <inkz@xakep.ru>
2020-10-22 04:22:02 +1000

    Client-side XSS JavaScript rules (#931)
    * add raw-html-concat rule
    
    * update raw-html-concat test
    
    * update raw-html-concat rule
    
    * update raw-html-concat rule
    
    * add jquery-insecure-selector rule
    
    * move jquery-insecure-selector rule to 'jquery' folder
    
    * move jquery-insecure-selector rule to 'jquery/security/audit' folder
    
    * add avoid-v-html rule

---
+++ javascript/vue/security/audit/xss/avoid-v-html.vue
@@ -0,0 +1,6 @@
+<div>
+  <!-- ruleid: avoid-v-html -->
+  <span dir="auto" class="markdown" v-html="entry.post"></span>
+  <!-- ok: avoid-v-html -->
+  <span dir="auto" class="markdown">{{entry.post}}</span>
+</div>
