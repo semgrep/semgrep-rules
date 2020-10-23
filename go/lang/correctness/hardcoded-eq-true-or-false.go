204f245090c0419e0d5a0c3cbd6b2e637c2d061f
AzureUmbra <geeteepee13@gmail.com>
2020-10-23 09:52:00 -0500

    split 'useless-eqeq' rules for go

--- go/lang/correctness/hardcoded-eq-true-or-false.go
+++ go/lang/correctness/hardcoded-eq-true-or-false.go
@@ -3,11 +3,6 @@ import "fmt"
 
 func main() {
     fmt.Println("hello world")
-    var y = 1;
-    // ruleid:eqeq-is-bad
-    fmt.Println(y == y)
-    // ok:eqeq-is-bad
-    assert(y == y)
 
     // ruleid:hardcoded-eq-true-or-false
     if (false) {
