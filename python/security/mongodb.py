f2227a22d6667c82563e50c4b78047250c654216
Isaac Evans <isaac.n.evans@gmail.com>
2020-10-26 21:25:43 +0000

    add python/security/mongodb.py

---
+++ python/security/mongodb.py
@@ -0,0 +1,17 @@
+from pymongo import MongoClient
+
+# ok:
+client = MongoClient('example.com',
+                     username='user',
+                     password='password',
+                     authSource='the_database',
+                     authMechanism='SCRAM-SHA-256')
+# ok:
+client = MongoClient('example.com',
+                     username='user',
+                     password='password',
+                     authSource='the_database',
+                     authMechanism='SCRAM-SHA-1')
+
+# ruleid:mongo-client-bad-auth
+client = MongoClient('example.com', username='user', password='password', authSource='the_database', authMechanism='MONGODB-CR')
