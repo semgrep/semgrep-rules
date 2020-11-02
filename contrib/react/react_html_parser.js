ab82eb9765b8598ebd028ef970133c28fd802995
Sebastien gioria <seb@gioria.org>
2020-11-02 09:58:38 +0100

    Add first react rules

---
+++ contrib/react/react_html_parser.js
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import ReactHtmlParser from 'react-html-parser';
+
+
+renderContent() {
+    let content = this.props.content;
+    if (!content || (typeof content === 'string' && !content.length)) {
+        if (this.props.children) {
+            return <span>{this.props.children}</span>;
+        }
+        return null;
+    }
+    return <span>{ReactHtmlParser(content)}</span>;
+}
