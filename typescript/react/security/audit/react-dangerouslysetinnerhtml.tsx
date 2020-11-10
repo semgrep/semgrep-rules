cb0b03a563889f125c15b29d67458e70edd5b053
Vasilii Ermilov <inkz@xakep.ru>
2020-11-10 15:20:05 +1000

    Update react-dangerouslysetinnerhtml.tsx

--- typescript/react/security/audit/react-dangerouslysetinnerhtml.tsx
+++ typescript/react/security/audit/react-dangerouslysetinnerhtml.tsx
@@ -3,28 +3,28 @@ function createMarkup() {
 }
 
 function TestComponent1() {
-	// ruleid:react-dangerouslysetinnerhtml
+    // ruleid:react-dangerouslysetinnerhtml
   return <div dangerouslySetInnerHTML={createMarkup()} />;
 }
 
 function TestComponent2() {
-	// ruleid:react-dangerouslysetinnerhtml
+    // ruleid:react-dangerouslysetinnerhtml
   return <li className={"foobar"} dangerouslySetInnerHTML={createMarkup()} />;
 }
 
 function TestComponent3() {
-	// ruleid:react-dangerouslysetinnerhtml
-	let params = {smth: 'test123', dangerouslySetInnerHTML: {__html: 'foobar'}};
-	return React.createElement('div', params);
+    // ruleid:react-dangerouslysetinnerhtml
+    let params = {smth: 'test123', dangerouslySetInnerHTML: {__html: 'foobar'}};
+    return React.createElement('div', params);
 }
 
 function OkComponent1() {
-	// ok:react-dangerouslysetinnerhtml
+    // ok:react-dangerouslysetinnerhtml
   return <li class="foobar" selected={true} />;
 }
 
 function OkComponent2() {
-	// ok:react-dangerouslysetinnerhtml
-	let params = {smth: "test123", style: {color: 'red'}};
-	return React.createElement('div', params);
+    // ok:react-dangerouslysetinnerhtml
+    let params = {smth: "test123", style: {color: 'red'}};
+    return React.createElement('div', params);
 }
