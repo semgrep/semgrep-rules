77297932008ddb586919cb5f119c5b568152e312
Vasilii Ermilov <inkz@xakep.ru>
2020-11-12 21:26:56 +1000

    add typescript/react rules

---
+++ typescript/react/security/audit/react-href-var.tsx
@@ -0,0 +1,7 @@
+
+const a = <a className={"foobar"} href={"javascript:"}></a>;
+const b = <a className={"foobar"} href="javascript:"></a>;
+
+let x = <a className={"foobar"} href={input}></a>;
+
+let zzz = <Foo className={"foobar"} href={input} />;
