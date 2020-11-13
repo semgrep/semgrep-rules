b1e8b5350d2688705ee84db90eef75be9fc9261e
Vasilii Ermilov <inkz@xakep.ru>
2020-11-13 18:58:44 +1000

    add typescript/react rules

---
+++ typescript/react/best-practice/react-props-in-state.tsx
@@ -0,0 +1,73 @@
+class Test1 extends React.Component {
+  state = {
+        foo: 'bar',
+// ruleid: react-props-in-state
+        color: this.props.color,
+        one: 1
+  };
+  render() {
+    const { color } = this.state;
+    return (
+      <button className={'Button-' + color}>
+        {this.props.children}
+      </button>
+    );
+  }
+}
+
+class Test2 extends React.Component {
+  state = {
+// ruleid: react-props-in-state
+    textColor: slowlyCalculateTextColor(this.props.color)
+  };
+  render() {
+    return (
+      <button className={
+        'Button-' + this.props.color +
+        ' Button-text-' + this.state.textColor
+      }>
+        {this.props.children}
+      </button>
+    );
+  }
+}
+
+function Test3({ text }) {
+// ruleid: react-props-in-state
+  const [buttonText] = useState(text)
+  return <button>{buttonText}</button>
+}
+
+function Test4(props) {
+// ruleid: react-props-in-state
+  const [formattedText] = useState(() => slowlyFormatText(props.text))
+  return <button>{formattedText}</button>
+}
+
+function OkTest1({ color, children }) {
+  const textColor = useMemo(
+// ok: react-props-in-state
+    () => slowlyCalculateTextColor(color),
+    [color]
+  );
+  return (
+    <button className={'Button-' + color + ' Button-text-' + textColor}>
+      {children}
+    </button>
+  );
+}
+
+class OkTest2 extends React.PureComponent {
+  render() {
+// ok: react-props-in-state
+    const textColor = slowlyCalculateTextColor(this.props.color);
+    return (
+      <button className={
+        'Button-' + this.props.color +
+        ' Button-text-' + textColor
+      }>
+        {this.props.children}
+      </button>
+    );
+  }
+}
