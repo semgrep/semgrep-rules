16c3fcb4d6c7033c7d3c3c2898873c287097c4a3
Vasilii Ermilov <inkz@xakep.ru>
2020-11-16 16:25:12 +1000

    add react-props-injection rule

---
+++ typescript/react/security/audit/react-props-injection.tsx
@@ -0,0 +1,45 @@
+import something from "foobar";
+
+const userInput = JSON.parse(storedValue);
+
+function test1() {
+  // ruleid: react-props-injection
+  return <div className={"foo"} {...userInput} />;
+}
+
+function test2() {
+  // ruleid: react-props-injection
+  return <div className={"foo"} {...doSmth(userInput)} />;
+}
+
+function test3(input) {
+  // ruleid: react-props-injection
+  return <div className={"foo"} {...input} />;
+}
+
+function test4() {
+  const input = JSON.parse(storedValue);
+  // ruleid: react-props-injection
+  return React.createElement("span", input);
+}
+
+function test5(input) {
+  // ruleid: react-props-injection
+  return React.createElement("span", doSmth(input));
+}
+
+function test6() {
+  // TODO: not currently supported
+  return <div className={"foo"} {...something} />;
+}
+
+function okTest1(input) {
+  // ok: react-props-injection
+  return <div className={"foo"} prop={input} />;
+}
+
+function okTest2() {
+  const input = JSON.parse(storedValue);
+  // ok: react-props-injection
+  return React.createElement("span", {attr: input.attr});
+}
