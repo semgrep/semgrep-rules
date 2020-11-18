d641eeab642ed7f2f85e44beab3f16ea3a2ee29b
Vasilii Ermilov <inkz@xakep.ru>
2020-11-18 14:30:16 +1000

    update react-href-var rule

--- typescript/react/security/audit/react-href-var.tsx
+++ typescript/react/security/audit/react-href-var.tsx
@@ -8,3 +8,14 @@ let x = <a className={"foobar"} href={input}></a>;
 
 // ruleid: react-href-var
 let zzz = <Foo className={"foobar"} href={input} />;
+
+function test1(input) {
+// ruleid: react-href-var
+  const params = {href: input};
+  return React.createElement("a", params);
+}
+
+function okTest1() {
+// ok: react-href-var
+  return React.createElement("a", {href: "https://www.example.com"});
+}
