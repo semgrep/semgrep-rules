55b374ce99ad5db1f9993b1406a3a2223912d762
Vasilii Ermilov <inkz@xakep.ru>
2020-11-23 23:53:39 +1000

    Adding more react rules (#1015)
    * add react-jwt-decoded-property rule
    
    * add react-contolled-component-password rule
    
    * fix react-jwt-decoded-property tetst
    
    * add react-markdown-insecure-html rule
    
    * add javascript lang to react rules
    
    * add react-insecure-request rule
    
    * add react-router-redirect rule
    
    * add react-styled-components-injection rule

---
+++ typescript/react/security/react-insecure-request.tsx
@@ -0,0 +1,36 @@
+import axios from 'axios';
+
+// ruleid: react-insecure-request
+fetch('http://www.example.com', 'GET', {})
+
+// ruleid: react-insecure-request
+let addr = "http://www.example.com"
+fetch(addr, 'POST', {})
+
+// ruleid: react-insecure-request
+axios.get('http://www.example.com');
+
+// ruleid: react-insecure-request
+const options = {
+  method: 'POST',
+  headers: { 'content-type': 'application/x-www-form-urlencoded' },
+  data: qs.stringify(data),
+  url: 'http://www.example.com',
+};
+axios(options);
+
+// ruleid: react-insecure-request
+axios({ method: 'POST', url: 'http://www.example.com' });
+
+// ok: react-insecure-request
+fetch('https://www.example.com', 'GET', {})
+
+// ok: react-insecure-request
+axios.get('https://www.example.com');
+
+// ok: react-insecure-request
+const options = {
+  method: 'POST',
+  url: 'https://www.example.com',
+};
+axios(options);
