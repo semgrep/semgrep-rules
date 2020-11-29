e862292e99267be8d35583df9797cfbe63d5e155
hunt3rkillerz <rod@MSI.localdomain>
2020-11-29 16:43:02 +1100

    fixing styles to pass pre-commit linting test

--- terraform/lang/security/ebs-unencrypted-volume.tf
+++ terraform/lang/security/ebs-unencrypted-volume.tf
@@ -1,7 +1,7 @@
 resource "aws_ebs_volume" "web_host_storage" {
   # unencrypted volume
   availability_zone = "ap-southeast-2"
-  encrypted         = true  # Setting this causes the volume to be recreated on apply 
+  encrypted         = false  # Setting this causes the volume to be recreated on apply 
   size = 1
   tags = {
     Name = "abcd-ebs"
