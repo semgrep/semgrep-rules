938cc49c520387e6cfb7d7560bcc6e00b9cb1895
Vasilii Ermilov <inkz@xakep.ru>
2020-12-02 13:54:49 +1000

    update react-missing-noreferrer rule

--- typescript/react/security/audit/react-missing-noreferrer.tsx
+++ typescript/react/security/audit/react-missing-noreferrer.tsx
@@ -22,6 +22,9 @@ var OkTest2 = <a target="_blank" rel="noreferrer" href="http://example.com"></a>
 // ok: react-missing-noreferrer
 var OkTest3 = <a target="_blank" rel="noopener noreferrer" href="http://example.com"></a>
 
+// ok: react-missing-noreferrer
+var OkTest4 = <a rel='noopener' target="_blank" href="/example.com"></a>
+
 function TestComponent1() {
 // ruleid: react-missing-noreferrer
   let params = {target: '_blank', href: 'http://example.com/'};
@@ -41,4 +44,9 @@ function TestComponent3() {
 function OkComponent1() {
 // ok: react-missing-noreferrer
   return React.createElement('a', {target: '_blank', href: 'http://example.com/', rel: "noopener noreferrer"});
+}
+
+function OkComponent2() {
+// ok: react-missing-noreferrer
+  return React.createElement('a', {target: '_blank', href: '/test', rel: 'noopener'});
 }
