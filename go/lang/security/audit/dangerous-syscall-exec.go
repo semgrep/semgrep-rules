f8a1b0640ca334f772bcd9a1fdcd8f0c9ef4a1dd
Vasilii Ermilov <inkz@xakep.ru>
2021-03-17 22:23:32 +1000

    add dangerous-syscall-exec rule

---
+++ go/lang/security/audit/dangerous-syscall-exec.go
@@ -0,0 +1,61 @@
+package main
+
+import "syscall"
+import "os"
+import "os/exec"
+
+func test1(userInput string) {
+
+    binary, lookErr := exec.LookPath(userInput)
+    if lookErr != nil {
+        panic(lookErr)
+    }
+
+    args := []string{"ls", "-a", "-l", "-h"}
+
+    env := os.Environ()
+
+    // ruleid:dangerous-syscall-exec
+    execErr := syscall.Exec(binary, args, env)
+    if execErr != nil {
+        panic(execErr)
+    }
+}
+
+
+func test1(userInput string) {
+
+    binary, lookErr := exec.LookPath("sh")
+    if lookErr != nil {
+        panic(lookErr)
+    }
+
+    args := []string{userInput, "-a", "-l", "-h"}
+
+    env := os.Environ()
+
+    // ruleid:dangerous-syscall-exec
+    execErr := syscall.Exec(binary, args, env)
+    if execErr != nil {
+        panic(execErr)
+    }
+}
+
+
+func okTest1(userInput string) {
+
+    binary, lookErr := exec.LookPath("ls")
+    if lookErr != nil {
+        panic(lookErr)
+    }
+
+    args := []string{"ls", "-a", "-l", "-h"}
+
+    env := os.Environ()
+
+    // ok:dangerous-syscall-exec
+    execErr := syscall.Exec(binary, args, env)
+    if execErr != nil {
+        panic(execErr)
+    }
+}
