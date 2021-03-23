73430e9ee324e421094964805af318d816a26fae
K <z@kwi.li>
2021-03-22 17:02:04 -0700

    Update dict-modify-iterating rule
    Previously, this would raise on loops over `dictVal.keys()` and
    `dictVal.items()` which doesn't actually raise a `RuntimeError` in my
    testing.

--- python/lang/correctness/dict-modify-iterating.py
+++ python/lang/correctness/dict-modify-iterating.py
@@ -1,19 +1,4 @@
-d = {'a': 1, 'b': 2}
+d = {"a": 1, "b": 2}
 # ruleid:dict-del-while-iterate
-for k,v in d.items():
+for k, v in d:
     del d[k]
-
-d = {'a': 1, 'b': 2}
-# ruleid:dict-del-while-iterate
-for k in d.keys():
-    del d[k]
-
-# ruleid:dict-del-while-iterate
-for k in d.keys():
-    print(d[k])
-    del d[k]
-
-# ok:dict-del-while-iterate
-for k in d.keys():
-    print(d[k])
-    x = d[k]
