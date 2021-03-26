53642eb7902139a8be604331f9ae5c35763258c3
grayson <grayson@returntocorp.com>
2021-03-26 11:36:30 -0500

    Pre-commit

--- java/jax-rs/security/insecure-resteasy.java
+++ java/jax-rs/security/insecure-resteasy.java
@@ -6,38 +6,38 @@ import javax.ws.rs.core.*;
 
 @Path("/")
 public class PoC_resource {
-	@POST
-	@Path("/concat")
-	@Produces(MediaType.APPLICATION_JSON)
-	// ruleid: insecure-resteasy-deserialization
-	@Consumes({"*/*"})
-	public Map<String, String> doConcat(Pair pair) {
-		HashMap<String, String> result = new HashMap<String, String>();
-		result.put("Result", pair.getP1() + pair.getP2());
+    @POST
+    @Path("/concat")
+    @Produces(MediaType.APPLICATION_JSON)
+    // ruleid: insecure-resteasy-deserialization
+    @Consumes({"*/*"})
+    public Map<String, String> doConcat(Pair pair) {
+        HashMap<String, String> result = new HashMap<String, String>();
+        result.put("Result", pair.getP1() + pair.getP2());
 
-		return result;
-	}
-	// ruleid:default-resteasy-provider-abuse
-	@POST
-	@Path("/vulnerable")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Map<String, String> doConcat(Pair pair) {
-		HashMap<String, String> result = new HashMap<String, String>();
-		result.put("Result", pair.getP1() + pair.getP2());
+        return result;
+    }
+    // ruleid:default-resteasy-provider-abuse
+    @POST
+    @Path("/vulnerable")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Map<String, String> doConcat(Pair pair) {
+        HashMap<String, String> result = new HashMap<String, String>();
+        result.put("Result", pair.getP1() + pair.getP2());
 
-		return result;
-	}
+        return result;
+    }
 
-	@POST
-	@Path("/count")
-	@Produces(MediaType.APPLICATION_JSON)
-	// ok: insecure-resteasy-deserialization
-	@Consumes(MediaType.APPLICATION_JSON)
-	public Map<String, Integer> doCount(ArrayList<Object> elements) {
-		HashMap<String, Integer> result = new HashMap<String, Integer>();
-		result.put("Result", elements.size());
+    @POST
+    @Path("/count")
+    @Produces(MediaType.APPLICATION_JSON)
+    // ok: insecure-resteasy-deserialization
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Map<String, Integer> doCount(ArrayList<Object> elements) {
+        HashMap<String, Integer> result = new HashMap<String, Integer>();
+        result.put("Result", elements.size());
 
-		return result;
-	}
-	
+        return result;
+    }
+
 }
