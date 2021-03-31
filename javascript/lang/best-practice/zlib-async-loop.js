d2779fac4d318548f1c569ea1691055ca85b5bad
Matt Schwager <matt@returntocorp.com>
2021-03-31 11:44:56 -0600

    Add JS rule for zlib async in a loop

---
+++ javascript/lang/best-practice/zlib-async-loop.js
@@ -0,0 +1,16 @@
+const zlib = require('zlib');
+
+const payload = Buffer.from('This is some data');
+
+for (let i = 0; i < 30000; ++i) {
+    // ruleid: zlib-async-loop
+    zlib.deflate(payload, (err, buffer) => {});
+}
+
+for (let i = 0; i < 30000; ++i) {
+    // ok: zlib-async-loop
+    zlib.deflateSync(payload);
+}
+
+// ok: zlib-async-loop
+zlib.deflate(payload, (err, buffer) => {});
