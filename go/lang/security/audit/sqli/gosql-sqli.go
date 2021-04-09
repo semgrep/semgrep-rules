efbbb20cabfdf151aa64a6922ab968f8295f1ca6
Colleen Dai <colleen@returntocorp.com>
2021-04-09 16:35:51 -0700

    editing to decrease fps

--- go/lang/security/audit/sqli/gosql-sqli.go
+++ go/lang/security/audit/sqli/gosql-sqli.go
@@ -1,75 +1,72 @@
-package main
+ipackage main
 
 import "database/sql"
 import "fmt"
 
-func setup() {
+func bad1() {
     db, err := sql.Open("mysql", "theUser:thePassword@/theDbName")
     if err != nil {
         panic(err)
     }
-}
-
-func bad1() {
     query = "SELECT name FROM users WHERE age=" + req.FormValue("age")
     // ruleid: gosql-sqli
     db.Query(query)
 }
 
-func bad2() {
+func bad2(db *sql.DB) {
     query = "SELECT name FROM users WHERE age="
     query += req.FormValue("age")
     // ruleid: gosql-sqli
     db.QueryRow(query)
 }
 
-func bad3() {
+func bad3(db *sql.DB) {
     query = fmt.Sprintf("SELECT * FROM users WHERE email='%s';", email)
     // ruleid: gosql-sqli
     db.Exec(query)
 }
 
-func bad4() {
+func bad4(db *sql.DB) {
     // ruleid: gosql-sqli
     db.Exec("SELECT name FROM users WHERE age=" + req.FormValue("age"))
 }
 
-func bad5() {
+func bad5(db *sql.DB) {
     // ruleid: gosql-sqli
     db.Exec(fmt.Sprintf("SELECT * FROM users WHERE email='%s';", email))
 }
 
-func ok1() {
+func ok1(db *sql.DB) {
     query = fmt.Sprintf("SELECT * FROM users WHERE email=hello;")
     // ok: gosql-sqli
     db.Exec(query)
 }
 
-func ok2() {
+func ok2(db *sql.DB) {
     query = "SELECT name FROM users WHERE age=" + "3"
     // ok: gosql-sqli
     db.Query(query)
 }
 
-func ok3() {
+func ok3(db *sql.DB) {
     query = "SELECT name FROM users WHERE age="
     query += "3"
     // ok: gosql-sqli
     db.Query(query)
 }
 
-func ok4() {
+func ok4(db *sql.DB) {
     // ok: gosql-sqli
     db.Exec("INSERT INTO users(name, email) VALUES($1, $2)",
   "Jon Calhoun", "jon@calhoun.io")
 }
 
-func ok5() {
+func ok5(db *sql.DB) {
     // ok: gosql-sqli
     db.Exec("SELECT name FROM users WHERE age=" + "3")
 }
 
-func ok6() {
+func ok6(db *sql.DB) {
     // ok: gosql-sqli
     db.Exec(fmt.Sprintf("SELECT * FROM users WHERE email=hello;"))
 }
