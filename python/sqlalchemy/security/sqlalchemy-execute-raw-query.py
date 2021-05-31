e655730dacace28b5fceb4fee588a71f14165b79
0xdbe <23268141+0xdbe@users.noreply.github.com>
2021-05-31 14:04:52 +0200

    sqlalchemy-execute-raw-query: fix rule to take params into consideration

--- python/sqlalchemy/security/sqlalchemy-execute-raw-query.py
+++ python/sqlalchemy/security/sqlalchemy-execute-raw-query.py
@@ -6,7 +6,7 @@
 engine = create_engine('postgresql://user@localhost/database')
 echo("database connexion: ok")
 # ruleid: sqlalchemy-execute-raw-query
-engine.execute("INSERT INTO person (name) VALUES ('" + name + "')")
+engine.execute("INSERT INTO person (name) VALUES ('" + name + "')", multi=False)
 
 # String concatenation using + operator
 engine = create_engine('postgresql://user@localhost/database')
@@ -193,3 +193,34 @@ connection.execute(stmt, name='Frodon Sacquet')
 engine = create_engine('postgresql://user@localhost/database')
 query = select(literal_column("users.fullname", String) + ', ' + literal_column("addresses.email_address").label("title")).where(and_(literal_column("users.id") == literal_column("addresses.user_id"), text("users.name BETWEEN 'm' AND 'z'"), text("(addresses.email_address LIKE :x OR addresses.email_address LIKE :y)"))).select_from(table('users')).select_from(table('addresses'))
 conn.execute(query, {"x":"%@aol.com", "y":"%@msn.com"}).fetchall()
+
+
+# SQL Composition using SQL Expression
+# ok: sqlalchemy-execute-raw-query
+connection_string = 'sqlite:///db.sqlite'
+engine = create_engine(connection_string, echo=True)
+with engine.connect() as connection:
+  meta = MetaData()
+  meta.reflect(bind=connection)
+  product_table = meta.tables['product']
+  stmt = (
+    select(product_table)
+    .where(product_table.columns[field_name] == value_name)
+  )
+  result = connection.execute(stmt)
+
+# Insert multi data record using SQL Expression
+# ok: sqlalchemy-execute-raw-query
+connection_string = 'sqlite:///db.sqlite'
+engine = create_engine(connection_string, echo=True)
+with engine.connect() as connection:
+  meta = MetaData()
+  meta.reflect(bind=connection)
+  product_table = meta.tables['product']
+  stmt = insert(product_table)
+  values = [
+      {field_name: 'hazelnut', field_price: 5},
+      {field_name: 'banana', field_price: 8}
+  ]
+  print(stmt)
+  connection.execute(stmt, values)
