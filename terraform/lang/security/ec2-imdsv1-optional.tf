3eda53622923414d5085da2c27c4074bc6c87f44
x64-latacora <xavier@latacora.com>
2021-06-01 20:30:25 +0200

    Prettify

--- terraform/lang/security/ec2-imdsv1-optional.tf
+++ terraform/lang/security/ec2-imdsv1-optional.tf
@@ -1,8 +1,8 @@
 # ruleid: ec2-imdsv1-optional
 resource "aws_instance" "test-instance" {
-  ami               = "ami-0d5eff06f840b45e9"  
-  metadata_options {      
-      http_endpoint = "enabled"
-      http_tokens   = "optional"
- }
+  ami = "ami-0d5eff06f840b45e9"
+  metadata_options {
+    http_endpoint = "enabled"
+    http_tokens = "optional"
+  }
 }
