c95dc07296c44acda5219ae9ed5ef10ff0a72531
Vasilii Ermilov <inkz@xakep.ru>
2021-07-15 19:57:46 +1000

    update hardcoded-jwt-secret rule

--- javascript/jsonwebtoken/security/jwt-hardcode.js
+++ javascript/jsonwebtoken/security/jwt-hardcode.js
@@ -1,32 +1,29 @@
 "use strict";
 
 const config = require('./config')
+const jsonwt = require('jsonwebtoken')
 
 function example1() {
-  // ruleid: hardcoded-jwt-secret
-  const jsonwt = require('jsonwebtoken')
   const payload = {foo: 'bar'}
   const secret = 'shhhhh'
+  // ruleid: hardcoded-jwt-secret
   const token1 = jsonwt.sign(payload, secret)
 }
 
 function example2() {
-  // ruleid: hardcoded-jwt-secret
-  const jsonwt = require('jsonwebtoken')
   const payload = {foo: 'bar'}
+  // ruleid: hardcoded-jwt-secret
   const token2 = jsonwt.sign(payload, 'some-secret')
 }
 
 function example3() {
   // ok: hardcoded-jwt-secret
-  const jsonwt = require('jsonwebtoken')
   const payload = {foo: 'bar'}
   const token3 = jsonwt.sign(payload, config.secret)
 }
 
 function example4() {
   // ok: hardcoded-jwt-secret
-  const jsonwt = require('jsonwebtoken')
   const payload = {foo: 'bar'}
   const secret2 = config.secret
   const token4 = jsonwt.sign(payload, secret2)
@@ -34,19 +31,17 @@ function example4() {
 
 function example5() {
   // ok: hardcoded-jwt-secret
-  const jsonwt = require('jsonwebtoken')
   const payload = {foo: 'bar'}
   const secret3 = process.env.SECRET || 'fallback-secret'
   const token5 = jsonwt.sign(payload, secret3)
 }
 
-// ruleid: hardcoded-jwt-secret
-const jwt = require('jsonwebtoken');
 const Promise = require("bluebird");
 const secret = "hardcoded-secret"
 class Authentication {
     static sign(obj){
-        return jwt.sign(obj, secret, {});
+        // ruleid: hardcoded-jwt-secret
+        return jsonwt.sign(obj, secret, {});
     }
 
     static authenticate(payload) {
