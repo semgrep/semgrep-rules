a705056f2493bb338019bb2531011886100f2b5a
grayson <grayson@returntocorp.com>
2021-07-22 20:49:30 -0400

    Switch to taint mode for Django open redirect mode

--- python/django/security/injection/open-redirect.py
+++ python/django/security/injection/open-redirect.py
@@ -3,30 +3,30 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.utils.http import is_safe_url
 
 def unsafe(request):
-    # ruleid: open-redirect
     url = request.headers.get('referrer')
     print("something")
+    # ruleid: open-redirect
     return redirect(url)
 
 def safe(request):
-    # ok: open-redirect
     url = "https://lmnop.qrs"
+    # ok: open-redirect
     return redirect(url)
 
 def unsafe2(request):
-    # ruleid: open-redirect
     url = request.POST.get("url")
+    # ruleid: open-redirect
     return HttpResponseRedirect(url)
 
 def unsafe3(request):
-    # ruleid: open-redirect
     url = request.POST["url"]
+    # ruleid: open-redirect
     return HttpResponseRedirect(url)
 
 def unsafe4(request):
-    # ruleid: open-redirect
     url = request.get_referrer()  # I made this up, but if it exists don't do this
     if url:
+        # ruleid: open-redirect
         return HttpResponseRedirect(url)
 
 def fine(request):
@@ -34,19 +34,20 @@ def fine(request):
     return HttpResponseRedirect(request.get_full_path())
 
 def url_validation(request):
-    # ok: open-redirect
     next = request.POST.get('next', request.GET.get('next'))
     if (next or not request.is_ajax()) and not is_safe_url(url=next, allowed_hosts=request.get_host()):
         next = "/index"
+    # ok: open-redirect
     response = HttpResponseRedirect(next) if next else HttpResponse(status=204)
     return response
 
 def url_validation2(request):
-    # ok: open-redirect
     next = request.POST.get('next', request.GET.get('next'))
     ok = is_safe_url(url=next, allowed_hosts=request.get_host())
     if ok:
+        # ok: open-redirect
         response = HttpResponseRedirect(next) if next else HttpResponse(status=204)
     else:
+        # ok: open-redirect
         response = HttpResponseRedirect("index")
     return response
