c344cfb65ac1fc42bdf0fcbea4cd2cecfb20684c
Max Maass <malexmave@users.noreply.github.com>
2021-08-02 23:22:49 +0200

    Add Django path validator rules (#1402)
    Django uses paths for routing requests to different views.
    If paths are defined multiple times, this can be either inefficient
    or, in the worst case, lead to unexpected behavior. These rules are
    a first attempt at writing validators for these path issues. Feedback
    is welcome.

---
+++ python/django/maintainability/duplicate-path-assignment.py
@@ -0,0 +1,142 @@
+from django.urls import path
+
+# ruleid: duplicate-path-assignment
+urlpatterns = [
+    path('path/to/view', views.example_view, name="test"),
+    path('path/to/view', views.example_view, name="test"),
+]
+
+# ruleid: duplicate-path-assignment
+urlpatterns = [
+    path('path/to/view', views.example_view, name="test"),
+    path('path/to/other_view', view.other_view, name="hello"),
+    path('path/to/view', views.example_view, name="test"),
+]
+
+# ruleid: duplicate-path-assignment
+urlpatterns = [
+    path('path/to/view', views.example_view),
+    path('path/to/view', views.example_view),
+]
+
+# ruleid: duplicate-path-assignment
+urlpatterns = [
+    path('path/to/view', views.example_view, {'abc': 'def'}),
+    path('path/to/view', views.example_view, {'abc': 'def'}),
+]
+
+# ruleid: duplicate-path-assignment
+urlpatterns = [
+    path('path/to/view', views.example_view, {'abc': 'def'}),
+    path('path/to/view', views.example_view, {'def': 'abc'}),
+]
+
+# ruleid: duplicate-path-assignment
+urlpatterns = [
+    path('path/to/view', views.example_view, {'abc': 'def'}, name="test123"),
+    path('path/to/view', views.example_view, {'abc': 'def'}, name="test123"),
+]
+
+# ruleid: duplicate-path-assignment
+urlpatterns = [
+    path('path/to/view', views.example_view, {'abc': 'def'}, name="test123"),
+    path('path/to/view', views.example_view, {'def': 'abc'}),
+]
+
+# ruleid: duplicate-path-assignment
+urlpatterns = [
+    path('path/to/view', views.example_view, {'abc': 'def'}, name="test123"),
+    path('path/to/view', views.example_view),
+]
+
+# ruleid: conflicting-path-assignment
+urlpatterns = [
+    path('path/to/view', views.example_view, name="test"),
+    path('path/to/view', views.other_view, name="test"),
+]
+
+# ruleid: conflicting-path-assignment
+urlpatterns = [
+    path('path/to/view', views.example_view, name="test"),
+    path('path/to/other_view', view.other_view, name="hello"),
+    path('path/to/view', views.other_view, name="test"),
+]
+
+# ruleid: conflicting-path-assignment
+urlpatterns = [
+    path('path/to/view', views.example_view),
+    path('path/to/view', views.other_view),
+]
+
+# ruleid: conflicting-path-assignment
+urlpatterns = [
+    path('path/to/view', views.example_view, {'abc': 'def'}),
+    path('path/to/view', views.other_view, {'abc': 'def'}),
+]
+
+# ruleid: conflicting-path-assignment
+urlpatterns = [
+    path('path/to/view', views.example_view, {'abc': 'def'}),
+    path('path/to/view', views.other_view, {'def': 'abc'}),
+]
+
+# I would prefer duplicate-path-assignment to not match the following test cases
+# to avoid giving two messages for the same issue, but could not find a way yet.
+# todook: duplicate-path-assignment
+# ruleid: duplicate-path-assignment-different-names, duplicate-path-assignment
+urlpatterns = [
+    path('path/to/view', views.example_view, name="test"),
+    path('path/to/view', views.example_view, name="other_name"),
+]
+
+# todook: duplicate-path-assignment
+# ruleid: duplicate-path-assignment-different-names, duplicate-path-assignment
+urlpatterns = [
+    path('path/to/view', views.example_view, {'abc': 'def'}, name="test"),
+    path('path/to/view', views.example_view, {'abc': 'def'}, name="other_name"),
+]
+
+# todook: duplicate-path-assignment
+# ruleid: duplicate-path-assignment-different-names, duplicate-path-assignment
+urlpatterns = [
+    path('path/to/view', views.example_view, {'abc': 'def'}, name="test"),
+    path('path/to/other/view', views.other_view, {'abc': 'def'}, name="some_test"),
+    path('path/to/view', views.example_view, {'abc': 'def'}, name="other_name"),
+]
+
+# todook: duplicate-path-assignment
+# ruleid: duplicate-path-assignment-different-names, duplicate-path-assignment
+urlpatterns = [
+    path('path/to/view', views.example_view, {'abc': 'def'}, name="test123"),
+    path('path/to/view', views.example_view, {'def': 'abc'}, name="test456"),
+]
+
+# ruleid: duplicate-name-assignment
+urlpatterns = [
+    path('path/to/view', views.example_view, name="test123"),
+    path('path/to/other/view', views.other_view, name="test123"),
+]
+
+# ruleid: conflicting-path-assignment
+urlpatterns = [
+    path('path/to/view', views.example_view, name="test123"),
+    path('path/to/view', views.other_view, name="test123"),
+]
+
+# ruleid: duplicate-name-assignment
+urlpatterns = [
+    path('path/to/view', views.example_view, name="test123"),
+    path('path/to/other/view', views.other_view, name="test123"),
+]
+
+# ok: duplicate-path-assignment-different-names, conflicting-path-assignment, duplicate-path-assignment
+urlpatterns = [
+    path('path/to/other_view', views.example_view, name="test"),
+    path('path/to/view', views.example_view, name="test"),
+]
+
+# ok: duplicate-path-assignment-different-names, conflicting-path-assignment, duplicate-path-assignment
+urlpatterns = [
+    path('path/to/other_view', views.example_view, name="test"),
+    path('path/to/view', views.other_view, name="test_abc"),
+]
