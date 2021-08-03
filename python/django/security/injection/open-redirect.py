3a22d70efba0712c6d81a8d753c5b3e112baabd9
Grayson H <grayson@returntocorp.com>
2021-08-03 14:34:43 -0500

    Revert "Switch to taint mode for Django open redirect mode (#1389)" (#1404)
    This reverts commit 93c15dba6c1ef4999804c9c627a3239aec0048c9.
    
    Doing this because taint mode is not available on older Semgrep versions.

--- python/django/security/injection/open-redirect.py
+++ python/django/security/injection/open-redirect.py
@@ -1,32 +1,32 @@
 from django.shortcuts import redirect
 from django.http import HttpResponseRedirect, HttpResponse
-from django.utils.http import is_safe_url, url_has_allowed_host_and_scheme
+from django.utils.http import is_safe_url
 
 def unsafe(request):
+    # ruleid: open-redirect
     url = request.headers.get('referrer')
     print("something")
-    # ruleid: open-redirect
     return redirect(url)
 
 def safe(request):
-    url = "https://lmnop.qrs"
     # ok: open-redirect
+    url = "https://lmnop.qrs"
     return redirect(url)
 
 def unsafe2(request):
-    url = request.POST.get("url")
     # ruleid: open-redirect
+    url = request.POST.get("url")
     return HttpResponseRedirect(url)
 
 def unsafe3(request):
-    url = request.POST["url"]
     # ruleid: open-redirect
+    url = request.POST["url"]
     return HttpResponseRedirect(url)
 
 def unsafe4(request):
+    # ruleid: open-redirect
     url = request.get_referrer()  # I made this up, but if it exists don't do this
     if url:
-        # ruleid: open-redirect
         return HttpResponseRedirect(url)
 
 def fine(request):
@@ -34,28 +34,19 @@ def fine(request):
     return HttpResponseRedirect(request.get_full_path())
 
 def url_validation(request):
+    # ok: open-redirect
     next = request.POST.get('next', request.GET.get('next'))
     if (next or not request.is_ajax()) and not is_safe_url(url=next, allowed_hosts=request.get_host()):
         next = "/index"
-    # ok: open-redirect
     response = HttpResponseRedirect(next) if next else HttpResponse(status=204)
     return response
 
 def url_validation2(request):
+    # ok: open-redirect
     next = request.POST.get('next', request.GET.get('next'))
     ok = is_safe_url(url=next, allowed_hosts=request.get_host())
     if ok:
-        # ok: open-redirect
         response = HttpResponseRedirect(next) if next else HttpResponse(status=204)
     else:
-        # ok: open-redirect
         response = HttpResponseRedirect("index")
-    return response
-
-def url_validation(request):
-    next = request.POST.get('next', request.GET.get('next'))
-    if (next or not request.is_ajax()) and not url_has_allowed_host_and_scheme(url=next, allowed_hosts=request.get_host()):
-        next = "/index"
-    # ok: open-redirect
-    response = HttpResponseRedirect(next) if next else HttpResponse(status=204)
     return response
