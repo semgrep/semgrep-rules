babf62df0bd9cfd7d60fb985c246b63b8d2a870e
Bernardo Chaves <bernardo.amc@gmail.com>
2021-08-11 09:12:55 -0400

    Expand rule to cover a broader set of origin validation scenarios
    The current rule takes into account:
    
    1. Inline function declarations within `addEventListener`
    2. Specific operators for origin validation within the handler
    
    The new rule expands it to cover:
    
    1. Inline and external function declarations
    2. Checks whether origin is being called or not within an if clause in
       the handler
    
    See this rule in action: https://semgrep.dev/s/rkOR
    
    If we think checking for specific equality operators is something
    desirable we can change the rule to something like: https://semgrep.dev/s/brjE

--- javascript/browser/security/insufficient-postmessage-origin-validation.js
+++ javascript/browser/security/insufficient-postmessage-origin-validation.js
@@ -1,25 +1,69 @@
-//ruleid:insufficient-postmessage-origin-validation
-window.addEventListener("message", receiveMessage, false);
-
-
-//addEventListener Safe Usage (Origin Checked)
-const globalRegex = RegExp('/^http://www.examplesender.com$/', 'g');
-window.addEventListener("message", function(message){
-    if(globalRegex.test(message.origin)){
-         console.log(message.data);
-   }
+// ruleid: insufficient-postmessage-origin-validation
+window.addEventListener("message", function (evt) {
+  console.log('Inline without origin check!');
 });
 
-//addEventListener Safe Usage (Origin Checked)
-window.addEventListener("message", function(message){
-if (event.origin !== "http://example.com") {
-    return;
-}
+// ruleid: insufficient-postmessage-origin-validation
+function oldHandler(evt) {
+  console.log('Normal function handler without origin check!');
+};
+
+window.addEventListener("message", oldHandler, false);
+
+// ruleid: insufficient-postmessage-origin-validation
+window.addEventListener('message', (evt) => {
+  console.log('Inline arrow function without origin check!');
 });
 
-//addEventListener Safe Usage (Origin Checked)
-window.addEventListener("message", function(message){
-if (event.origin == "http://example.com") {
-    alert("Works");
-}
+// ruleid: insufficient-postmessage-origin-validation
+window.addEventListener('message', evt => {
+  console.log('Inline arrow function without parenthesis & origin check!');
+});
+
+// ruleid: insufficient-postmessage-origin-validation
+const handler = (evt) => {
+  console.log('Arrow function handler without origin check!');
+};
+
+window.addEventListener("message", handler, false);
+
+// ok: insufficient-postmessage-origin-validation
+window.addEventListener("message", function (evt) {
+  if (evt.origin == "http://example.com") {
+    console.log('Normal inline function declaration with origin validation');
+  }
+});
+
+// ok: insufficient-postmessage-origin-validation
+function normalHandler(evt) {
+  if (evt.origin == "http://example.com") {
+    console.log('Normal function handler with origin validation');
+  }
+};
+
+window.addEventListener('message', normalHandler, false);
+
+// ok: insufficient-postmessage-origin-validation
+window.addEventListener('message', (evt) => {
+  if (evt.origin !== "http://example.com") {
+    console.log('Inline arrow function declaration with origin validation');
+  }
+});
+
+// ok: insufficient-postmessage-origin-validation
+const arrowHandler = (evt) => {
+  if (evt.origin == "http://example.com") {
+    console.log('Arrow function handler with origin validation');
+  }
+};
+
+window.addEventListener('message', arrowHandler, false);
+
+const globalRegex = RegExp('/^http://www\.example\.com$/', 'g');
+
+// ok: insufficient-postmessage-origin-validation
+window.addEventListener("message", (evt) => {
+  if (globalRegex.test(evt.origin)) {
+    console.log(message.data);
+  }
 });
