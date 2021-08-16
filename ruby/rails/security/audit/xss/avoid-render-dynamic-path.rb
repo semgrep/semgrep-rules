cf24845b8bd58813edddbb99078b13b839857f0f
Emma Jin <emjin@users.noreply.github.com>
2021-08-16 15:35:21 -0700

    avoid-render-dynamic-path

---
+++ ruby/rails/security/audit/xss/avoid-render-dynamic-path.rb
@@ -0,0 +1,17 @@
+#ruleid: avoid-render-dynamic-path
+render(action => params[:action], {})
+
+#ruleid: avoid-render-dynamic-path
+render(action => cookies[:name], {})
+
+#ruleid: avoid-render-dynamic-path
+render(action => request.env['HTTP_REFERRER'], {})
+
+#ok: avoid-render-dynamic-path
+render(inline => params[:action], {})
+
+#ok: avoid-render-dynamic-path
+render(action => my_action, {})
+
+#ok: avoid-render-dynamic-path
+render(my_json)
