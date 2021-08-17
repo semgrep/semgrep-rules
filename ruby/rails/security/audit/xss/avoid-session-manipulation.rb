3b452ba289a5a16f73aa02161b07e04f04cdca5d
Emma Jin <emjin@users.noreply.github.com>
2021-08-17 11:42:35 -0700

    Add a layer of separation test

--- ruby/rails/security/audit/xss/avoid-session-manipulation.rb
+++ ruby/rails/security/audit/xss/avoid-session-manipulation.rb
@@ -1,5 +1,9 @@
 # ruleid: avoid-session-manipulation
 id = session[params[:uid]]
 
+# ruleid: avoid-session-manipulation
+uid = params[:uid]
+id = session[uid]
+
 # ok: avoid-session-manipulation
 id = session[user_id]
