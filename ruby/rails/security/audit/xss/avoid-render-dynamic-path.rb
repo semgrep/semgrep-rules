464b6e76a5f60265e34dcf741aa705c06b336d68
Emma Jin <emjin@users.noreply.github.com>
2021-08-18 16:07:06 -0700

    Modified avoid-render-dynamic-path test

--- ruby/rails/security/audit/xss/avoid-render-dynamic-path.rb
+++ ruby/rails/security/audit/xss/avoid-render-dynamic-path.rb
@@ -7,6 +7,9 @@ render(action => cookies[:name], {})
 #ruleid: avoid-render-dynamic-path
 render(action => h(request.env['HTTP_REFERRER']), {})
 
+#ruleid: avoid-render-dynamic-path
+render(template => foo(request.env['HTTP_REFERRER']), {})
+
 #ok: avoid-render-dynamic-path
 render(inline => params[:action], {})
 
