bb8e059c86a6dc3e7a3821c0296af3a816bf6c4c
Emma Jin <emjin@users.noreply.github.com>
2021-08-17 17:38:23 -0700

    Update avoid-session-manipulation

--- ruby/rails/security/audit/xss/avoid-session-manipulation.rb
+++ ruby/rails/security/audit/xss/avoid-session-manipulation.rb
@@ -1,8 +1,8 @@
 # ruleid: avoid-session-manipulation
 id = session[params[:uid]]
 
-# ruleid: avoid-session-manipulation
 uid = params[:uid]
+# ruleid: avoid-session-manipulation
 id = session[uid]
 
 # ok: avoid-session-manipulation
