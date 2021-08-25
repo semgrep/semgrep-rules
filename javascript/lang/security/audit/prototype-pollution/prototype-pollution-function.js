7f21a1c5b0b22c8845b6747e6b575a11af2061ad
Vasilii <inkz@xakep.ru>
2021-08-25 17:17:39 +1000

    add prototype-pollution-function rule

---
+++ javascript/lang/security/audit/prototype-pollution/prototype-pollution-function.js
@@ -0,0 +1,36 @@
+function merge1(dst, src) {
+    for (let key in src) {
+        if (!src.hasOwnProperty(key)) continue;
+        if (isObject(dst[key])) {
+            merge1(dst[key], src[key]);
+        } else {
+            // ruleid: prototype-pollution-function
+            dst[key] = src[key];
+        }
+    }
+}
+
+function okMerge1(dst, src) {
+    for (let key in src) {
+        if (!src.hasOwnProperty(key)) continue;
+        if (dst.hasOwnProperty(key) && isObject(dst[key])) {
+            okMerge1(dst[key], src[key]);
+        } else {
+            // ok: prototype-pollution-function
+            dst[key] = src[key];
+        }
+    }
+}
+
+function okMerge2(dst, src) {
+    for (let key in src) {
+        if (!src.hasOwnProperty(key)) continue;
+        if (key === "__proto__" || key === "constructor") continue;
+        if (isObject(dst[key])) {
+            okMerge2(dst[key], src[key]);
+        } else {
+            // ok: prototype-pollution-function
+            dst[key] = src[key];
+        }
+    }
+}
