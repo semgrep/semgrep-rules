7832b4299127aea63d3c163a740b97c980e13b55
Emma Jin <emjin@users.noreply.github.com>
2021-08-27 10:14:05 -0700

    Convert a detect-angular-trust-as rule

--- javascript/angular/security/detect-angular-trust-as-js-method.js
+++ javascript/angular/security/detect-angular-trust-as-js-method.js
@@ -8,6 +8,10 @@ $scope.userInput = 'foo';
      // ruleid:detect-angular-trust-as-js-method
      input = $scope.html
      $scope.trustedurl = $sce.trustAsJs(input);
+     // ruleid:detect-angular-trust-as-js-method
+     input = $scope.html
+     sce_alias = $sce
+     $scope.trustedurl = $sce.trustAsJs(input);
 
 
      //Data is not coming from user input
@@ -15,3 +19,38 @@ $scope.userInput = 'foo';
    };
 
 });
+
+var app1 = angular.module('MyApp', []);
+app1.controller('myCtrl', function() {
+
+$scope.userInput = 'foo';
+    $scope.sayHello = function() {
+     // $sce is present but not in the function args
+     $scope.trustedurl = $sce.trustAsJs($scope.html);
+
+  };
+
+});
+
+// Another controller style
+var app = angular.module('MyApp', []);
+app.controller('myCtrl',['$scope','$sce','$document', function($scope, $sce, $document) {
+
+$scope.userInput = 'foo';
+    $scope.sayHello = function() {
+     // ruleid:detect-angular-trust-as-js-method
+     $scope.trustedurl = $sce.trustAsJs($scope.html);
+     // ruleid:detect-angular-trust-as-js-method
+     input = $scope.html
+     $scope.trustedurl = $sce.trustAsJs(input);
+     // ruleid:detect-angular-trust-as-js-method
+     input = $scope.html
+     sce_alias = $sce
+     $scope.trustedurl = $sce.trustAsJs(input);
+
+
+     //Data is not coming from user input
+     $scope.trustedurl = $sce.trustAsJs('stringLiteral');
+   };
+
+}]);
