c6fab18384c9959feb527e6f2b713d193786f73e
gregory draperi <gregory.draperi@gmail.com>
2021-08-30 13:31:14 +0200

    Second version for CSharp SSRF

--- csharp/lang/security/ssrf/csharp_ssrf.cs
+++ csharp/lang/security/ssrf/csharp_ssrf.cs
@@ -6,6 +6,7 @@ namespace Ssrf
     public class Ssrf
     {
         // ruleid: ssrf
+        // ko
         public void ssrf1(string ssrf)
         {
             Uri uri = new Uri(ssrf);
@@ -14,9 +15,49 @@ namespace Ssrf
         }
 
         // ruleid: ssrf
+        // ko
         public void ssrf2(string ssrf)
         {
             WebRequest webRequest = WebRequest.Create(ssrf);
+
+        }
+        // ruleid: ssrf
+        // ko
+        public void ssrf3(string ssrf)
+        {
+
+            String base = "https://www.example.com";
+
+            Uri uri = new Uri(base+ssrf);
+            WebRequest webRequest = WebRequest.Create(uri);
+
+
+
+        }
+        // ruleid: ssrf
+        // ok
+        public void ssrf4(string ssrf)
+        {
+
+
+            WebRequest webRequest = WebRequest.Create("https://www.example.com");
+
+
+
+        }
+        // ruleid: ssrf
+        // ok
+        public void ssrf5(string ssrf)
+        {
+
+            String domain = "www.example.com";
+
+            Uri uri = new Uri(ssrf);
+            if (domain.Equals(uri.Host)){
+            WebRequest webRequest = WebRequest.Create(request);
+          }
+
+
 
         }
 
