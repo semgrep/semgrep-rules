615ffe6077dd599d50c2fc809a9935f09bdcc8de
Grayson H <grayson@returntocorp.com>
2021-09-30 10:53:12 -0500

    Add test case

--- javascript/lang/security/audit/detect-non-literal-regexp.js
+++ javascript/lang/security/audit/detect-non-literal-regexp.js
@@ -9,3 +9,10 @@ function bad (name) {
   const reg = new RegExp("\\w+" + name)
   return reg.exec(name)
 }
+
+function jsliteral (name) {
+  const exp = /a.*/;
+  //ok: detect-non-literal-regexp
+  const reg = new RegExp(exp);
+  return reg.exec(name);
+}
