0ae34c128c98e78b59d4fc4be2eee39119b4db22
grayson <grayson@returntocorp.com>
2021-10-01 16:50:23 -0500

    Remove top-ellipses from various rules for performance

--- javascript/jsonwebtoken/security/audit/jwt-decode-without-verify.js
+++ javascript/jsonwebtoken/security/audit/jwt-decode-without-verify.js
@@ -1,6 +1,23 @@
+const { ok } = require('assert');
 const jwt = require('jsonwebtoken');
 
 // ruleid: jwt-decode-without-verify
 if (jwt.decode(token, true).param === true) {
   console.log('token is valid');
 }
+
+function ok(token, key) {
+  // ok: jwt-decode-without-verify
+  jwt.verify(token, key);
+  if (jwt.decode(token, true).param === true) {
+    console.log('token is valid');
+  }
+}
+
+const ok2 = (token, key) => {
+  // ok: jwt-decode-without-verify
+  jwt.verify(token, key);
+  if (jwt.decode(token, true).param === true) {
+    console.log('token is valid');
+  }
+};
