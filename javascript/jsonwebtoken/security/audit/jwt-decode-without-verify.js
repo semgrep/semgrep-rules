e674fe96a3b20b0be97dd4735b9c7b7315fb4af7
Vasilii Ermilov <inkz@xakep.ru>
2021-10-08 15:17:45 +1000

    update jwt-decode-without-verify rule (#1518)
    * update jwt-decode-without-verify rule
    
    * Update jwt-decode-without-verify.yaml

--- javascript/jsonwebtoken/security/audit/jwt-decode-without-verify.js
+++ javascript/jsonwebtoken/security/audit/jwt-decode-without-verify.js
@@ -1,9 +1,10 @@
-const { ok } = require('assert');
 const jwt = require('jsonwebtoken');
 
-// ruleid: jwt-decode-without-verify
-if (jwt.decode(token, true).param === true) {
-  console.log('token is valid');
+function notOk(token) {
+  // ruleid: jwt-decode-without-verify
+  if (jwt.decode(token, true).param === true) {
+    console.log('token is valid');
+  }
 }
 
 function ok(token, key) {
@@ -16,8 +17,8 @@ function ok(token, key) {
 
 const ok2 = (token, key) => {
   // ok: jwt-decode-without-verify
-  jwt.verify(token, key);
-  if (jwt.decode(token, true).param === true) {
+  const value = jwt.decode(token, key).param;
+  if (jwt.verify(token, true).param === true) {
     console.log('token is valid');
   }
 };
