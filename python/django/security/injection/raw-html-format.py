cd09197d348fdf8904d8891c8ac951bae307c106
grayson <grayson@returntocorp.com>
2021-10-12 23:25:29 -0500

    Pre-commit

--- python/django/security/injection/raw-html-format.py
+++ python/django/security/injection/raw-html-format.py
@@ -10,88 +10,88 @@ class FalsePositiveCheck499View(VulnerableTemplateView):
     url_path = '499_check.py?text=1'
     false_positive_check = True
     references = ['https://github.com/andresriancho/w3af/pull/499']
-    
+
     def get(self, request, *args, **kwds):
         context = self.get_context_data()
-        
+
         text = request.GET['text']
         text = text.replace('"', '')
-        
+
         link = '<a href="http://external/abc/%s">Check link href</a>'
-        
+
         # ruleid: raw-html-format
         context['html'] = link % text
-        
+
         return render(request, self.template_name, context)
 
     def get2(self, request, *args, **kwds):
         context = self.get_context_data()
-        
+
         text = request.GET['text']
         text = text.replace('"', '')
-        
+
         link = '<a href="http://external/abc/{}">Check link href</a>'
-        
+
         # ruleid: raw-html-format
         context['html'] = link.format(text)
-        
+
         return render(request, self.template_name, context)
 
     def get3(self, request, *args, **kwds):
         context = self.get_context_data()
-        
+
         text = request.GET['text']
         text = text.replace('"', '')
-        
+
         # ruleid: raw-html-format
         context['html'] = '<a href="http://external/abc/%s">Check link href</a>' % text
-        
+
         return render(request, self.template_name, context)
 
     def get4(self, request, *args, **kwds):
         context = self.get_context_data()
-        
+
         text = request.GET['text']
         text = text.replace('"', '')
-        
+
         # ruleid: raw-html-format
         context['html'] = '<a href="http://external/abc/%s">Check link href</a>'.format(text)
-        
+
         return render(request, self.template_name, context)
 
     def get5(self, request, *args, **kwds):
         context = self.get_context_data()
-        
+
         text = request.GET['text']
         text = text.replace('"', '')
-        
+
         # ruleid: raw-html-format
         context['html'] = f'<a href="http://external/abc/{text}">Check link href</a>'
-        
+
         return render(request, self.template_name, context)
 
     def ok(self, request, *args, **kwds):
         context = self.get_context_data()
-        
+
         text = request.GET['text']
         text = text.replace('"', '')
-        
+
         link = 'something other than html, %s!'
-        
+
         # ok: raw-html-format
         context['html'] = link % text
-        
+
         return render(request, self.template_name, context)
 
     def ok2(self, request, *args, **kwds):
         context = self.get_context_data()
-        
+
         text = request.GET['text']
         text = text.replace('"', '')
-        
+
         # ok: raw-html-format
         context['html'] = 'this is a random string. {}'.format(text)
-        
+
         return render(request, self.template_name, context)
 
     def ok3(self, request):
