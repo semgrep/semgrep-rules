9c68599c56cba5ffcee211f338a761b928d16b46
grayson <grayson@returntocorp.com>
2021-10-13 11:14:02 -0500

    Add addt'l test annotations

--- ocaml/lang/performance/list.ml
+++ ocaml/lang/performance/list.ml
@@ -1,11 +1,11 @@
 let test xs =
-  (* ruleid:ocamllint-length-list-zero *)
+  (* ruleid:ocamllint-length-list-zero,ocamllint-length-list-compare *)
   if List.length xs = 0
   then 1
   else 2
 
 let test2 xs =
-  (* ruleid:ocamllint-length-more-than-zero *)
+  (* ruleid:ocamllint-length-more-than-zero,ocamllint-length-list-compare *)
   if List.length xs > 0
   then 1
   else 2
