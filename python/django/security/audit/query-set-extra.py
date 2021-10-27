f58c4d183cb8047cb4380e0f9df9ad272d9e397b
grayson <grayson@returntocorp.com>
2021-10-26 22:21:21 -0500

    Filter constant strings from select clauses in Django queryset extra

--- python/django/security/audit/query-set-extra.py
+++ python/django/security/audit/query-set-extra.py
@@ -1,14 +1,1 @@
-# ruleid:avoid-query-set-extra
-Entry.objects.get().extra()
-
-# ruleid:avoid-query-set-extra
-Entry.objects.filter().extra()
-
-# ruleid:avoid-query-set-extra
-Entry.objects.update().extra()
-
-# ruleid:avoid-query-set-extra
-Entry.objects.filter().udpate().extra()
-
-# ruleid:avoid-query-set-extra
-Entry.objects.get({}).filter().udpate().extra()
+update
