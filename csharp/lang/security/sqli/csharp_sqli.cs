42305193825d745ade0a18ccaa91f3f838988384
gregory draperi <gregory.draperi@gmail.com>
2021-11-09 11:28:47 +0100

    fixing typo in cs file and add new patterns following comments for improvments

--- csharp/lang/security/sqli/csharp_sqli.cs
+++ csharp/lang/security/sqli/csharp_sqli.cs
@@ -63,9 +63,9 @@ namespace Sqli
          connection.Open();
          SqlCommand command= connection.CreateCommand();
          command.CommandText = string.Concat(new string[]
-            {
-            "UPDATE [dbo].[tblBigTableReference] SET tblBigTableReference.PropertyName = @NewPropertyName FROM [dbo].[tblBigTableReference] INNER JOIN [dbo].[",sqli,"] ON ",sqli,".pkId = tblBigTableReference.pkId WHERE ", sqli, ".StoreName = @storeName AND tblBigTableReference.PropertyName = @OldPropertyName"
-           });
+			{
+			"UPDATE [dbo].[tblBigTableReference] SET tblBigTableReference.PropertyName = @NewPropertyName FROM [dbo].[tblBigTableReference] INNER JOIN [dbo].[",sqli,"] ON ",sqli,".pkId = tblBigTableReference.pkId WHERE ", sqli, ".StoreName = @storeName AND tblBigTableReference.PropertyName = @OldPropertyName"
+		   });
          command.CommandTimeout = 15;
          command.CommandType = CommandType.Text;
       }
@@ -83,9 +83,9 @@ namespace Sqli
          connection.Open();
          SqlCommand command= connection.CreateCommand();
          command.CommandText = String.Concat(new string[]
-            {
-            "UPDATE [dbo].[tblBigTableReference] SET tblBigTableReference.PropertyName = @NewPropertyName FROM [dbo].[tblBigTableReference] INNER JOIN [dbo].[",sqli,"] ON ",sqli,".pkId = tblBigTableReference.pkId WHERE ", sqli, ".StoreName = @storeName AND tblBigTableReference.PropertyName = @OldPropertyName"
-           });
+			{
+			"UPDATE [dbo].[tblBigTableReference] SET tblBigTableReference.PropertyName = @NewPropertyName FROM [dbo].[tblBigTableReference] INNER JOIN [dbo].[",sqli,"] ON ",sqli,".pkId = tblBigTableReference.pkId WHERE ", sqli, ".StoreName = @storeName AND tblBigTableReference.PropertyName = @OldPropertyName"
+		   });
          command.CommandTimeout = 15;
          command.CommandType = CommandType.Text;
       }
@@ -98,7 +98,7 @@ namespace Sqli
 
          using (SqlConnection connection = new SqlConnection("Data Source=(local);Initial Catalog=Northwind;Integrated Security=SSPI;")) {
          connection.Open();
-         SqlCommand command = new Sqlcommand(sqli)
+         SqlCommand command = new SqlCommand(sqli)
 
       }
 
@@ -110,87 +110,75 @@ namespace Sqli
 
          using (SqlConnection connection = new SqlConnection("Data Source=(local);Initial Catalog=Northwind;Integrated Security=SSPI;")) {
          connection.Open();
-         SqlCommand command = new Sqlcommand(string.Format("SELECT c.name AS column_name,t.name AS type_name,c.max_length,c.precision,c.scale,             CAST(CASE WHEN EXISTS(SELECT * FROM sys.index_columns AS i WHERE i.object_id=c.object_id AND i.column_id=c.column_id) THEN 1 ELSE 0 END AS BIT) AS column_indexed             FROM sys.columns AS c              JOIN sys.types AS t ON c.user_type_id=t.user_type_id              WHERE c.object_id = OBJECT_ID('{0}')              ORDER BY c.column_id;", sqli));
-    )
+         SqlCommand command = new SqlCommand(string.Format("SELECT c.name AS column_name,t.name AS type_name,c.max_length,c.precision,c.scale,CAST(CASE WHEN EXISTS(SELECT * FROM sys.index_columns AS i WHERE i.object_id=c.object_id AND i.column_id=c.column_id) THEN 1 ELSE 0 END AS BIT) AS column_indexed             FROM sys.columns AS c              JOIN sys.types AS t ON c.user_type_id=t.user_type_id              WHERE c.object_id = OBJECT_ID('{0}')              ORDER BY c.column_id;", sqli));
+
       }
 
 
         }
         // rule-id: csharp-sqli
-        public void sqli8(string ssrf)
+        public void sqli8(string sqli)
         {
 
          using (SqlConnection connection = new SqlConnection("Data Source=(local);Initial Catalog=Northwind;Integrated Security=SSPI;")) {
          connection.Open();
-         SqlCommand command = new Sqlcommand(String.Format("SELECT c.name AS column_name,t.name AS type_name,c.max_length,c.precision,c.scale,             CAST(CASE WHEN EXISTS(SELECT * FROM sys.index_columns AS i WHERE i.object_id=c.object_id AND i.column_id=c.column_id) THEN 1 ELSE 0 END AS BIT) AS column_indexed             FROM sys.columns AS c              JOIN sys.types AS t ON c.user_type_id=t.user_type_id              WHERE c.object_id = OBJECT_ID('{0}')              ORDER BY c.column_id;", sqli));
+         SqlCommand command = new SqlCommand(String.Format("SELECT c.name AS column_name,t.name AS type_name,c.max_length,c.precision,c.scale,             CAST(CASE WHEN EXISTS(SELECT * FROM sys.index_columns AS i WHERE i.object_id=c.object_id AND i.column_id=c.column_id) THEN 1 ELSE 0 END AS BIT) AS column_indexed             FROM sys.columns AS c              JOIN sys.types AS t ON c.user_type_id=t.user_type_id              WHERE c.object_id = OBJECT_ID('{0}')              ORDER BY c.column_id;", sqli));
 
       }
 
 
         }
         // rule-id: csharp-sqli
-        public void sqli9(string ssrf)
+        public void sqli9(string sqli)
         {
 
          using (SqlConnection connection = new SqlConnection("Data Source=(local);Initial Catalog=Northwind;Integrated Security=SSPI;")) {
          connection.Open();
          StringBuilder stringBuilder = new StringBuilder("SELECT Id, ItemType");
          stringBuilder.AppendFormat(" FROM {0} ", sqli);
-         SqlCommand command= new Sqlcommand(stringBuilder.ToString());
+         SqlCommand command= new SqlCommand(stringBuilder.ToString());
       }
 
 
         }
         // rule-id: csharp-sqli
-        public void sqli10()
+        public void sqli10(string sqli)
         {
 
          using (SqlConnection connection = new SqlConnection("Data Source=(local);Initial Catalog=Northwind;Integrated Security=SSPI;")) {
          connection.Open();
-         String sqli = String.Concat(new string[]
-            {
-            "UPDATE [dbo].[tblBigTableReference] SET tblBigTableReference.PropertyName = @NewPropertyName FROM [dbo].[tblBigTableReference] INNER JOIN [dbo].[",sqli,"] ON ",sqli,".pkId = tblBigTableReference.pkId WHERE ", sqli, ".StoreName = @storeName AND tblBigTableReference.PropertyName = @OldPropertyName"
-            });
-         SqlCommand command= new Sqlcommand(sqli);
+         String sqlcommand = String.Concat(new string[]
+			{
+			"UPDATE [dbo].[tblBigTableReference] SET tblBigTableReference.PropertyName = @NewPropertyName FROM [dbo].[tblBigTableReference] INNER JOIN [dbo].[",sqli,"] ON ",sqli,".pkId = tblBigTableReference.pkId WHERE ", sqli, ".StoreName = @storeName AND tblBigTableReference.PropertyName = @OldPropertyName"
+			});
+         SqlCommand command= new SqlCommand(sqlcommand);
 
       }
 
 
         }
 
+
+
+
+
       // rule-id: csharp-sqli
-        public void sqli11()
+      public void sqli11(string sqli)
         {
 
          using (SqlConnection connection = new SqlConnection("Data Source=(local);Initial Catalog=Northwind;Integrated Security=SSPI;")) {
          connection.Open();
-         string sqli = string.Concat(new string[]
-            {
-            "UPDATE [dbo].[tblBigTableReference] SET tblBigTableReference.PropertyName = @NewPropertyName FROM [dbo].[tblBigTableReference] INNER JOIN [dbo].[",sqli,"] ON ",sqli,".pkId = tblBigTableReference.pkId WHERE ", sqli, ".StoreName = @storeName AND tblBigTableReference.PropertyName = @OldPropertyName"
-           });
-         SqlCommand command= new Sqlcommand(sqli);
-
-      }
-
-
-        }
-      // rule-id: csharp-sqli
-      public void sqli11(string ssrf)
-        {
-
-         using (SqlConnection connection = new SqlConnection("Data Source=(local);Initial Catalog=Northwind;Integrated Security=SSPI;")) {
-         connection.Open();
-         SqlCommand command= new Sqlcommand(ssrf);
+         SqlCommand command= new SqlCommand(sqli);
       }
         }
       // rule-id: csharp-sqli
-      public void sqli12(string ssrf)
+      public void sqli12(string sqli)
         {
 
          using (SqlConnection connection = new SqlConnection("Data Source=(local);Initial Catalog=Northwind;Integrated Security=SSPI;")) {
          connection.Open();
          SqlCommand command= connection.CreateCommand();
-         command.CommandText = ssrf;
+         command.CommandText = sqli;
          command.CommandTimeout = 15;
          command.CommandType = CommandType.Text;
       }
