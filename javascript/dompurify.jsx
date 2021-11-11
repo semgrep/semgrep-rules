fa0febe81e9caa669de9839bbaab4e48f6de9e97
Isaac Evans <isaac.n.evans@gmail.com>
2021-11-11 22:56:42 +0000

    add javascript/dompurify.jsx

---
+++ javascript/dompurify.jsx
@@ -0,0 +1,27 @@
+/*
+ * Control permitted attribute values
+ */
+
+/**
+ * Influence the return-type
+ */
+
+// return a DOM HTMLBodyElement instead of an HTML string (default is false)
+// okruleid: harden-dompurify-usage
+var clean = DOMPurify.sanitize(dirty, {RETURN_DOM: true});
+
+// return a DOM DocumentFragment instead of an HTML string (default is false)
+// okruleid: harden-dompurify-usage
+var clean = DOMPurify.sanitize(dirty, {RETURN_DOM_FRAGMENT: true});
+
+// return a DOM DocumentFragment instead of an HTML string (default is false)
+// also import it into the current document (default is false).
+// RETURN_DOM_IMPORT must be set if you would like to append
+// the returned node to the current document
+// okruleid: harden-dompurify-usage
+var clean = DOMPurify.sanitize(dirty, {RETURN_DOM_FRAGMENT: true, RETURN_DOM_IMPORT: true});
+document.body.appendChild(clean);
+
+// ruleid: harden-dompurify-usage
+var yikes = DOMPurify.sanitize(dirty, {})
+document.body.innerHTML = yikes;
