24b1429b28b6fd907c62d2ee814b3efdc19bf61d
Brandon Bailey <81580001+brandon-bailey-rc@users.noreply.github.com>
2021-11-17 12:44:36 -0500

    Adding new terraform rules (Azure)

---
+++ terraform/azure/security/appservice/appservice-enable-http2.tf
@@ -0,0 +1,28 @@
+resource "azurerm_app_service" "good_example" {
+  name                = "example-app-service"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  app_service_plan_id = azurerm_app_service_plan.example.id
+
+  site_config {
+      http2_enabled = true
+  }
+}
+# ruleid: appservice-enable-http2
+resource "azurerm_app_service" "bad_example" {
+  name                = "example-app-service"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  app_service_plan_id = azurerm_app_service_plan.example.id
+  site_config {
+      http2_enabled = false
+  }
+}
+
+# ruleid: appservice-enable-http2
+resource "azurerm_app_service" "bad_example" {
+  name                = "example-app-service"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  app_service_plan_id = azurerm_app_service_plan.example.id
+}
