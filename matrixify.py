42a2c57645a1caa3895fb9a5d708ea23c34f0b28
lejliu <14846866+lejliu@users.noreply.github.com>
2021-11-30 20:09:07 -0800

    Updated matrixify script (#1632)

--- matrixify.py
+++ matrixify.py
@@ -29,10 +29,14 @@ class ArchList(list):
         except IndexError:
             return default
 
-
 def get_owasp(rule: Dict[str, Any]) -> str:
     try:
-        return rule.get("metadata", {}).get("owasp", "")
+        output = rule.get("metadata", {}).get("owasp", "")
+        if type(output) == str:
+            if output == '':
+                output = 'Not OWASP Related'
+            return [output]
+        return output
     except AttributeError:
         return ArchList(filter(lambda d: "owasp" in d.keys(), rule.get('metadata'))).get(0, {}).get('owasp', "")
     except Exception:
@@ -48,16 +52,20 @@ def get_cwe(rule: Dict[str, Any]) -> str:
         logger.warning(f"Could not get cwe for rule {rule.get('id', '')}")
         return ""
 
-def get_lang(rule: Dict[str, Any]) -> str:
-    return ArchList(rule.get('languages', [])).get(0, "")
+# Seems like the only reliable way to get the lanaguage is from the filepath. Sometimes, the language as defined within the ArchList will be something that's not in the dict
+def get_lang(path: str) -> str:
+    return path.split(os.path.sep)[1]
+    #archlist =  ArchList(rule.get('languages', [])).get(0, "")
+    #return archlist
 
 def get_framework(path: str, rule: Dict[str, Any]) -> str:
-    # get the dir name immediately under the language
-    lang = get_lang(rule)
+    #  get the dir name immediately under the language
     s = path.split(os.path.sep)
-    logger.debug(f"Split path={s}")
+    lang = s[1]
     if 'contrib' in path:
         return s[s.index('contrib')+1]
+    # Crashes if lang is 'hcl'. This occurs with azure, aws, and lang
+    # if lang == hcl:
     return s[s.index(lang)+1]
 
 def is_security(path: str) -> bool:
@@ -67,6 +75,7 @@ def is_rule(path: str) -> bool:
     _, ext = os.path.splitext(path)
     return "yaml" in ext
 
+# Old function. Probably not useful anymore, but keeping it just in case.
 def normalize_owasp(owasp: str) -> str:
     return owasp.replace(" -", ":").replace("\'", "")
 
@@ -92,22 +101,25 @@ if __name__ == "__main__":
                 continue
             if not is_security(path):
                 continue
-            logger.debug(f"Opening {path}")
+            logger.debug(f"\n\nOpening {path}")
             with open(path, 'r') as fin:
                 rules = yaml.safe_load(fin)
                 for rule in rules.get('rules', []):
-                    framework = get_framework(path, rule)
-                    lang = get_lang(rule)
-                    cwe = get_cwe(rule)
-                    owasp = normalize_owasp(get_owasp(rule))
-                    owasp_matrix[owasp].append((path, rule))
-                    cwe_matrix[cwe].append((path, rule))
-                    owasp_by_lang_matrix[owasp][lang].append((path, rule))
-                    cwe_by_lang_matrix[cwe][lang].append((path, rule))
-                    owasp_by_framework_matrix[owasp][lang][framework].append((path, rule))
-                    cwe_by_framework_matrix[cwe][lang][framework].append((path, rule))
 
-    print(json.dumps({
+                    framework = get_framework(path, rule)
+                    lang = get_lang(path)
+                    cwe = get_cwe(rule)
+                    owasp = get_owasp(rule)
+                    for owasp_standard in owasp:
+                        owasp_matrix[owasp_standard].append((path, rule))
+                        cwe_matrix[cwe].append((path, rule))
+                        owasp_by_lang_matrix[owasp_standard][lang].append((path, rule))
+                        cwe_by_lang_matrix[cwe][lang].append((path, rule))
+                        owasp_by_framework_matrix[owasp_standard][lang][framework].append((path, rule))
+                        cwe_by_framework_matrix[cwe][lang][framework].append((path, rule))
+
+    of = open('json_output.json', 'w')
+    of.write(json.dumps({
         "owasp": {
             "totals": {owasp: len(v) for owasp, v in sorted(owasp_matrix.items())},
             "per_framework": {owasp: {lang: {frm: len(v) for frm, v in owasp_by_framework_matrix[owasp][lang].items()} for lang in sorted(owasp_by_framework_matrix[owasp])} for owasp in sorted(owasp_by_framework_matrix)},
