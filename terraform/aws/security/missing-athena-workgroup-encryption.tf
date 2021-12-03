274d2097296787a546c8f746af26f2402a4ac48c
Ajurr <lytnaver@gmail.com>
2021-12-02 19:31:10 -0500

    Add new rule for unencrypted Athena Workgroups.

---
+++ terraform/aws/security/missing-athena-workgroup-encryption.tf
@@ -0,0 +1,31 @@
+# ruleid: missing-athena-workgroup-encryption
+resource "aws_athena_workgroup" "fail" {
+  name = "wg-non-encrypted"
+
+  configuration {
+    enforce_workgroup_configuration    = true
+    publish_cloudwatch_metrics_enabled = true
+
+    result_configuration {
+      output_location = "s3://mys3bucket"
+    }
+  }
+}
+
+# ok: missing-athena-workgroup-encryption
+resource "aws_athena_workgroup" "pass" {
+  name = "wg-encrypted"
+
+  configuration {
+    enforce_workgroup_configuration    = true
+    publish_cloudwatch_metrics_enabled = true
+
+    result_configuration {
+      output_location = "s3://mys3bucket"
+      encryption_configuration {
+        encryption_option = "SSE_KMS"
+        kms_key_arn       = "mykmsarn"
+      }
+    }
+  }
+}
