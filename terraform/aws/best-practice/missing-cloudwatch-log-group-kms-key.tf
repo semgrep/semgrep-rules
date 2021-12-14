01c2460c8485aeba2a7de3221bc1894d1930ca79
Ajurr <lytnaver@gmail.com>
2021-12-13 22:53:02 -0500

    Add new rule for missing KMS key in CloudWatch Log groups. (#1651)
    * Add new rule for missing KMS key in CloudWatch Log groups.
    
    * Update missing-cloudwatch-log-group-kms-key.yaml
    
    Co-authored-by: Grayson H <locrian.vii@gmail.com>

---
+++ terraform/aws/best-practice/missing-cloudwatch-log-group-kms-key.tf
@@ -0,0 +1,10 @@
+# ruleid: missing-cloudwatch-log-group-kms-key
+resource "aws_cloudwatch_log_group" "fail" {
+  retention_in_days = 1
+}
+
+# ok: missing-cloudwatch-log-group-kms-key
+resource "aws_cloudwatch_log_group" "pass" {
+  retention_in_days = 1
+  kms_key_id        = "someKey"
+}
