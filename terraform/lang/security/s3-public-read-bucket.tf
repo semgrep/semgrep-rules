ce4424b3ff86e6d512c84868926251bc10392499
grayson <grayson@r2c.dev>
2021-12-17 18:27:06 -0600

    This will intentionally fail

--- terraform/lang/security/s3-public-read-bucket.tf
+++ terraform/lang/security/s3-public-read-bucket.tf
@@ -22,7 +22,7 @@ resource "aws_s3_bucket" "b" {
 
 resource "aws_s3_bucket" "c" {
   bucket = "s3-website-test.hashicorp.com"
-  # ok: s3-public-read-bucket
+  # ruleid: s3-public-read-bucket
   acl    = "public-read"
   policy = file("policy.json")
 
