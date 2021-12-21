9c53da9498b0e4a71bcd9aa9ca17fcc1317d491e
Mert Coskuner <26259548+mertcancoskuner@users.noreply.github.com>
2021-12-21 21:05:27 +0000

    Adding migrated checkov rules to AWS terraform checks

---
+++ terraform/aws/best-practice/missing-aws-lb-deletion-protection.tf
@@ -0,0 +1,53 @@
+# pass
+
+resource "aws_lb" "enabled" {
+  internal           = false
+  load_balancer_type = "application"
+  name               = "alb"
+  subnets            = var.public_subnet_ids
+
+  enable_deletion_protection = true
+}
+
+resource "aws_alb" "enabled" {
+  internal           = false
+  load_balancer_type = "application"
+  name               = "alb"
+  subnets            = var.public_subnet_ids
+
+  enable_deletion_protection = true
+}
+
+# failure
+
+resource "aws_lb" "default" {
+  internal           = false
+  load_balancer_type = "application"
+  name               = "alb"
+  subnets            = var.public_subnet_ids
+}
+
+resource "aws_alb" "default" {
+  internal           = false
+  load_balancer_type = "application"
+  name               = "alb"
+  subnets            = var.public_subnet_ids
+}
+
+resource "aws_lb" "disabled" {
+  internal           = false
+  load_balancer_type = "application"
+  name               = "alb"
+  subnets            = var.public_subnet_ids
+
+  enable_deletion_protection = false
+}
+
+resource "aws_alb" "disabled" {
+  internal           = false
+  load_balancer_type = "application"
+  name               = "alb"
+  subnets            = var.public_subnet_ids
+
+  enable_deletion_protection = false
+}
