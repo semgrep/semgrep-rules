e513e1e08031654fcbae4ceaf689e5f93194533c
lejliu <14846866+lejliu@users.noreply.github.com>
2022-01-25 15:00:52 -0800

    Update dompurify.jsx
    Changed `okruleid` to `ok`

--- javascript/dompurify.jsx
+++ javascript/dompurify.jsx
@@ -7,18 +7,18 @@
  */
 
 // return a DOM HTMLBodyElement instead of an HTML string (default is false)
-// okruleid: harden-dompurify-usage
+// ok
 var clean = DOMPurify.sanitize(dirty, {RETURN_DOM: true});
 
 // return a DOM DocumentFragment instead of an HTML string (default is false)
-// okruleid: harden-dompurify-usage
+// ok
 var clean = DOMPurify.sanitize(dirty, {RETURN_DOM_FRAGMENT: true});
 
 // return a DOM DocumentFragment instead of an HTML string (default is false)
 // also import it into the current document (default is false).
 // RETURN_DOM_IMPORT must be set if you would like to append
 // the returned node to the current document
-// okruleid: harden-dompurify-usage
+// ok
 var clean = DOMPurify.sanitize(dirty, {RETURN_DOM_FRAGMENT: true, RETURN_DOM_IMPORT: true});
 document.body.appendChild(clean);
 
