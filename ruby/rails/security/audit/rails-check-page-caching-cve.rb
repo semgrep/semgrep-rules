f3c0231f86886d3aec2830d1b7f1cb92505fba09
Leland Liu <14846866+lejliu@users.noreply.github.com>
2022-01-31 18:22:12 -0800

    Added rules for Rails code to accompany Gemfile rules for page caching and render dos cve.

--- ruby/rails/security/audit/rails-check-page-caching-cve.rb
+++ ruby/rails/security/audit/rails-check-page-caching-cve.rb
@@ -3,6 +3,11 @@ class CachingController < ApplicationController
   caches_page :show
 end
 
+class CachingController2 < ApplicationController
+  # ruleid: rails-check-page-caching-cve
+  caches_page :uhoh
+end
+
 class SafeController < ApplicationController
   # ruleid: ok
   asdf :show
