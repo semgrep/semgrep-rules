1a66d15664b065e9e8457ac7cf8051fef5d61586
semgrep.dev <support@r2c.dev>
2022-02-04 00:18:40 +0000

    add .cs

---
+++ .cs
@@ -0,0 +1,10 @@
+private void somefunc(){
+   //ruleid: csharp-metavariable-compare
+   double foo = 0;
+   double bar = 1;
+   double biz; 
+   //ruleid: csharp-metavariable-compare
+   biz = 0;
+   double baz;
+   baz = 1;
+}
