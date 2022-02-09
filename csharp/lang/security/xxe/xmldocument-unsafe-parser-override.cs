ed8ca94f25bb9c9a40269ad06d92c6dfd5beb2ca
Kurt Boberg <kurt@returntocorp.com>
2022-02-09 15:41:50 -0800

    initial commit of standard lib XML parser XXE rules

---
+++ csharp/lang/security/xxe/xmldocument-unsafe-parser-override.cs
@@ -0,0 +1,21 @@
+public void LoadBad(string input)
+{
+  string fileName = @"C:\Users\user\Documents\test.xml";
+  XmlDocument xmlDoc = new XmlDocument();
+  xmlDoc.XmlResolver = new XmlUrlResolver();
+  // ruleid: xmldocument-unsafe-parser-override
+  xmlDoc.Load(input);
+  Console.WriteLine(xmlDoc.InnerText);
+
+    Console.ReadLine();
+}
+
+public void LoadBad(string input)
+{
+  XmlDocument xmlDoc = new XmlDocument();
+  // ok: xmldocument-unsafe-parser-override
+  xmlDoc.Load(input);
+  Console.WriteLine(xmlDoc.InnerText);
+
+    Console.ReadLine();
+}
