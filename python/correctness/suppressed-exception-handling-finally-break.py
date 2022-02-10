e913bb859d18bab87953b6d20b7d692826360049
Drew Dennison <drew@drewdennison.com>
2022-02-10 15:46:44 -0800

    Update suppressed-exception-handling-finally-break.py

--- python/correctness/suppressed-exception-handling-finally-break.py
+++ python/correctness/suppressed-exception-handling-finally-break.py
@@ -9,3 +9,28 @@ try:
       break
 except ZeroDivisionError:
   print("outer ZeroDivisionError caught")
+  
+  
+try:
+  for i in range(3):
+    try:
+      1 / 0
+    except ZeroDivisionError:
+      raise ZeroDivisionError("Error: you're trying to divide by zero!")
+    finally:
+      print("finally executed")
+      continue
+except ZeroDivisionError:
+  print("outer ZeroDivisionError caught")
+  
+try:
+  for i in range(3):
+    try:
+      1 / 0
+    except ZeroDivisionError:
+      raise ZeroDivisionError("Error: you're trying to divide by zero!")
+    finally:
+      print("finally executed")
+      return None
+except ZeroDivisionError:
+  print("outer ZeroDivisionError caught")
