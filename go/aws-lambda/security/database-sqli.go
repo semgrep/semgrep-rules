171f4acbd14b5b020960fec77993240a3d0f3562
Vasilii Ermilov <inkz@xakep.ru>
2022-02-12 14:48:01 +1000

    Update database-sqli.go

--- go/aws-lambda/security/database-sqli.go
+++ go/aws-lambda/security/database-sqli.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"log"
 	"os"
+	"context"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -81,6 +82,39 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 	}, nil
 }
 
+func HandleRequest(ctx context.Context, name MyEvent) (string, error) {
+	searchCriteria := context.Smth
+
+	db, err = sql.Open("mysql", dataSource)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer db.Close()
+
+  	// ok: database-sqli
+	results, err := db.Query("select e.emp_no, e.first_name, e.last_name " +
+		"from employees e, departments d, dept_emp de " +
+		"where de.emp_no = e.emp_no " +
+		"and de.dept_no = d.dept_no " +
+		"and d.dept_name = 'Marketing' " +
+		"and e.last_name LIKE '" + searchCriteria + "%';")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer results.Close()
+
+	data, _ := json.Marshal(results)
+
+	return events.APIGatewayProxyResponse{
+		StatusCode:      200,
+		Body:            string(data),
+		IsBase64Encoded: false,
+	}, nil
+}
+
 func main() {
 	lambda.Start(handler)
+	lambda.Start(HandleRequest)
 }
