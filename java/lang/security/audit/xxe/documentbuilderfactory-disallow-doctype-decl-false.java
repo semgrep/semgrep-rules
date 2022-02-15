8b5d0a61069070c448309fca2c72a48189179d6c
Pieter De Cremer <pieter@returntocorp.com>
2022-02-15 13:35:52 +0100

    added xml tag to techonology in xxe rules, updated disallow-doctype-decl-false rule to avoid FP case identified by kurt

--- java/lang/security/audit/xxe/documentbuilderfactory-disallow-doctype-decl-false.java
+++ java/lang/security/audit/xxe/documentbuilderfactory-disallow-doctype-decl-false.java
@@ -11,6 +11,39 @@ class GoodDocumentBuilderFactory {
         //ok:documentbuilderfactory-disallow-doctype-decl-false
         dbf.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
     }
+
+    public void GoodXMLInputFactory2() throws  ParserConfigurationException {
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        //ok:documentbuilderfactory-disallow-doctype-decl-false
+        dbf.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+        dbf.setFeature("http://xml.org/sax/features/external-general-entities", false);
+        dbf.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
+    }
+
+    public void GoodXMLInputFactory3() throws  ParserConfigurationException {
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        //ok:documentbuilderfactory-disallow-doctype-decl-false
+        dbf.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+        dbf.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
+        dbf.setFeature("http://xml.org/sax/features/external-general-entities", false);
+    }
+
+    public void GoodXMLInputFactory4() throws  ParserConfigurationException {
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        //ok:documentbuilderfactory-disallow-doctype-decl-false
+        dbf.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+        dbf.setAttribute(XMLConstants.ACCESS_EXTERNAL_SCHEMA, "");
+        dbf.setAttribute(XMLConstants.ACCESS_EXTERNAL_DTD, "");
+    }
+
+    public void GoodXMLInputFactory4() throws  ParserConfigurationException {
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        //ok:documentbuilderfactory-disallow-doctype-decl-false
+        dbf.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+        dbf.setAttribute(XMLConstants.ACCESS_EXTERNAL_DTD, "");
+        dbf.setAttribute(XMLConstants.ACCESS_EXTERNAL_SCHEMA, "");
+    }
+
 }
 
 class BadDocumentBuilderFactory{
