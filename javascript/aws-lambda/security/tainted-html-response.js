aee9d48858e946cdc345d29fdef198f1dadb34ce
Vasilii <inkz@xakep.ru>
2022-02-23 16:24:43 +1000

    update tainted-html-response rule

--- javascript/aws-lambda/security/tainted-html-response.js
+++ javascript/aws-lambda/security/tainted-html-response.js
@@ -1,13 +1,19 @@
 exports.handler = function (event, context) {
     const html = `<div>${event.name}</div>`;
 
+    const someRandomStuff = {
+        // ok: tainted-html-response
+        data: event.foo
+    }
+    bar(someRandomStuff)
+
     const response = {
         statusCode: 200,
+        // ruleid: tainted-html-response
+        body: html,
         headers: {
             'Content-Type': 'text/html',
-        },
-        // ruleid: tainted-html-response
-        body: html
+        }
     };
 
     return response
