8fed82118afd229f8793f97046a653671183bbc6
Kurt Boberg <kurt@returntocorp.com>
2022-02-24 15:34:29 -0800

    autogeneration script + autogenerated rules

---
+++ yaml/semgrep/consistency/generate_rules.py
@@ -0,0 +1,56 @@
+from pdb import runeval
+import requests
+import yaml
+import os
+
+def fetch_lang_data():
+    print('fetching latest lang configs..')
+    lang_url = "https://raw.githubusercontent.com/returntocorp/semgrep-langs/main/lang.json"
+    return requests.get(lang_url).json()
+
+def generate_rule(lang_data):
+    alt_names = list(filter(lambda x: x != lang_data['id'], lang_data['keys']))
+    rule_obj = None
+    if len(alt_names) > 0:
+        key_string = '|'.join(alt_names)
+        rule_obj = {
+            "rules":[
+                {
+                    "id":f"lang-consistency-{lang_data['id']}",
+                    "patterns":[
+                        {
+                            "pattern" : "$X"
+                        },
+                        {"pattern-inside":"languages: ..."},
+                        {
+                            "metavariable-regex": {
+                                "metavariable":"$X",
+                                "regex":f"^({key_string})$"
+                            }
+                        }
+                    ],
+                    "message": "Found '$X' in language config which diverges from semgrep.dev normalization.",
+                    "languages":["yaml"],
+                    "severity":"WARNING",
+                    "fix":lang_data['id'],
+                    "metadata":{
+                        "category":"correctness",
+                        "technology":["semgrep"]
+                    }
+                }
+            ]
+        }
+    return rule_obj
+
+def write_rule(rule_obj,target_dir="lang_rules"):
+    if not os.path.exists(target_dir):
+        os.mkdir(target_dir)
+    with open(os.path.join(target_dir,f"{rule_obj['rules'][0]['id']}.yaml"),"w") as f:
+        f.writelines(yaml.safe_dump(rule_obj))
+
+if __name__ == "__main__":
+    lang_data = fetch_lang_data()
+    for lang in lang_data:
+        rule = generate_rule(lang)
+        if rule:
+            write_rule(rule,".")
