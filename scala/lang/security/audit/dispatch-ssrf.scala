6f115d161b007fe65c6e757f6fdaff4a3136a6fd
Vasilii <inkz@xakep.ru>
2022-03-03 22:00:38 +1000

    fix test for dispatch-ssrf

--- scala/lang/security/audit/dispatch-ssrf.scala
+++ scala/lang/security/audit/dispatch-ssrf.scala
@@ -8,14 +8,14 @@ import Defaults._
 
 object Smth {
   def call1(request_url: String): Future[Unit] = {
-    // ruleid: scalaj-http-ssrf
+    // ruleid: dispatch-ssrf
     val req = url(request_url)
     val data = Http.default(req OK as.String)
     data
   }
 
   def call2(): Future[Unit] = {
-    // ok: scalaj-http-ssrf
+    // ok: dispatch-ssrf
     val req = url("https://www.google.com")
     val data = Http.default(req OK as.String)
     data
@@ -24,13 +24,14 @@ object Smth {
 
 object FooBar {
   def call1(request_url: String): Future[Unit] = {
+    // ruleid: dispatch-ssrf
     val request = url(request_url).POST.setHeader("Content-Type", "application/json")
     val res = Http(request OK as.String)
     res
   }
 
   def call2(): Future[Unit] = {
-    // ok: scalaj-http-ssrf
+    // ok: dispatch-ssrf
     val request = url("https://www.google.com").POST.setHeader("Content-Type", "application/json")
     val res = Http(request OK as.String)
     res
@@ -44,14 +45,14 @@ class HomeController @Inject()(
 ) extends BaseController {
 
   def req1(request_url: String) = Action.async { implicit request: Request[AnyContent] =>
-    // ruleid: scalaj-http-ssrf
+    // ruleid: dispatch-ssrf
     val request = url(request_url).POST.setHeader("Content-Type", "application/json")
     val res = Http(request OK as.String)
     Ok("ok")
   }
 
   def req2() = Action.async { implicit request: Request[AnyContent] =>
-    // ok: scalaj-http-ssrf
+    // ok: dispatch-ssrf
     val request = url("https://www.google.com").POST.setHeader("Content-Type", "application/json")
     val res = Http(request OK as.String)
     Ok("ok")
