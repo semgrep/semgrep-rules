cd6d848e17a2093365dc630a596317d8a758ad47
Leland Liu <14846866+lejliu@users.noreply.github.com>
2022-03-16 15:03:53 -0700

    Fixed ruleid

--- javascript/express/security/express-insecure-template-usage.jsx
+++ javascript/express/security/express-insecure-template-usage.jsx
@@ -15,32 +15,32 @@ var app = express();
 
 app.get('/', function(req, res) {
     let tainted = req.query.id;
-    // express-insecure-template-usage
+    // ruleid: express-insecure-template-usage
     pug.compile(tainted); 
-    // express-insecure-template-usage
+    // ruleid: express-insecure-template-usage
     pug.render(tainted); 
-    // express-insecure-template-usage
+    // ruleid: express-insecure-template-usage
     jade.compile(tainted);
-    // express-insecure-template-usage
+    // ruleid: express-insecure-template-usage
     jade.render(tainted);
-     // express-insecure-template-usage
+     // ruleid: express-insecure-template-usage
     dot.template(tainted); 
-    // express-insecure-template-usage
+    // ruleid: express-insecure-template-usage
     ejs.render(tainted); 
-    // express-insecure-template-usage
+    // ruleid: express-insecure-template-usage
     nunjucks.renderString(tainted); 
-    // express-insecure-template-usage
+    // ruleid: express-insecure-template-usage
     lodash.template(tainted); 
-    // express-insecure-template-usage
+    // ruleid: express-insecure-template-usage
     dot.compile(tainted); 
-    // express-insecure-template-usage
+    // ruleid: express-insecure-template-usage
     handlebars.compile(req.query.id); 
-    // express-insecure-template-usage
+    // ruleid: express-insecure-template-usage
     mustache.render(req.body._); 
-    // express-insecure-template-usage
+    // ruleid: express-insecure-template-usage
     Hogan.compile(tainted); 
-    // express-insecure-template-usage
+    // ruleid: express-insecure-template-usage
     Eta.render(tainted); 
-    // express-insecure-template-usage
+    // ruleid: express-insecure-template-usage
     Sqrl.render(tainted); 
 });
