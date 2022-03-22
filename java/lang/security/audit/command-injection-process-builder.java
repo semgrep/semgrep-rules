e08ec72d2b171c1a856a9eee3bba27493f520d2f
Pieter De Cremer <pieter@returntocorp.com>
2022-03-22 08:11:14 +0100

    fix mistake in test case

--- java/lang/security/audit/command-injection-process-builder.java
+++ java/lang/security/audit/command-injection-process-builder.java
@@ -43,7 +43,7 @@ public class TestExecutor {
     public String okTest() {
       ProcessBuilder builder = new ProcessBuilder();
       // ok: command-injection-process-builder
-      builder.command("bash", "/c", "ls");
+      builder.command("bash", "-c", "ls");
       return "foo";
     }
 
