7fa2c519f6b3e0e408fa57d5690386fe29dbd96c
Pieter De Cremer <pieter@r2c.dev>
2022-04-04 15:17:47 +0200

    revert to 92efedd6efe3f9c63962edc1ace935906b8fd65a

---
+++ python/jinja2/security/audit/missing-autoescape-disabled.py
@@ -0,0 +1,46 @@
+# cf. https://github.com/PyCQA/bandit/blob/02bad2e42311f420aef52dcd9806d66516ef594d/examples/jinja2_templating.py
+
+import jinja2
+from jinja2 import Environment, select_autoescape
+templateLoader = jinja2.FileSystemLoader( searchpath="/" )
+something = ''
+
+#ok:missing-autoescape-disabled
+Environment(loader=templateLoader, load=templateLoader, autoescape=True)
+
+# ok:missing-autoescape-disabled
+templateEnv = jinja2.Environment(autoescape=True,
+        loader=templateLoader )
+
+# ok:missing-autoescape-disabled
+Environment(loader=templateLoader, load=templateLoader, autoescape=something)
+
+
+# ok:missing-autoescape-disabled
+templateEnv = jinja2.Environment(autoescape=False, loader=templateLoader )
+
+
+# ok:missing-autoescape-disabled
+Environment(loader=templateLoader,
+            load=templateLoader,
+            autoescape=False)
+
+
+# ruleid:missing-autoescape-disabled
+Environment(loader=templateLoader,
+            load=templateLoader)
+
+# ok:missing-autoescape-disabled
+Environment(loader=templateLoader, autoescape=select_autoescape())
+
+# ok:missing-autoescape-disabled
+Environment(loader=templateLoader,
+            autoescape=select_autoescape(['html', 'htm', 'xml']))
+
+
+def fake_func():
+    return 'foobar'
+
+
+# ok:missing-autoescape-disabled
+Environment(loader=templateLoader, autoescape=fake_func())
