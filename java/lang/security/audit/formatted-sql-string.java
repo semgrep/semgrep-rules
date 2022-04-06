6e83cef5f511f30b0d367a7da53e47842427e73b
Colleen Dai <colleen@returntocorp.com>
2022-04-06 16:01:53 -0700

    adding more test cases

--- java/lang/security/audit/formatted-sql-string.java
+++ java/lang/security/audit/formatted-sql-string.java
@@ -27,23 +27,23 @@ public class SqlExample {
     }
 
     public void findAccountsById(String id) throws SQLException {
-        // ruleid:formatted-sql-string
         String sql = "SELECT * "
             + "FROM accounts WHERE id = '"
             + id
             + "'";
         Connection c = DB.getConnection();
+        // ruleid:formatted-sql-string
         ResultSet rs = c.createStatement().executeQuery(sql);
     }
 
     public void findAccountsById(String id, String field) throws SQLException {
-        // ruleid:formatted-sql-string
         String sql = "SELECT ";
         sql += field;
         sql += " FROM accounts WHERE id = '";
         sql += id;
         sql += "'";
         Connection c = DB.getConnection();
+        // ruleid:formatted-sql-string
         ResultSet rs = c.createStatement().executeQuery(sql);
     }
 }
@@ -56,18 +56,18 @@ public class SqlExample2 {
     }
 
     public void findAccountsById(String id) throws SQLException {
-        // ruleid:formatted-sql-string
         String sql = "SELECT * "
             + "FROM accounts WHERE id = '"
             + id
             + "'";
         Connection c = db.getConnection();
+        // ruleid:formatted-sql-string
         ResultSet rs = c.createStatement().execute(sql);
     }
 
     public List<AccountDTO> findAccountsById(String id) {
-        // ruleid:formatted-sql-string
         String jql = "from Account where id = '" + id + "'";
+        // ruleid:formatted-sql-string
         TypedQuery<Account> q = em.createQuery(jql, Account.class);
         return q.getResultList()
         .stream()
@@ -76,7 +76,7 @@ public class SqlExample2 {
     }
 }
 
-public class SqlExample {
+public class SQLExample3 {
     public void getAllFields(String tableName) throws SQLException {
         Connection c = db.getConnection();
         // ruleid:formatted-sql-string
@@ -84,21 +84,30 @@ public class SqlExample {
     }
 
     public void findAccountsById(String id) throws SQLException {
-        // ruleid:formatted-sql-string
         String sql = String.format("SELECT * FROM accounts WHERE id = '%s'", id);
         Connection c = db.getConnection();
+        // ruleid:formatted-sql-string
         ResultSet rs = c.createStatement().execute(sql);
     }
 
-
     public List<AccountDTO> findAccountsById(String id) {
         String jql = String.format("from Account where id = '%s'", id);
+        // ruleid: formatted-sql-string
         TypedQuery<Account> q = em.createQuery(jql, Account.class);
         return q.getResultList()
         .stream()
         .map(this::toAccountDTO)
         .collect(Collectors.toList());
     }
+
+    public void findAccountsByIdOk() throws SQLException {
+        String id = "const"
+        String sql = String.format("SELECT * FROM accounts WHERE id = '%s'", id);
+        Connection c = db.getConnection();
+        // ok:formatted-sql-string
+        ResultSet rs = c.createStatement().execute(sql);
+    }
+
 }
 
 public class tableConcatStatements {
