673be0eb5816bb0d98b4fd18f78ee4280f769b9e
Mert <mcoskuner@windowslive.com>
2022-04-07 00:26:56 +0300

    Fixed ruleid line for TLS check

--- terraform/azure/security/appservice/azure-appservice-min-tls-version.tf
+++ terraform/azure/security/appservice/azure-appservice-min-tls-version.tf
@@ -1,11 +1,11 @@
 # fail
-# ruleid: azure-appservice-min-tls-version
 resource "azurerm_app_service" "example" {
   name                = "example-app-service"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   app_service_plan_id = azurerm_app_service_plan.example.id
   site_config {
+    # ruleid: azure-appservice-min-tls-version
     min_tls_version = "1.1"
   }
 }
