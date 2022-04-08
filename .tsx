ccd9c3db9a4bf1dc6e08c3552accb8bb26f7b8a4
Michael Musil <infi.pc@gmail.com>
2022-04-08 06:49:59 +0000

    add .tsx

---
+++ .tsx
@@ -0,0 +1,17 @@
+const btn = <Button
+  intent={Intent.PRIMARY}
+>
+  boo
+</Button>
+
+const ok = <Button
+  intent="primary"
+>
+  boo
+</Button>
+
+const noIntent = <Button
+  boo="xxx"
+>
+  boo
+</Button>
