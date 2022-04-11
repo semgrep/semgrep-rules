d211cc3d96a1adc91f601dceb74cbe0887765a28
grayson <grayson@r2c.dev>
2022-04-11 13:49:27 -0500

    Use metavariable-regex plus taint mode to limit Ruby's divide-by-zero rule to integers. In Ruby, floats divided by zero are idiomatic for 'Infinity'. This should filter out that case.

--- ruby/lang/security/divide-by-zero.rb
+++ ruby/lang/security/divide-by-zero.rb
@@ -1,13 +1,19 @@
  def divide_by_zero
-    # ruleid: divide-by-zero
-    3/0
-    # ruleid: divide-by-zero
-    oops = 4/0
-    variable = 3
-    # ruleid: divide-by-zero
-    oops = variable / 0
-    # ruleid: divide-by-zero
-    zero = 0
-    # ruleid: divide-by-zero
-    bad = variable/zero
+   # ruleid: divide-by-zero
+   3/0
+   # ruleid: divide-by-zero
+   oops = 4/0
+   variable = 3
+   # ruleid: divide-by-zero
+   oops = variable / 0
+
+   zero = 0
+   # ruleid: divide-by-zero
+   bad = variable/zero
+
+   # ok: divide-by-zero
+   ok = 1.0 / 0
+   # ok: divide-by-zero
+   ok2 = 2.0 / zero
+   
  end
