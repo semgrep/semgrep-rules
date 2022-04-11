faa5a5c6171b9097afa6c52a2ad43416329856a0
grayson <grayson@r2c.dev>
2022-04-11 17:41:22 -0500

    Add example based on https://brakemanscanner.org/docs/warning_types/unscoped_find/

--- ruby/rails/security/brakeman/check-unscoped-find.rb
+++ ruby/rails/security/brakeman/check-unscoped-find.rb
@@ -20,6 +20,18 @@ class GroupsController < ApplicationController
     end
   end
 
+  def show_ok2
+    #ok: check-unscoped-find
+    current_user = User.find(session[:id])
+    #ok: check-unscoped-find
+    current_user.accounts.find(param[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render :json => @user }
+    end
+  end
+
   def get
     #ruleid: check-unscoped-find
     @some_record = SomeRecord.find_by_id!(params[:id])
