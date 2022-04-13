90cb13cfc604df7c0067dcbb899219182b4f25be
Sjoerd Langkemper <sjoerd-github@linuxonly.nl>
2022-04-13 14:06:17 +0200

    Let detect-insecure-websocket match word boundary
    So that it matches ws://, and not anythingws://.
    
    Closes #2004.

--- javascript/lang/security/detect-insecure-websocket.js
+++ javascript/lang/security/detect-insecure-websocket.js
@@ -3,3 +3,18 @@ var scheme   = "ws://";
 var uri      = scheme + window.document.location.host + "/";
 var ws       = new WebSocket(uri);
 ws.onmessage = function(message) {}
+
+
+// at start of the line
+// ruleid: detect-insecure-websocket
+ws://foo/bar
+
+// ok: detect-insecure-websocket
+var secure_url = "wss://my/url";
+
+// ok: detect-insecure-websocket
+var amazon_url = "aws://my/url";
+
+// from https://github.com/ytdl-org/youtube-dl/blob/master/youtube_dl/extractor/yahoo.py#L124
+// ok: detect-insecure-websocket
+// # ytwnews://cavideo/
