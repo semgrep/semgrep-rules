c9bdf384106c85d11412fb23e4470266c3cb17a6
Vasilii <inkz@xakep.ru>
2022-04-20 20:35:14 +1000

    rename dynamodb-request-object to dynamodb-filter-injection

--- python/aws-lambda/security/dynamodb-filter-injection.py
+++ python/aws-lambda/security/dynamodb-filter-injection.py
@@ -5,13 +5,13 @@ def handler(event, context):
   dynamodb_table = dynamodb.Table('Name')
   dynamodb_table.query(
     Select = 'ALL_ATTRIBUTES',
-    # ruleid: dynamodb-request-object
+    # ruleid: dynamodb-filter-injection
     QueryFilter = event.body.filter
   )
 
   dynamodb_table.scan(
     Select = 'ALL_ATTRIBUTES', 
-    # ok: dynamodb-request-object
+    # ok: dynamodb-filter-injection
     ScanFilter = {'first_name': event.body.name}
   )
 
@@ -20,12 +20,12 @@ def new_handler(event, context):
 
   client.scan(
     Select = 'ALL_ATTRIBUTES', 
-    # ruleid: dynamodb-request-object
+    # ruleid: dynamodb-filter-injection
     ScanFilter = event.body.filter
   )
 
   client.query(
     Select = 'ALL_ATTRIBUTES', 
-    # ok: dynamodb-request-object
+    # ok: dynamodb-filter-injection
     QueryFilter = {'first_name': event.body.name}
   )
