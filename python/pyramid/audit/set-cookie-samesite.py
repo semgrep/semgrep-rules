cd531963b99c5bc58da23cea790e2e7a56c4b772
Claudio <claudio@r2c.dev>
2022-04-20 15:10:45 +0200

    Fix pyramid-set-cookie-samesite for default value

--- python/pyramid/audit/set-cookie-samesite.py
+++ python/pyramid/audit/set-cookie-samesite.py
@@ -22,6 +22,23 @@ def my_bad_view2(request):
     return resp
 
 
+@view_config(route_name='my_view')
+def my_bad_view3(request):
+    response = request.response
+    # ruleid: pyramid-set-cookie-samesite
+    response.set_cookie('MY_COOKIE', value='MY_COOKIE_VALUE',
+                        secure=True, httponly=True)
+    return {'foo': 'bar'}
+
+
+@view_config(route_name='my_view')
+def my_bad_view4(request):
+    resp = exc.HTTPFound(location=request.referer or request.application_url)
+    # ruleid: pyramid-set-cookie-samesite
+    resp.set_cookie('MY_COOKIE', secure=True, httponly=True)
+    return resp
+
+
 ### True negatives ###
 
 @view_config(route_name='my_view')
@@ -34,19 +51,10 @@ def my_good_view1(request):
 
 
 @view_config(route_name='my_view')
-def my_good_view2(request):
-    response = request.response
-    # ok: pyramid-set-cookie-samesite
-    response.set_cookie('MY_COOKIE', value='MY_COOKIE_VALUE',
-                        secure=True, httponly=True)
-    return {'foo': 'bar'}
-
-
-@view_config(route_name='my_view')
 def my_good_view3(request):
     resp = exc.HTTPFound(location=request.referer or request.application_url)
     # ok: pyramid-set-cookie-samesite
-    resp.set_cookie('MY_COOKIE', secure=True, httponly=True)
+    resp.set_cookie('MY_COOKIE', secure=True, httponly=True, samesite='Lax')
     return resp
 
 
