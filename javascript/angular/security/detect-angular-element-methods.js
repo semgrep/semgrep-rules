40224674858f9f7eb2957fa7a606771ddf6cdcd0
Lewis Ardern <lewis@Lewiss-MBP.lan>
2022-04-25 11:24:27 -0700

    fix angularjs methods and add new taint rule

--- javascript/angular/security/detect-angular-element-methods.js
+++ javascript/angular/security/detect-angular-element-methods.js
@@ -1,18 +1,11 @@
 var app = angular.module('MyApp', []);
-app.controller('myCtrl', function($scope) {
+app.controller('myCtrl', function($scope,$sanitize) {
+    $scope.value = grabInput()
+    $rootScope.value = grabInput()
     // ruleid: detect-angular-element-methods
-    var now = angular.element($scope.input).html();
+    angular.element('div').html($scope.value)
+    // ok: detect-angular-element-methods
+    angular.element('div').html('hi')
     // ruleid: detect-angular-element-methods
-    var now = angular.element($scope.input).append("foo");
-    // ruleid: detect-angular-element-methods
-    var now = angular.element($scope.input).prepend("foo");
-    // ruleid: detect-angular-element-methods
-    var now = angular.element($scope.input).wrap();
-
-    var el = angular.element($scope.input);
-    // ruleid: detect-angular-element-methods
-    var now = el.html();
-
-    return now;
-
+    angular.element('div').html($rootScope.value)
 });
---
+++ javascript/angular/security/detect-angular-element-taint.js
@@ -0,0 +1,12 @@
+var app = angular.module('MyApp', []);
+app.controller('myCtrl', function($scope,$sanitize) {
+    let a = unescape(window.location.href)
+    // ruleid: detect-angular-element-taint
+    angular.element('div').html(a)
+    let b = $sanitize(unescape(window.location.href))
+    // ok: detect-angular-element-taint
+    angular.element('div').html(b)
+    // ruleid: detect-angular-element-taint
+    angular.element('div').html((new URLSearchParams(window.location.search)).get('returnUrl'))
+
+});
