ebf2c61f6cf322bc4a4e2e634209a68eff3098f2
Claudio <claudio@r2c.dev>
2022-05-10 16:36:35 +0200

    Fix use-timeout

--- python/requests/best-practice/use-timeout.py
+++ python/requests/best-practice/use-timeout.py
@@ -11,13 +11,16 @@ r = requests.post(url)
 # ok: use-timeout
 r = requests.get(url, timeout=50)
 
+
 def from_import_test1(url):
     from requests import get, post
+
     # ok: use-timeout
     r = get(url, timeout=3)
 
     # ruleid: use-timeout
     r = post(url)
+
 
 def test2():
     """Perform a requests.get and default headers set"""
@@ -28,9 +31,10 @@ def test2():
     )
     return r
 
+
 def test3():
-    # ruleid: use-timeout
     session = requests.Session()
+    # ruleid: use-timeout
     r = session.get(url)
 
     # ok: use-timeout
