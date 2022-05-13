2bf09adc4de3434dbe2ef6e9f874baab1debc5f1
enncoded <14846866+enncoded@users.noreply.github.com>
2022-05-13 15:31:15 -0700

    Fixed serve-index rule and example

--- javascript/express/security/audit/express-check-directory-listing.js
+++ javascript/express/security/audit/express-check-directory-listing.js
@@ -1,12 +1,26 @@
 const serveIndex = require('serve-index');
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+
+var serve = serveIndex('public/ftp', {
+    icons: true
+})
+
+var server = http.createServer(function onRequest(req, res) {
+    var done = finalhandler(req, res)
+    // ruleid: express-check-directory-listing
+    serve(req, res, function onNext(err) {
+        if (err) return done(err)
+        index(req, res, done)
+    })
+})
 
 // ruleid: express-check-directory-listing
-app.use('/ftp', serveIndexMiddleware, serveIndex('ftp', { icons: true }));
-
-// ruleid: express-check-directory-listing
-app.use('/ftp', serveIndex('ftp', { icons: true }));
+app.use('/ftp', serveIndex('ftp', {
+    icons: true
+}));
 
 // ok: express-check-directory-listing
-app.use(bodyParser.text({ type: '*/*' }));
+app.use(bodyParser.text({
+    type: '*/*'
+}));
