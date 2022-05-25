8e7deb72873d5969b92f713d48b0e69d5441caef
Pieter De Cremer <pieter@r2c.dev>
2022-05-25 14:40:36 -0700

    SSRF rule

---
+++ java/lang/security/tainted-ssrf.java
@@ -0,0 +1,46 @@
+
+package org.owasp.benchmark.testcode;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(value = "/cmdi-00/BenchmarkTest00006")
+public class bad1 extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doPost(request, response);
+    }
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // some code
+        response.setContentType("text/html;charset=UTF-8");
+
+        String param = "";
+        if (request.getHeader("BenchmarkTest00006") != null) {
+            param = request.getHeader("BenchmarkTest00006");
+        }
+
+        // URL Decode the header value since req.getHeader() doesn't. Unlike req.getParameter().
+        param = java.net.URLDecoder.decode(param, "UTF-8");
+
+        String url = "https://" + param;
+
+        // ruleid: tainted-ssrf
+        new URL(String url).openConnection();
+        // ruleid: tainted-ssrf
+        new URL(String url).openStream();
+        // ruleid: tainted-ssrf
+        new URL(String url).getContent();
+
+    }
+
