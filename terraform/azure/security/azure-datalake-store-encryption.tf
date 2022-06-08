246d451a980d9a4badbc78bcebc87b00265b571a
Colleen Dai <colleen@returntocorp.com>
2022-06-08 14:56:06 -0700

    edits

--- terraform/azure/security/azure-datalake-store-encryption.tf
+++ terraform/azure/security/azure-datalake-store-encryption.tf
@@ -8,7 +8,7 @@ resource "azurerm_data_lake_store" "example" {
     encryption_state = "Disabled"
 }
 
-# pass
+# ruleid: azure-datalake-store-encryption
 resource "azurerm_data_lake_store" "example" {
     name                = "consumptiondatalake"
     location            = azurerm_resource_group.example.location
