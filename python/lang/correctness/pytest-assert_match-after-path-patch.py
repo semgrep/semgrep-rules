41bb592ca73753efb48f7a71e717cf808d560e3f
Matthew McQuaid <matthew@returntocorp.com>
2022-06-08 11:55:03 -0700

    add rule

---
+++ python/lang/correctness/pytest-assert_match-after-path-patch.py
@@ -0,0 +1,12 @@
+import pytest
+
+@pytest.mark.quick
+def test_foo(snapshot, mocker):
+    mocker.patch.object(Path, "open", mocker.mock_open(read_data=file_content))
+    #ruleid: pytest-assert_match-after-path-patch
+    snapshot.assert_match(foo(), "results.json")
+
+@pytest.mark.quick
+def test_bar(snapshot, mocker):
+    #ok: pytest-assert_match-after-path-patch
+    snapshot.assert_match(foo(), "results.json")
