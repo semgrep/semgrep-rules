9370e248971786b45efaae073d1748df3feb6caf
grayson <grayson@r2c.dev>
2022-06-15 01:07:38 -0500

    Change ruleid test annotations to ok

--- typescript/react/security/audit/react-href-var.tsx
+++ typescript/react/security/audit/react-href-var.tsx
@@ -3,14 +3,14 @@ const a = <a className={"foobar"} href={"http://www.example.com"}></a>;
 // ok: react-href-var
 const b = <a className={"foobar"} href="http://www.example.com"></a>;
 
-// ruleid: react-href-var
+// ok: react-href-var
 let x = <a className={"foobar"} href={input}></a>;
 
-// ruleid: react-href-var
+// ok: react-href-var
 let zzz = <Foo className={"foobar"} href={input} />;
 
 function test1(input) {
-// ruleid: react-href-var
+// ok: react-href-var
   const params = {href: input};
   return React.createElement("a", params);
 }
