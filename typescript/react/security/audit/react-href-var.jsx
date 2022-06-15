4239b32ea977faff81bf4a49406af5badf278153
grayson <grayson@r2c.dev>
2022-06-15 01:21:09 -0500

    Add back in test annotations for react-href-var

--- typescript/react/security/audit/react-href-var.jsx
+++ typescript/react/security/audit/react-href-var.jsx
@@ -3,14 +3,14 @@ const a = <a className={"foobar"} href={"http://www.example.com"}></a>;
 // ok: react-href-var
 const b = <a className={"foobar"} href="http://www.example.com"></a>;
 
-// ok: react-href-var
+// ruleid: react-href-var
 let x = <a className={"foobar"} href={input}></a>;
 
-// ok: react-href-var
+// ruleid: react-href-var
 let zzz = <Foo className={"foobar"} href={input} />;
 
 function test1(input) {
-// ok: react-href-var
+// ruleid: react-href-var
   const params = {href: input};
   return React.createElement("a", params);
 }
