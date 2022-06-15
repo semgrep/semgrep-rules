020d296fa66399617756949aa480b69585fa4ec9
LewisArdern <LewisArdern@live.co.uk>
2022-06-15 09:24:58 -0700

    add blackmagic for improve source/sink detection

--- typescript/react/security/audit/react-dangerouslysetinnerhtml.tsx
+++ typescript/react/security/audit/react-dangerouslysetinnerhtml.tsx
@@ -11,30 +11,42 @@ function TestComponent2() {
   return <li className={"foobar"} dangerouslySetInnerHTML={createMarkup()} />;
 }
 
+function TestComponent3() {
+    // ruleid:react-dangerouslysetinnerhtml
+  return <li className={"foobar"} dangerouslySetInnerHTML={{__html: foo}} />;
+}
+
 
 function OkComponent1() {
+    // ok:react-dangerouslysetinnerhtml
+  return <li className={"foobar"} dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(foo)}} />;
+}
+
+
+
+function OkComponent2() {
     // ok:react-dangerouslysetinnerhtml
   return <li className={"foobar"} dangerouslySetInnerHTML={DOMPurify.sanitize(createMarkup())} />;
 }
 
-function OkComponent2() {
+function OkComponent3() {
     // ok:react-dangerouslysetinnerhtml
     let params = {smth: 'test123', dangerouslySetInnerHTML: {__html: sanitize(foo)},a:b};
     return React.createElement('div', params);
 }
 
-function OkComponent3() {
+function OkComponent4() {
     // ok:react-dangerouslysetinnerhtml
     let params = {smth: 'test123', dangerouslySetInnerHTML: {__html: "hi"},a:b};
     return React.createElement('div', params);
 }
 
-function OkComponent4() {
+function OkComponent5() {
     // ok:react-dangerouslysetinnerhtml
   return <li class="foobar" selected={true} />;
 }
 
-function OkComponent5() {
+function OkComponent6() {
     // ok:react-dangerouslysetinnerhtml
     let params = {smth: "test123", style: {color: 'red'}};
     return React.createElement('div', params);
