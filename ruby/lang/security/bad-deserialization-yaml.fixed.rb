a3888347373bf3ccd1a3d1189fe5e5c8b7ea2b69
Pieter De Cremer <pieter@r2c.dev>
2022-06-20 09:51:11 +0200

    add fixtest for ruby/lang/security/bad-deserialization-yaml

---
+++ ruby/lang/security/bad-deserialization-yaml.fixed.rb
@@ -0,0 +1,23 @@
+ def bad_deserialization
+
+    o = Klass.new("hello\n")
+    data = YAML.dump(o)
+    # ruleid: bad-deserialization-yaml
+    obj = Psych.safe_load(data)
+
+ end
+
+ def ok_deserialization
+    o = Klass.new("hello\n")
+    data = YAML.dump(o)
+    # ok: bad-deserialization-yaml
+    obj = YAML.load(data, safe: true)
+
+    filename = File.read("test.txt")
+    data = YAML.dump(filename)
+    # ok: bad-deserialization-yaml
+    YAML.load(filename)
+
+    # ok: bad-deserialization-yaml
+    YAML.load(File.read("test.txt"))
+ end
