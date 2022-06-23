1c1f601911006ba9062b80fa6f5b4ed0e6e752bd
LewisArdern <LewisArdern@live.co.uk>
2022-06-23 08:05:26 -0700

    update open redirect rules

--- javascript/browser/security/open-redirect-from-function.js
+++ javascript/browser/security/open-redirect-from-function.js
@@ -3,17 +3,17 @@ var hi = new URLSearchParams(window.location.search).get('gamer')
 var hi1 = new URLSearchParams(window.location.search)
 
 function test1(userInput) {
-    //ruleid:js-open-redirect
-    location.href = hi;
-    //ruleid:js-open-redirect
-    location.href = hi1.get('gamer');
+    //ruleid:js-open-redirect-from-function
+    location.href = userInput;
+    //ruleid:js-open-redirect-from-function
+    location.href = `${userInput}/hi`
 }
 
 
 function test4(userInput) {
-    // ok:js-open-redirect
+    // ok:js-open-redirect-from-function
     location.href = `https://www.hardcoded.place/${userInput}`
-    // ok:js-open-redirect
+    // ok:js-open-redirect-from-function
     location.href = "https://www.hardcoded.place/" + userInput;
 }
 
