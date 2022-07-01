93534d86e13baea2dc541e5f802031118073cd20
cikay <muzaffer123cikay@gmail.com>
2022-07-01 16:55:44 +0300

    Add dangerous system calls dynamically

--- python/lang/security/audit/dangerous-system-call.py
+++ python/lang/security/audit/dangerous-system-call.py
@@ -12,6 +12,32 @@ from somewhere import something
 os.system(something())
 
 # ruleid:dangerous-system-call
+getattr(os, "system")(something())
+
+# ruleid:dangerous-system-call
+dynamic_system_by_static_os = getattr(os, "system")
+dynamic_system_by_static_os(something())
+
+# ruleid:dangerous-system-call
+__import__("os").system(something())
+
+# ruleid:dangerous-system-call
+getattr(__import__("os"), "system")(something())
+
+# ruleid:dangerous-system-call
+dynamic_os = __import__("os")
+dynamic_os.system(something())
+
+# ruleid:dangerous-system-call
+dynamic_os = __import__("os")
+getattr(dynamic_os, "system")(something())
+
+# ruleid:dangerous-system-call
+dynamic_os = __import__("os")
+dynamic_system = getattr(dynamic_os, "system")
+dynamic_system(something())
+
+# ruleid:dangerous-system-call
 os.popen(something())
 
 # ruleid:dangerous-system-call
