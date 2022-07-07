b9ed7ad4ab187fbf620710d8200ba8c6b6fd6cfd
Tom Petr <trpetr@gmail.com>
2022-07-07 14:03:55 +0000

    add SteveBrulesSemgrepRule.py

---
+++ SteveBrulesSemgrepRule.py
@@ -0,0 +1,14 @@
+print("Welcome to Semgrep!" + "Use our Run button to start experimenting -->")
+
+
+print("...")
+
+# To detect ALL calls to the print() function, change the Semgrep Rule from print("...") to print(...)
+
+print(not_a_string)
+
+print(first_var, second_var)
+
+print()
+
+# print("This is commented out so it will never be found")
