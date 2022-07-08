4d36027441e7a3e05caf054836428a7205ef4983
Claudio <claudio@r2c.dev>
2022-07-08 19:45:34 +0200

    Restore previous ids for registry sanity

--- python/lang/maintainability/useless-literal.py
+++ python/lang/maintainability/useless-literal.py
@@ -1,23 +1,23 @@
-# ruleid: useless-literal-dict
+# ruleid: useless-literal
 d = dict((1, "a"), (2, "b"), (1, "a"))
 
 d = dict(
-    # ruleid: useless-literal-dict
+    # ruleid: useless-literal
     (1, "a"),
     (2, "b"),
     (1, "a"),
 )
 
-# ruleid: useless-literal-dict
+# ruleid: useless-literal
 d = {1: "a", 2: "b", 1: "a"}
 d = {
     1: "a",
     2: "b",
-    # ruleid: useless-literal-dict
+    # ruleid: useless-literal
     1: "a",
 }
-# ruleid: useless-literal-dict
+# ruleid: useless-literal
 d = {"a": 1, "a": 1}
 
-# ok: useless-literal-dict
+# ok: useless-literal
 d = {1: "a", 2: "b", 3: "a"}
