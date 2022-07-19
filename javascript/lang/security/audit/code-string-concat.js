4d829f29808e4bf26c49bc5b8c10fce0bd97ca34
kurt (sca-automation) <kurt@r2c.dev>
2022-07-19 13:53:31 -0700

    fix tests - were all using  which is...not real

--- javascript/lang/security/audit/code-string-concat.js
+++ javascript/lang/security/audit/code-string-concat.js
@@ -2,18 +2,18 @@ function test1(req,res) {
   const data = JSON.stringify(req.query.key);
   const command = `(secret) => {${data}}`
   // ruleid:code-string-concat
-  return exec(command)
+  return eval(command)
 }
 
 test2.post(foo, bar, function (req,res) {
   userInput = req.params.input
   var command = "new Function('"+userInput+"')";
   // ruleid:code-string-concat
-  return exec(command)
+  return eval(command)
 });
 
 function ok1(req,res) {
   var command = "eval('123')";
   // ok:code-string-concat
-  return exec(command)
+  return eval(command)
 }
