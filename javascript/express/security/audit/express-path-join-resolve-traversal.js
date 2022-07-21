7aa307a6546def1657ef737a65859c12e028cd98
Vasilii <inkz@xakep.ru>
2022-07-21 17:25:43 +1000

    update express-path-join-resolve-traversal rule

--- javascript/express/security/audit/express-path-join-resolve-traversal.js
+++ javascript/express/security/audit/express-path-join-resolve-traversal.js
@@ -17,24 +17,24 @@ app.post('/test2', function test2(req, res) {
 })
 
 function testCtrl3(req,res) {
-    // ruleid:express-path-join-resolve-traversal
     let somePath = req.body.path;
+    // ruleid:express-path-join-resolve-traversal
     const pth = path.join(opts.path, somePath);
     extractFile(pth);
     res.send('Hello World!');
 }
 
 const func4 = function testCtrl4(req,res) {
-    // ruleid:express-path-join-resolve-traversal
     let somePath = req.body.path;
+    // ruleid:express-path-join-resolve-traversal
     const pth = path.join(opts.path, somePath);
     extractFile(pth);
     res.send('Hello World!');
 }
 
 const func5 = function (req,res) {
-    // ruleid:express-path-join-resolve-traversal
     let somePath = req.body.path;
+    // ruleid:express-path-join-resolve-traversal
     const pth = path.join(opts.path, somePath);
     extractFile(pth);
     res.send('Hello World!');
@@ -42,12 +42,75 @@ const func5 = function (req,res) {
 
 app.post('/test3', testCtrl3)
 
-app.post('/okTest', function (req,res) {
-    // ok:express-path-join-resolve-traversal
-    createFile({
-        filePath: pth.join(opts.path, 'val')
+app.post('/test4', function (req,res) {
+    let data = req.body.path;
+    // ruleid:express-path-join-resolve-traversal
+    data.forEach((entry) => {
+        var pth = path.join(opts.path, entry);
+        doSmth(pth);
     })
-    res.send('Hello World!');
+})
+
+
+app.post('/test5', function (req,res) {
+    let data = req.body.path;
+    for (let i = 0; i < data.length; i++) {
+        // ruleid:express-path-join-resolve-traversal
+        var pth = path.join(opts.path, data[i]);
+        doSmth(pth);
+    }
+})
+
+app.post('/test6', function test6(req,res) {
+    let data = req.body.path;
+    for (let x of data) {
+        // ruleid:express-path-join-resolve-traversal
+        var pth = path.join(opts.path, x);
+        doSmth(pth);
+    }
+})
+
+app.post('/ok-test1', function okTest1(req,res) {
+    let data = ['one', 'two', 'three'];
+    for (let x of data) {
+        // ok:express-path-join-resolve-traversal
+        var pth = path.join(opts.path, x);
+        doSmth(pth);
+    }
+})
+
+app.post('/ok-test2', function okTest2() {
+    function someFunc() {
+        createFile({
+            // ok:express-path-join-resolve-traversal
+            filePath: path.join(__dirname, 'val')
+        })
+        return true
+    }
+    someFunc()
+})
+
+app.post('/ok-test3', function (req,res) {
+    let somePath = req.body.path;
+    somePath = somePath.replace(/^(\.\.(\/|\\|$))+/, '');
+    // ok:express-path-join-resolve-traversal
+    return path.join(opts.path, somePath);
+})
+
+app.post('/ok-test4', function (req,res) {
+    let somePath = sanitizer(req.body.path);
+    // ok:express-path-join-resolve-traversal
+    return path.join(opts.path, somePath);
+})
+
+app.post('/ok-test5', function okTest5(req,res) {
+    let somePath = req.body.path;
+    // ok:express-path-join-resolve-traversal
+    let result = path.join(opts.path, somePath);  
+    if (result.indexOf(opts.path) === 0) {
+        return path;
+    }
+    return null
 })
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
