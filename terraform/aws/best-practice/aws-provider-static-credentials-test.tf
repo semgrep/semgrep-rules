c8e16cf7f9bba19318cb56146f4bfb41bce67818
Pieter De Cremer <pieter@r2c.dev>
2022-07-26 09:49:26 +0200

    move files and fix ruleid in annotations

--- terraform/aws/best-practice/aws-provider-static-credentials-test.tf
+++ terraform/aws/best-practice/aws-provider-static-credentials-test.tf
@@ -1,4 +1,4 @@
-//ruleid: aws-provider-static-credentials
+//ruleid: aws-provider-static-credentials-test
 provider "aws" {
   region     = "us-west-2"
   access_key = "AKIAEXAMPLEKEY"
@@ -6,7 +6,7 @@ provider "aws" {
   profile = "customprofile"
 }
 
-//ok: aws-provider-static-credentials
+//ok: aws-provider-static-credentials-test
 provider "aws" {
   region                  = "us-west-2"
   shared_credentials_file = "/Users/tf_user/.aws/creds"
