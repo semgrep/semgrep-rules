77e34d9a2a64d0c0e607c717474f2009e990f753
Colleen Dai <colleen@returntocorp.com>
2022-08-02 11:49:38 -0700

    fixing some fps

--- ruby/rails/security/audit/avoid-tainted-shell-call.rb
+++ ruby/rails/security/audit/avoid-tainted-shell-call.rb
@@ -16,3 +16,23 @@ def foo
   Shell.cat("/var/log/www/access.log")
 
 end
+
+def self.call(params)
+  # ok: avoid-tainted-shell-call
+  new(params).call
+end
+
+def self.execute(site, params, user)
+  # ok: avoid-tainted-shell-call
+  new(site, params, user).execute
+end
+
+def self.execute(relation, params)
+  # ok: avoid-tainted-shell-call
+  new(relation, params).execute
+end
+
+def self.execute(relation, params, site)
+  # ok: avoid-tainted-shell-call
+  new(relation, params, site).execute
+end
