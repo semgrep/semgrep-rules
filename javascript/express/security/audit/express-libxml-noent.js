0feadfe2f32c11bd727e64e6585b0ad75dff565e
Vasilii <inkz@xakep.ru>
2022-08-23 20:26:28 +1000

    return tests for

--- javascript/express/security/audit/express-libxml-noent.js
+++ javascript/express/security/audit/express-libxml-noent.js
@@ -4,11 +4,11 @@ var libxmljs2 = require("libxmljs2");
 module.exports.foo =  function(req, res) {
 
 	// ruleid: express-libxml-noent
-	libxmljs.parseXmlString(req.query.products, {noent:true,noblanks:true})
+	libxmljs.parseXmlString(req.files.products.data.toString('utf8'), {noent:true,noblanks:true})
 	// ruleid: express-libxml-noent
 	libxmljs.parseXml(req.query.products, {noent:true,noblanks:true})
 	// ok: express-libxml-noent
-	libxmljs.parseXml(req.query.productsdata, {noent:false,noblanks:true})
+	libxmljs.parseXml(req.files.products.data.toString('utf8'), {noent:false,noblanks:true})
 	// ruleid: express-libxml-noent
 	libxmljs2.parseXmlString(req.body, {noent:true,noblanks:true})
 	// ruleid: express-libxml-noent
