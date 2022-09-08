6a4ec837c6fef66796f2549affd5d86e3b15884a
Mert <mcoskuner@windowslive.com>
2022-09-08 10:42:45 +0300

    Added kms_key_self_link case

--- terraform/gcp/security/gcp-compute-disk-encryption.tf
+++ terraform/gcp/security/gcp-compute-disk-encryption.tf
@@ -16,6 +16,18 @@ resource "google_compute_disk" "success" {
     image = "debian-8-jessie-v20170523"
     physical_block_size_bytes = 4096
     disk_encryption_key {
-    raw_key = "acXTX3rxrKAFTF0tYVLvydU1riRZTvUNC4g5I11NY-c="
+        raw_key = "acXTX3rxrKAFTF0tYVLvydU1riRZTvUNC4g5I11NY-c="
+    }
+}
+
+# ok: gcp-compute-disk-encryption
+resource "google_compute_disk" "success" {
+    name  = "test-disk"
+    type  = "pd-ssd"
+    zone  = "us-central1-a"
+    image = "debian-8-jessie-v20170523"
+    physical_block_size_bytes = 4096
+    disk_encryption_key {
+        kms_key_self_link = google_kms_crypto_key.example-key.id
     }
 }
