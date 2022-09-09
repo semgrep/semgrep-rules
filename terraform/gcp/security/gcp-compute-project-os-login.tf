783e2cd2c542cf700d54ec13ad838a4df8fdcd8b
Mert <mcoskuner@windowslive.com>
2022-09-09 09:52:17 +0300

    Added boolean case for oslogin

--- terraform/gcp/security/gcp-compute-project-os-login.tf
+++ terraform/gcp/security/gcp-compute-project-os-login.tf
@@ -13,3 +13,11 @@ resource "google_compute_project_metadata" "pass" {
     enable-oslogin = "True"
   }
 }
+
+# ok: gcp-compute-project-os-login
+resource "google_compute_project_metadata" "pass" {
+  metadata = {
+    foo            = "bar"
+    enable-oslogin = True
+  }
+}
