25bab509a4304d5a4c7064477c40f526d065097f
LewisArdern <LewisArdern@live.co.uk>
2022-09-16 13:12:21 -0700

    fix angular-translate

--- javascript/angular/security/detect-third-party-angular-translate.js
+++ javascript/angular/security/detect-third-party-angular-translate.js
@@ -1,6 +1,6 @@
 var app = angular.module('MyApp', []);
 app.controller('myCtrl', function($scope, $sce) {
-    // ruleid: detect-angular-translateprovider-useStrategy-method
+    // ruleid: detect-angular-translateprovider-translations-method
     $translateSanitization.useStrategy();
     var output = 'Hallo <b>{{name}}</b>';
     // ruleid:detect-angular-translateprovider-translations-method
