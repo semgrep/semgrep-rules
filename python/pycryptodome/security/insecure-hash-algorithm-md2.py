66a340990339a002c62b9f04590787e11341133e
LewisArdern <LewisArdern@live.co.uk>
2022-09-23 16:31:04 +0800

    split tests add missing ones

---
+++ python/pycryptodome/security/insecure-hash-algorithm-md2.py
@@ -0,0 +1,24 @@
+# cf. https://github.com/PyCQA/bandit/blob/b78c938c0bd03d201932570f5e054261e10c5750/examples/crypto-md5.py
+
+from cryptography.hazmat.primitives import hashes
+from Crypto.Hash import MD2 as pycrypto_md2
+from Crypto.Hash import MD4 as pycrypto_md4
+from Crypto.Hash import MD5 as pycrypto_md5
+from Crypto.Hash import SHA as pycrypto_sha
+from Cryptodome.Hash import MD2 as pycryptodomex_md2
+from Cryptodome.Hash import MD4 as pycryptodomex_md4
+from Cryptodome.Hash import MD5 as pycryptodomex_md5
+from Cryptodome.Hash import SHA as pycryptodomex_sha
+from Crypto.Hash import SHA3_256
+
+# ok:insecure-hash-algorithm-md2
+h_obj = SHA3_256.new()
+h_obj.update(b'Some data')
+print(h_obj.hexdigest())
+
+# ruleid:insecure-hash-algorithm-md2
+pycrypto_md2.new()
+
+
+# ruleid:insecure-hash-algorithm-md2
+pycryptodomex_md2.new()
