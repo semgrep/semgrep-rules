0db17efc9bb0bc19c5ac37e5ebbff15d69b5a893
Pieter De Cremer <pieter@r2c.dev>
2022-09-23 08:10:28 +0200

    Fix false positive bug report for Java resteasy rule(#2408)
    * fix false positive
    
    * fix another FP and fix metadata
    
    * fix metadata

--- java/jax-rs/security/insecure-resteasy.java
+++ java/jax-rs/security/insecure-resteasy.java
@@ -40,4 +40,30 @@ public class PoC_resource {
         return result;
     }
 
+
+    // ok: default-resteasy-provider-abuse
+    @GET
+    @Path("/tenantmode")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getTenantMode() {
+        return kubernetesService.getMessage();
+    }
+
+}
+
+
+@Path("/")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class PoC_resource {
+
+    // ok: default-resteasy-provider-abuse
+    @POST
+    @Path("/concat")
+    public Map<String, String> doConcat(Pair pair) {
+        HashMap<String, String> result = new HashMap<String, String>();
+        result.put("Result", pair.getP1() + pair.getP2());
+        return result;
+    }
+
 }
