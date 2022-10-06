e67626128757e93b80bac00e19e6ef118d229c6b
Claudio <claudio@r2c.dev>
2022-10-06 15:23:26 +0200

    Restore correct rules and tests

--- java/jax-rs/security/insecure-resteasy.java
+++ java/jax-rs/security/insecure-resteasy.java
@@ -6,64 +6,63 @@ import javax.ws.rs.core.*;
 
 @Path("/")
 public class PoC_resource {
-    @POST
-    @Path("/concat")
-    @Produces(MediaType.APPLICATION_JSON)
-    // todoid: insecure-resteasy-deserialization
-    @Consumes({"*/*"})
-    public Map<String, String> doConcat(Pair pair) {
-        HashMap<String, String> result = new HashMap<String, String>();
-        result.put("Result", pair.getP1() + pair.getP2());
+  @POST
+  @Path("/concat")
+  @Produces(MediaType.APPLICATION_JSON)
+  // ruleid: insecure-resteasy-deserialization
+  @Consumes({ "*/*" })
+  public Map<String, String> doConcat(Pair pair) {
+    HashMap<String, String> result = new HashMap<String, String>();
+    result.put("Result", pair.getP1() + pair.getP2());
 
-        return result;
-    }
-    // ruleid:default-resteasy-provider-abuse
-    @POST
-    @Path("/vulnerable")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Map<String, String> doConcat(Pair pair) {
-        HashMap<String, String> result = new HashMap<String, String>();
-        result.put("Result", pair.getP1() + pair.getP2());
+    return result;
+  }
 
-        return result;
-    }
+  // ruleid:default-resteasy-provider-abuse
+  @POST
+  @Path("/vulnerable")
+  @Produces(MediaType.APPLICATION_JSON)
+  public Map<String, String> doConcat(Pair pair) {
+    HashMap<String, String> result = new HashMap<String, String>();
+    result.put("Result", pair.getP1() + pair.getP2());
 
-    @POST
-    @Path("/count")
-    @Produces(MediaType.APPLICATION_JSON)
-    // todo: insecure-resteasy-deserialization
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Map<String, Integer> doCount(ArrayList<Object> elements) {
-        HashMap<String, Integer> result = new HashMap<String, Integer>();
-        result.put("Result", elements.size());
+    return result;
+  }
 
-        return result;
-    }
+  @POST
+  @Path("/count")
+  @Produces(MediaType.APPLICATION_JSON)
+  // todook: insecure-resteasy-deserialization
+  @Consumes(MediaType.APPLICATION_JSON)
+  public Map<String, Integer> doCount(ArrayList<Object> elements) {
+    HashMap<String, Integer> result = new HashMap<String, Integer>();
+    result.put("Result", elements.size());
 
+    return result;
+  }
 
-    // ok: default-resteasy-provider-abuse
-    @GET
-    @Path("/tenantmode")
-    @Produces(MediaType.TEXT_PLAIN)
-    public String getTenantMode() {
-        return kubernetesService.getMessage();
-    }
+  // ok: default-resteasy-provider-abuse
+  @GET
+  @Path("/tenantmode")
+  @Produces(MediaType.TEXT_PLAIN)
+  public String getTenantMode() {
+    return kubernetesService.getMessage();
+  }
 
 }
-
 
 @Path("/")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class PoC_resource {
 
-    // ok: default-resteasy-provider-abuse
-    @POST
-    @Path("/concat")
-    public Map<String, String> doConcat(Pair pair) {
-        HashMap<String, String> result = new HashMap<String, String>();
-        result.put("Result", pair.getP1() + pair.getP2());
-        return result;
-    }
+  // ok: default-resteasy-provider-abuse
+  @POST
+  @Path("/concat")
+  public Map<String, String> doConcat(Pair pair) {
+    HashMap<String, String> result = new HashMap<String, String>();
+    result.put("Result", pair.getP1() + pair.getP2());
+    return result;
+  }
 
 }
