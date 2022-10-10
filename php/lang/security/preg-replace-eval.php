61aea10e45fc3c568d3e5e5bfc8584d340252ee9
Pieter De Cremer <pieter@r2c.dev>
2022-10-10 13:24:24 +0200

    fix false positives in preg-replace-eval rule

--- php/lang/security/preg-replace-eval.php
+++ php/lang/security/preg-replace-eval.php
@@ -5,3 +5,11 @@ preg_replace($user_input_pattern, $replacement, $string);
 
 // ok: preg-replace-eval
 preg_replace("/some_regexp/", "replacement", $string_before);
+
+// ok: preg-replace-eval
+preg_replace(array("/hello/"), array("world"), "hello world");
+
+$input = array("/constant/");
+// ok: preg-replace-eval
+preg_replace($input, array("world"), "hello world");
+
