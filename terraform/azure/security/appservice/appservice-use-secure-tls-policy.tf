b0bd48e35b9271bba29e442ba493ed4d91af3cb8
lds <lds@nianticlabs.com>
2022-10-12 13:43:33 +0000

    Do not detect GCP tls configuration in Azure detection rules.

--- terraform/azure/security/appservice/appservice-use-secure-tls-policy.tf
+++ terraform/azure/security/appservice/appservice-use-secure-tls-policy.tf
@@ -18,6 +18,13 @@ resource "azurerm_app_service" "good_example" {
   }
 }
 
+resource "google_compute_ssl_policy" "good_example" {
+  # ok: appservice-use-secure-tls-policy
+  min_tls_version = "TLS_1_2"
+  name            = "modern-ssl-policy"
+  profile         = "MODERN"
+}
+
 # fail
 
 resource "azurerm_app_service" "bad_example" {
@@ -26,8 +33,8 @@ resource "azurerm_app_service" "bad_example" {
   resource_group_name = azurerm_resource_group.example.name
   app_service_plan_id = azurerm_app_service_plan.example.id
 
+  # ruleid: appservice-use-secure-tls-policy
   site_config {
-    # ruleid: appservice-use-secure-tls-policy
       min_tls_version = "1.0"
   }
 }
@@ -38,8 +45,8 @@ resource "azurerm_app_service" "bad_example" {
   resource_group_name = azurerm_resource_group.example.name
   app_service_plan_id = azurerm_app_service_plan.example.id
 
+  # ruleid: appservice-use-secure-tls-policy
   site_config {
-    # ruleid: appservice-use-secure-tls-policy
       min_tls_version = "1.1"
   }
 }
