12498bfc7463a07fb86c708af5b5c93e850e5e89
Colleen Dai <colleen@returntocorp.com>
2022-10-20 16:59:20 -0700

    adding case to unverified ssl context

--- python/lang/security/unverified-ssl-context.py
+++ python/lang/security/unverified-ssl-context.py
@@ -11,3 +11,7 @@ conn = httplib.client.HTTPSConnection("123.123.21.21", context=context)
 
 # ruleid:unverified-ssl-context
 conn = httplib.client.HTTPSConnection("123.123.21.21", context=ssl._create_unverified_context())
+
+# ruleid:unverified-ssl-context
+ssl._create_default_https_context = ssl._create_unverified_context
+urllib2.urlopen("https://google.com").read()
