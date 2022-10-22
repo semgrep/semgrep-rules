8fb2c6c23eaa33fbd47c10b5a788e8aa14d7f5ac
keeganparr1 <keeganparr@gmail.com>
2022-10-22 08:39:12 -0400

    Updated location of ruleid and ok

--- arbitrary-send-erc20.sol
+++ arbitrary-send-erc20.sol
@@ -1,9 +1,9 @@
-    // ruleid: arbitrary-send-erc20
     function a(address from, address to, uint256 amount) public {
+        // ruleid: arbitrary-send-erc20
         erc20.transferFrom(address, to, am);
     }
 
-    // ok: arbitrary-send-erc20
-        function b(address from, address to, uint256 amount) public {
+    function b(address from, address to, uint256 amount) public {
+        // ok: arbitrary-send-erc20
         erc20.transferFrom(msg.sender, to, am);
     }
