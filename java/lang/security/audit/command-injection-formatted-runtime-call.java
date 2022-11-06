a45042e7c78b491bf43222d7b1f0b9a32a5197a9
Vasilii <inkz@xakep.ru>
2022-11-06 16:33:31 +0200

    update command-injection-formatted-runtime-call rule

--- java/lang/security/audit/command-injection-formatted-runtime-call.java
+++ java/lang/security/audit/command-injection-formatted-runtime-call.java
@@ -20,9 +20,25 @@ class Cls {
         r.exec("bash", "-c", input);
     }
 
+    public void test3(String input) {
+        // ruleid: command-injection-formatted-runtime-call
+        Runtime.getRuntime().loadLibrary(String.format("%s.dll", input));
+    }
+
+    public void test4(String input) {
+        // ruleid: command-injection-formatted-runtime-call
+        Runtime.getRuntime().exec("bash", "-c", input);
+    }
+
     public void okTest(String input) {
         Runtime r = Runtime.getRuntime();
         // ok: command-injection-formatted-runtime-call
         r.exec("echo 'blah'");
     }
+
+    public void okTest2(String input) {
+        // ok: command-injection-formatted-runtime-call
+        Runtime.getRuntime().loadLibrary("lib.dll");
+    }
 }
+
