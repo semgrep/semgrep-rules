264743d867b237e67a18a7ff7e108a425c99807e
enncoded <14846866+enncoded@users.noreply.github.com>
2022-11-07 10:10:07 -0800

    Updated `use-filepath-join`

--- go/lang/correctness/use-filepath-join.go
+++ go/lang/correctness/use-filepath-join.go
@@ -13,3 +13,26 @@ func main() {
 	// ok: use-filepath-join
 	var fpath = filepath.Join(getDir())
 }
+
+func main() {
+	url, err := url.Parse("http://foo:666/bar")
+	if err != nil {
+		panic(err)
+	}
+
+	// ok: use-filepath-join
+	fmt.Println(path.Join(url.Path, "baz"))
+}
+
+func main(a *http.Request) {
+	p, err := path.Parse("/opt/foo/bar")
+	if err != nil {
+		panic(err)
+	}
+
+	// ruleid: use-filepath-join
+	fmt.Println(path.Join(p.Path, "baz"))
+
+	// ok: use-filepath-join
+	fmt.Println(path.Join(a.Path, "baz"))
+}
