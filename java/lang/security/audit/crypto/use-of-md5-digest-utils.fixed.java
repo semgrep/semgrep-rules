f556d4cdf28796d942eac95e56191f53a0b77628
Pieter De Cremer <pieter@r2c.dev>
2022-11-09 09:11:52 +0100

    update ruleid

--- java/lang/security/audit/crypto/use-of-md5-digest-utils.fixed.java
+++ java/lang/security/audit/crypto/use-of-md5-digest-utils.fixed.java
@@ -3,7 +3,7 @@ import org.apache.commons.codec.digest.DigestUtils;
 
 public class Bad{
   public byte[] bad1(String password) {
-    // ok: use-of-md5
+    // ok: use-of-md5-digest-utils
     MessageDigest md5Digest = MessageDigest.getInstance("MD5");
     md5Digest.update(password.getBytes());
     byte[] hashValue = md5Digest.digest();
@@ -11,13 +11,13 @@ public class Bad{
   }
 
   public byte[] bad2(String password) {
-    // ruleid: use-of-md5
+    // ruleid: use-of-md5-digest-utils
     byte[] hashValue = DigestUtils.getSha512Digest().digest(password.getBytes());
     return hashValue;
   }
 
   public byte[] ok(String password) {
-    // ok: use-of-md5
+    // ok: use-of-md5-digest-utils
     byte[] hashValue = DigestUtils.getSha512Digest().digest(password.getBytes());
     return hashValue;
   }
