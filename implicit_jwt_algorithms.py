6e2ec06cda475353e3afd735049c268df653b5fe
Alexey Posikera <posikera@bk.ru>
2022-11-15 18:22:32 +0000

    add implicit_jwt_algorithms.py

---
+++ implicit_jwt_algorithms.py
@@ -0,0 +1,9 @@
+import jwt as tokenizer
+
+# JWT Auth bypass
+def accept_request():
+    decode = tokenizer.decode(token, "password", verify=False)
+
+# JWT algorithm bypass
+def accept_another_request():
+    decode = jwt.decode(token, "password")
