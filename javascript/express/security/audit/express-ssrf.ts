64b878e95db1627859714ea89e138c4e0c6d284d
Lewis <LewisArdern@live.co.uk>
2023-01-04 00:32:16 -0800

    Improvements to SSRF (#2624)
    * Improvements to SSRF
    
    * add comments to explain and update assign values

--- javascript/express/security/audit/express-ssrf.ts
+++ javascript/express/security/audit/express-ssrf.ts
@@ -4,19 +4,73 @@ const request = require('request')
 
 module.exports = function badNormal () {
   return (req: Request, res: Response, next: NextFunction) => {
-    const url = req.body.imageUrl
+    const url = "//"+req.body.imageUrl
+    const url1 = req.body['imageUrl'] + 123
     // ruleid: express-ssrf
     request.get(url)
-
-}
-}
-module.exports = function goodNormal () {
-  return (req: Request, res: Response, next: NextFunction) => {
-    const url = 'https://google.com'
+    // ruleid: express-ssrf
+    request.get(url1+123)
     // ok: express-ssrf
-     request.get(url)
-  }
+    request.get(`https://reddit.com/${req.query.url}/fooo`)
+    // ok: express-ssrf
+    request.get("https://google.com/"+req.query.url)
+    // ok: express-ssrf
+    request.get(config_value.foo+req.query.url)
+    // ok: express-ssrf
+    request.get(config_value.foo+req.body.shouldalsonotcatch)
+    // ok: express-ssrf
+    request.get(config_value.foo+req)
 
+    // ruleid: express-ssrf
+    request.get(req.body.url)
+    // ruleid: express-ssrf
+    request.get(`${req.query.url}/fooo`)
+    // ruleid: express-ssrf
+    request.get("//"+req.query.url+config_value.url)
+
+    const a = req.body.url
+    // ruleid: express-ssrf
+    request.get(a)
+    // ruleid: express-ssrf
+    request.get(`${url1}/fooo`)
+    // ruleid: express-ssrf
+    request.get(a+config_value.url)
+
+    // ok: express-ssrf
+    request.get(c+a)
+    // ok: express-ssrf
+    request.get(`${c}${a}/fooo`)
+    // ok: express-ssrf
+    request.get(c+a+config_value.url)
+
+    // ok: express-ssrf
+    request.get(c)
+    // ok: express-ssrf
+    request.get(`${c}`)
+    // ok: express-ssrf
+    request.get(c+config_value.url)
+
+    // ruleid: express-ssrf
+    request.get(req.body['url'])
+    // ruleid: express-ssrf
+    request.get(`${req.body['url']}/fooo`)
+    // ruleid: express-ssrf
+    request.get(req.body['url']+config_value.url)
+
+    // ruleid: express-ssrf
+    request.get("https://"+url1)
+    // ruleid: express-ssrf
+    request.get(`https://${req.body['url']}/fooo`)
+    // ruleid: express-ssrf
+    request.get("https://"+req.body['url']+config_value.url)
+    // ruleid: express-ssrf
+    request.get("//"+req.body['url']+config_value.url)
+    // ok: express-ssrf
+    request.get("//"+c+req.body['url']+config_value.url)
+    // todo: express-ssrf
+    request.get("https://google.com"+req.query.url)
+
+}
 }
 
 
