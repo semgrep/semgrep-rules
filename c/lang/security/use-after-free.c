13e34424a0a0600d1fdb776d6bf37029d5c903ce
Pieter De Cremer (r2c) <pieter@r2c.dev>
2023-01-09 09:37:31 +0100

    Fix use-after-free rule triggering when using free() on structure member (#2632)
    * Fix use-after-free rule triggering when using free() on structure member
    
    * turn into search mode rule instead
    
    * fix incorrect code
    
    * make c code compilable
    
    * make c code compilable

--- c/lang/security/use-after-free.c
+++ c/lang/security/use-after-free.c
@@ -1,9 +1,12 @@
+#include <stdio.h>
 #include <stdlib.h>
 
 typedef struct name {
     char *myname;
     void (*func)(char *str);
 } NAME;
+
+void other_func(char *ignored) {}
 
 int bad_code1() {
     NAME *var;
@@ -48,10 +51,11 @@ int okay_code2() {
 struct NAME {
   char first_name[32];
   int auth;
-};
+} s_auth;
+
 int bad_code3(){
     struct NAME *var;
-    var = malloc(sizeof(auth));
+    var = malloc(sizeof(s_auth));
     free(var);
     // ruleid: use-after-free
     if(var->auth){
@@ -67,7 +71,7 @@ int bad_code3(){
 
 int ok_code3(){
     struct NAME *var;
-    var = malloc(sizeof(auth));
+    var = malloc(sizeof(s_auth));
     free(var);
     var=NULL;
     // ok: use-after-free
@@ -80,3 +84,75 @@ int ok_code3(){
     return 0;
 
 }
+
+struct lv {
+    int length;
+    char *value;
+} lv;
+
+struct lv2 {
+    int length;
+    struct lv *lv;
+} lv2;
+
+
+int bad_code4(){
+    int initial = 1000;
+    struct lv *lv = malloc(sizeof(*lv));
+    lv->length = initial;
+    lv->value = malloc(initial);
+    free(lv);
+    // ruleid: use-after-free
+    free(lv->value);
+    return 0;
+}
+
+int ok_code4(){
+    int initial = 1000;
+    struct lv *lv = malloc(sizeof(*lv));
+    lv->length = initial;
+    lv->value = malloc(initial);
+    // ok: use-after-free
+    free(lv->value);
+    // ok: use-after-free
+    free(lv);
+    return 0;
+}
+
+int bad_code5(){
+    int initial = 1000;
+    struct lv *lv = malloc(sizeof(*lv));
+    lv->length = initial;
+    lv->value = malloc(initial);
+
+    struct lv2 *lv2 = malloc(sizeof(*lv2));
+    lv2->length = initial;
+    lv2->lv = lv;
+    // ok: use-after-free
+    free(lv2->lv);
+    // ruleid: use-after-free
+    free(lv2->lv->value);
+    // ok: use-after-free
+    free(lv2);
+    return 0;
+}
+
+
+int ok_code5(){
+    int initial = 1000;
+    struct lv *lv = malloc(sizeof(*lv));
+    lv->length = initial;
+    lv->value = malloc(initial);
+
+    struct lv2 *lv2 = malloc(sizeof(*lv2));
+    lv2->length = initial;
+    lv2->lv = lv;
+    // ok: use-after-free
+    free(lv2->lv->value);
+    // ok: use-after-free
+    free(lv2->lv);
+    // ok: use-after-free
+    free(lv2);
+    return 0;
+}
+
