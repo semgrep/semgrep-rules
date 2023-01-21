4d307f1c1637ac8c919653561cabee85d9edd9ba
Kurt Boberg <kurt@r2c.dev>
2023-01-20 16:32:26 -0800

    add more test cases, fix metavar disagreement from editor testing

--- php/lang/security/audit/assert-use-audit.php
+++ php/lang/security/audit/assert-use-audit.php
@@ -6,5 +6,14 @@ assert($user_input);
 // ok: assert-use-audit
 assert('2 > 1');
 
-// todook: assert-use-audit
+// ok: assert-use-audit
 assert($user_input > 1);
+
+// ok: assert-use-audit
+assert($ok < 1 || $ok > 2);
+
+// ok: assert-use-audit
+assert($ok != "something");
+
+// ok: assert-use-audit
+assert($ok instanceof FakeClass);
