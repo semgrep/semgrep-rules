aca1199dd3985086f76b30ea5cad39de786eabd4
Pieter De Cremer <pieter@r2c.dev>
2023-01-25 10:47:22 +0100

    update OSS rules regarding

--- python/aws-lambda/security/tainted-pickle-deserialization.py
+++ python/aws-lambda/security/tainted-pickle-deserialization.py
@@ -1,31 +1,67 @@
 import _pickle
 import cPickle
-from dill import loads
 import shelve
+
+import dill as securedill
+from dill import load, loads
 
 
 def lambda_handler(event, context):
 
-  # ruleid: tainted-pickle-deserialization
-  _pickle.load(event['exploit_code'])
+    tainted = event['exploit_code']
+    s = "encoded-data"
+    b = bytearray().extend(map(ord,string))
 
-  # ruleid: tainted-pickle-deserialization
-  obj = cPickle.loads(f"foobar{event['exploit_code']}")
+    # ruleid: tainted-pickle-deserialization
+    _pickle.load(event['exploit_code'])
 
-  # ruleid: tainted-pickle-deserialization
-  loads(event['exploit_code'])(123)
+    # ruleid: tainted-pickle-deserialization
+    obj = cPickle.loads(f"foobar{event['exploit_code']}")
 
-  # ruleid: tainted-pickle-deserialization
-  with shelve.open(f"/tmp/path/{event['object_path']}") as db:
-    db['eggs'] = 'eggs'
+    # ruleid: tainted-pickle-deserialization
+    loads(event['exploit_code'])(123)
 
-  # ok: tainted-pickle-deserialization
-  _pickle.loads('hardcoded code')
+    # ruleid: tainted-pickle-deserialization
+    with shelve.open(f"/tmp/path/{event['object_path']}") as db:
+      db['eggs'] = 'eggs'
 
-  # ok: tainted-pickle-deserialization
-  code = '/file/path'
-  cPickle.load(code)
+    # ok: tainted-pickle-deserialization
+    _pickle.loads('hardcoded code')
 
-  # ok: tainted-pickle-deserialization
-  name = 'foobar'
-  shelve.open(f"/tmp/path/{name}")
+    # ok: tainted-pickle-deserialization
+    code = '/file/path'
+    cPickle.load(code)
+
+    # ok: tainted-pickle-deserialization
+    name = 'foobar'
+    shelve.open(f"/tmp/path/{name}")
+
+    # ruleid: tainted-pickle-deserialization
+    dill.load(tainted)
+    # ruleid: tainted-pickle-deserialization
+    dill.loads(tainted)
+
+    # ok: tainted-pickle-deserialization
+    dill.load(s)
+    # ok: tainted-pickle-deserialization
+    dill.loads(b)
+
+    # ruleid: tainted-pickle-deserialization
+    securedill.load(tainted)
+    # ruleid: tainted-pickle-deserialization
+    securedill.loads(tainted)
+
+    # ok: tainted-pickle-deserialization
+    securedill.load(s)
+    # ok: tainted-pickle-deserialization
+    securedill.loads(b)
+
+    # ruleid: tainted-pickle-deserialization
+    load(tainted)
+    # ruleid: tainted-pickle-deserialization
+    loads(tainted)
+
+    # ok: tainted-pickle-deserialization
+    load(s)
+    # ok: tainted-pickle-deserialization
+    loads(b)
--- python/lang/security/deserialization/pickle.py
+++ python/lang/security/deserialization/pickle.py
@@ -32,9 +32,8 @@ def insecure_deserialization_2(exploit_code):
     adaasfa.loads(exploit_code)
 
 
-import cPickle
 import socket
-
+import cPickle
 
 class Shell_code(object):
     def __reduce__(self):
@@ -48,9 +47,155 @@ class Shell_code(object):
 shell = cPickle.dumps(Shell_code())
 
 import dill
+import dill as securedill
 
-# ruleid: avoid-dill
-shell = dill.dumps(Shell_code())
+import dill as pickle
+from dill import load, loads
+
+from http.server import BaseHTTPRequestHandler
+import urllib.parse
+
+class GetHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        tainted = urlparse.urlparse(self.path).query
+        
+        # ruleid: avoid-dill
+        dill.load(tainted)
+        # ruleid: avoid-dill
+        dill.loads(tainted)
+        # ruleid: avoid-dill
+        dill.load_module(filename=tainted)
+        # ruleid: avoid-dill
+        dill.load_module_asdict(filename=tainted)
+        # ruleid: avoid-dill
+        dill.temp.load(tainted)
+        # ruleid: avoid-dill
+        dill.temp.loadIO(tainted)
+        # ruleid: avoid-dill
+        dill.temp.load_source(tainted)
+        # ruleid: avoid-dill
+        dill.temp.loadIO_source(tainted)
+
+        # ruleid: avoid-dill
+        unpickler = dill.Unpickler(tainted)
+        unpickler.load()
+
+        # ok: avoid-dill
+        dill.load(s)
+        # ok: avoid-dill
+        dill.loads(b)
+        # ok: avoid-dill
+        dill.load_module(filename=s)
+        # ok: avoid-dill
+        dill.load_module_asdict(filename=s)
+        # ok: avoid-dill
+        dill.temp.load(s)
+        # ok: avoid-dill
+        dill.temp.loadIO(s)
+        # ok: avoid-dill
+        dill.temp.load_source(s)
+        # ok: avoid-dill
+        dill.temp.loadIO_source(s)
+
+        # ok: avoid-dill
+        unpickler = dill.Unpickler(s)
+        unpickler.load()
+
+        # ruleid: avoid-dill
+        pickle.load(tainted)
+        # ruleid: avoid-dill
+        pickle.loads(tainted)
+        # ruleid: avoid-dill
+        pickle.load_module(filename=tainted)
+        # ruleid: avoid-dill
+        pickle.load_module_asdict(filename=tainted)
+        # ruleid: avoid-dill
+        pickle.temp.load(tainted)
+        # ruleid: avoid-dill
+        pickle.temp.loadIO(tainted)
+        # ruleid: avoid-dill
+        pickle.temp.load_source(tainted)
+        # ruleid: avoid-dill
+        pickle.temp.loadIO_source(tainted)
+
+        # ruleid: avoid-dill
+        unpickler = pickle.Unpickler(tainted)
+        unpickler.load()
+
+        # ok: avoid-dill
+        pickle.load(s)
+        # ok: avoid-dill
+        pickle.loads(b)
+        # ok: avoid-dill
+        pickle.load_module(filename=s)
+        # ok: avoid-dill
+        pickle.load_module_asdict(filename=s)
+        # ok: avoid-dill
+        pickle.temp.load(s)
+        # ok: avoid-dill
+        pickle.temp.loadIO(s)
+        # ok: avoid-dill
+        pickle.temp.load_source(s)
+        # ok: avoid-dill
+        pickle.temp.loadIO_source(s)
+
+        # ok: avoid-dill
+        unpickler = pickle.Unpickler(s)
+        unpickler.load()
+
+        
+        # ruleid: avoid-dill
+        securedill.load(tainted)
+        # ruleid: avoid-dill
+        securedill.loads(tainted)
+        # ruleid: avoid-dill
+        securedill.load_module(filename=tainted)
+        # ruleid: avoid-dill
+        securedill.load_module_asdict(filename=tainted)
+        # ruleid: avoid-dill
+        securedill.temp.load(tainted)
+        # ruleid: avoid-dill
+        securedill.temp.loadIO(tainted)
+        # ruleid: avoid-dill
+        securedill.temp.load_source(tainted)
+        # ruleid: avoid-dill
+        securedill.temp.loadIO_source(tainted)
+
+        # ruleid: avoid-dill
+        unpickler = securedill.Unpickler(tainted)
+        unpickler.load()
+
+        # ok: avoid-dill
+        securedill.load(s)
+        # ok: avoid-dill
+        securedill.loads(b)
+        # ok: avoid-dill
+        securedill.load_module(filename=s)
+        # ok: avoid-dill
+        securedill.load_module_asdict(filename=s)
+        # ok: avoid-dill
+        securedill.temp.load(s)
+        # ok: avoid-dill
+        securedill.temp.loadIO(s)
+        # ok: avoid-dill
+        securedill.temp.load_source(s)
+        # ok: avoid-dill
+        securedill.temp.loadIO_source(s)
+
+        # ok: avoid-dill
+        unpickler = securedill.Unpickler(s)
+        unpickler.load()
+
+        # ruleid: avoid-dill
+        load(tainted)
+        # ruleid: avoid-dill
+        loads(tainted)
+
+        # ok: avoid-dill
+        load(s)
+        # ok: avoid-dill
+        loads(b)
 
 import shelve
 
