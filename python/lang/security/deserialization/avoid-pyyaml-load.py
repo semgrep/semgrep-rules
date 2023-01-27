afdae74847c1c3846996ef70472f1ecd1cb68f3f
Pieter De Cremer <pieter@r2c.dev>
2023-01-27 10:05:16 +0100

    update oss rules on python deserialization

--- python/lang/security/deserialization/avoid-pyyaml-load.py
+++ python/lang/security/deserialization/avoid-pyyaml-load.py
@@ -1,65 +1,59 @@
 import yaml
 
+from http.server import BaseHTTPRequestHandler
+import urllib.parse
 
-#ruleid:avoid-pyyaml-load
-yaml.unsafe_load("!!python/object/new:os.system [echo EXPLOIT!]")
+class GetHandler(BaseHTTPRequestHandler):
 
-def thing(**kwargs):
-    #ruleid:avoid-pyyaml-load
-    yaml.unsafe_load("!!python/object/new:os.system [echo EXPLOIT!]", **kwargs)
+    def do_GET(self, **kwargs):
+        tainted = urlparse.urlparse(self.path).query
 
-def other_thing(**kwargs):
-    #ruleid:avoid-pyyaml-load
-    yaml.load("!!python/object/new:os.system [echo EXPLOIT!]", Loader=yaml.Loader, **kwargs)
-
-def other_thing_two(**kwargs):
-    #ruleid:avoid-pyyaml-load
-    yaml.load("!!python/object/new:os.system [echo EXPLOIT!]", Loader=yaml.UnsafeLoader, **kwargs)
-
-def other_thing_three(**kwargs):
-    #ruleid:avoid-pyyaml-load
-    yaml.load("!!python/object/new:os.system [echo EXPLOIT!]", Loader=yaml.CLoader, **kwargs)
-
-def other_thing_four(**kwargs):
-    #ruleid:avoid-pyyaml-load
-    yaml.load_all("!!python/object/new:os.system [echo EXPLOIT!]", Loader=yaml.Loader, **kwargs)
-
-def other_thing_five(**kwargs):
-    #ruleid:avoid-pyyaml-load
-    yaml.load_all("!!python/object/new:os.system [echo EXPLOIT!]", Loader=yaml.UnsafeLoader, **kwargs)
-
-def other_thing_six(**kwargs):
-    #ruleid:avoid-pyyaml-load
-    yaml.load_all("!!python/object/new:os.system [echo EXPLOIT!]", Loader=yaml.CLoader, **kwargs)
-
-def this_is_ok(stream):
-    #ok:avoid-pyyaml-load
-    return yaml.load(stream, Loader=yaml.CSafeLoader)
-
-def this_is_also_ok(stream):
-    #ok:avoid-pyyaml-load
-    return yaml.load(stream, Loader=yaml.SafeLoader)
-
-def this_is_additionally_ok(stream):
-    #ok:avoid-pyyaml-load
-    return yaml.load_all(stream, Loader=yaml.CSafeLoader)
-
-def this_is_ok_too(stream):
-    #ok:avoid-pyyaml-load
-    return yaml.load_all(stream, Loader=yaml.SafeLoader)
-
-def this_is_ok_as_well(stream):
-    #ok:avoid-pyyaml-load
-    return yaml.load(stream, Loader=yaml.BaseLoader)
-
-def this_is_ok_too_two(stream):
-    #ok:avoid-pyyaml-load
-    return yaml.load_all(stream, Loader=yaml.BaseLoader)
-
-def check_ruamel_yaml():
-    from ruamel.yaml import YAML
-    yaml = YAML(typ="rt")
-    # ok:avoid-pyyaml-load
-    yaml.load("thing.yaml")
-    # ok:avoid-pyyaml-load
-    yaml.load_all("thing.yaml")
+        #ruleid:avoid-pyyaml-load
+        yaml.unsafe_load(tainted)
+        
+        #ruleid:avoid-pyyaml-load
+        yaml.unsafe_load(tainted, **kwargs)
+        
+        #ruleid:avoid-pyyaml-load
+        yaml.load(tainted, Loader=yaml.Loader, **kwargs)
+        
+        #ruleid:avoid-pyyaml-load
+        yaml.load(tainted, Loader=yaml.UnsafeLoader, **kwargs)
+        
+        #ruleid:avoid-pyyaml-load
+        yaml.load(tainted, Loader=yaml.CLoader, **kwargs)
+        
+        #ruleid:avoid-pyyaml-load
+        yaml.load_all(tainted, Loader=yaml.Loader, **kwargs)
+        
+        #ruleid:avoid-pyyaml-load
+        yaml.load_all(tainted, Loader=yaml.UnsafeLoader, **kwargs)
+        
+        #ruleid:avoid-pyyaml-load
+        yaml.load_all(tainted, Loader=yaml.CLoader, **kwargs)
+        
+        #ok:avoid-pyyaml-load
+        yaml.load(stream, Loader=yaml.CSafeLoader)
+        
+        #ok:avoid-pyyaml-load
+        yaml.load(stream, Loader=yaml.SafeLoader)
+        
+        #ok:avoid-pyyaml-load
+        yaml.load_all(stream, Loader=yaml.CSafeLoader)
+        
+        #ok:avoid-pyyaml-load
+        yaml.load_all(stream, Loader=yaml.SafeLoader)
+        
+        #ok:avoid-pyyaml-load
+        yaml.load(stream, Loader=yaml.BaseLoader)
+        
+        #ok:avoid-pyyaml-load
+        yaml.load_all(stream, Loader=yaml.BaseLoader)
+        
+        
+        from ruamel.yaml import YAML
+        yaml = YAML(typ="rt")
+        # ok: avoid-pyyaml-load
+        yaml.load(tainted)
+        # ok: avoid-pyyaml-load
+        yaml.load_all(tainted)
