cda195f8e0c3bde9d48c43874d315549a1dd03bd
semgrep.dev <support@r2c.dev>
2023-02-22 01:53:41 +0000

    add ruby-rails-performance-index-may-be-beneficial.rb

---
+++ ruby-rails-performance-index-may-be-beneficial.rb
@@ -0,0 +1,18 @@
+class CreateProducts < ActiveRecord::Migration[7.0]
+  def change
+    # ok: ruby-rails-performance-index-may-be-beneficial
+    add_column :users, :email_id, :integer
+    add_index :users, :email_id
+
+    # ok: ruby-rails-performance-index-may-be-beneficial
+    add_column :users2, :email2_id, :integer, foo: :bar
+    add_index :users2, :email2_id, name: "asdf"
+
+    # todoruleid: ruby-rails-performance-index-may-be-beneficial
+    add_column :users3, :email3_id, :integer, foo: bar
+    add_index :users3, [:email2_id, :other_id], name: "asdf"
+
+    # ruleid: ruby-rails-performance-index-may-be-beneficial
+    add_column :users4, :email4_id, :integer, { other_stuff: :asdf }
+  end
+end
