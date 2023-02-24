5eb807f5d1bd83122ac3f913ce29909e41a6d857
Pieter De Cremer <pieter@r2c.dev>
2023-02-24 11:32:18 +0100

    merge with other rule

--- terraform/aws/security/aws-ec2-security-group-rule-missing-description.tf
+++ terraform/aws/security/aws-ec2-security-group-rule-missing-description.tf
@@ -34,12 +34,12 @@ resource "aws_security_group_rule" "fail_3" {
   security_group_id = "sg-123456"
 }
 
-# ruleid: aws-ec2-security-group-rule-missing-description
 resource "aws_security_group" "fail_4" {
   name        = "http"
   description = "Allow inbound HTTP traffic"
 
   ingress {
+    # ruleid: aws-ec2-security-group-rule-missing-description
     description = ""
     from_port   = 80
     to_port     = 80
@@ -48,12 +48,12 @@ resource "aws_security_group" "fail_4" {
   }
 }
 
-# ruleid: aws-ec2-security-group-rule-missing-description
 resource "aws_security_group" "fail_5" {
   name        = "http"
   description = "Allow outbound HTTP traffic"
 
   egress {
+    # ruleid: aws-ec2-security-group-rule-missing-description
     description = ""
     from_port   = 80
     to_port     = 80
@@ -62,8 +62,8 @@ resource "aws_security_group" "fail_5" {
   }
 }
 
-# ruleid: aws-ec2-security-group-rule-missing-description
 resource "aws_security_group_rule" "fail_6" {
+  # ruleid: aws-ec2-security-group-rule-missing-description
   description       = ""
   type              = "egress"
   from_port         = 80
@@ -73,9 +73,9 @@ resource "aws_security_group_rule" "fail_6" {
   security_group_id = "sg-123456"
 }
 
-# ok: aws-ec2-security-group-rule-missing-description
 resource "aws_security_group" "pass_1" {
   name        = "http"
+  # ruleid: aws-ec2-security-group-rule-missing-description
   description = "Managed by Terraform"
 
   ingress {
@@ -163,5 +163,46 @@ resource "aws_security_group_rule" "pass_3" {
   protocol          = "tcp"
   cidr_blocks       = [aws_vpc.main.cidr_block]
   security_group_id = "sg-123456"
+}
+
+# ruleid: aws-ec2-security-group-rule-missing-description
+resource "aws_security_group" "fail_1" {
+  name = "http"
+
+  ingress {
+    description = "HTTP from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.main.cidr_block]
+  }
+}
+
+resource "aws_security_group" "fail_2" {
+  name        = "http"
+  # ruleid: aws-ec2-security-group-rule-missing-description
+  description = ""
+
+  ingress {
+    description = "HTTP from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.main.cidr_block]
+  }
+}
+
+# ok: aws-ec2-security-group-rule-missing-description
+resource "aws_security_group" "pass" {
+  name        = "http"
+  description = "Allow inbound HTTP traffic"
+
+  ingress {
+    description = "HTTP from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.main.cidr_block]
+  }
 }
 
