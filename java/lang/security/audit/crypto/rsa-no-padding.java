11f02de78f8eb52cad81f1613cb3d7fdfc629d7d
Colleen Dai <colleen@returntocorp.com>
2023-02-27 17:31:35 -0600

    editing cipher rules

--- java/lang/security/audit/crypto/rsa-no-padding.java
+++ java/lang/security/audit/crypto/rsa-no-padding.java
@@ -4,7 +4,12 @@ class RSAPadding {
     Cipher.getInstance("RSA/NONE/NoPadding");
   }
 
-  public void rsaNoPadding() {
+  public void rsaNoPadding2() {
+    // ruleid: rsa-no-padding
+    useCipher(Cipher.getInstance("RSA/None/NoPadding"));
+  }
+
+  public void rsaPadding() {
     // ok: rsa-no-padding
     Cipher.getInstance("RSA/ECB/OAEPWithMD5AndMGF1Padding");
   }
