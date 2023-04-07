52796eeaa98d66f816b70209daabfa2011bc794c
Vasilii <inkz@xakep.ru>
2023-04-07 16:41:57 +0900

    update raw-html-format rule

--- javascript/express/security/injection/raw-html-format.js
+++ javascript/express/security/injection/raw-html-format.js
@@ -26,6 +26,14 @@ app.post('/test4', async (req, res) => {
     res.send(html);
 })
 
+app.post('/ok-test', async (req, res) => {
+    let { foobar } = req.query
+    let sanitizedParam = sanitizeUrl(foobar)
+    const url = `${baseUrl}/foo/bar?yo=123&param=${sanitizedParam}`
+    // ok: raw-html-format
+    return res.send(`<a href="${url}" />`)
+})
+
 app.get('/ok', async (req, res) => {
     // ok: raw-html-format
     res.send("message: " + req.query.message);
