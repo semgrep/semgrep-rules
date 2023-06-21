e7e58e4d6cf32716da99e5ec9f0281b3b29ab256
Phil Turnbull <phil@semgrep.com>
2023-06-21 13:10:02 -0400

    Deprecate timing-attack rule

--- ruby/lang/security/timing-attack.rb
+++ ruby/lang/security/timing-attack.rb
@@ -1,6 +1,4 @@
 class timing_attack
-  # ruleid: timing-attack
   http_basic_authenticate_with name: "Chris", password: "LimpBizkitRules420"
-  # ruleid: timing-attack
   http_basic_authenticate_with :name => ENV["NAME"], :password => ENV["PASSWORD"]
 end
