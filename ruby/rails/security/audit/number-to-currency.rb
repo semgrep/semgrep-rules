0f3b718bacca2173e244e8f811e95ed5dc62259d
Phil Turnbull <phil@semgrep.com>
2023-06-21 13:14:12 -0400

    Deprecate number-to-currency rule

--- ruby/rails/security/audit/number-to-currency.rb
+++ ruby/rails/security/audit/number-to-currency.rb
@@ -1,17 +1,14 @@
 def bad1()
-  # ruleid: number-to-currency
   number_to_currency(1.02, unit: params[:currency])
 end
 
 def bad2()
   currency = params[:currency]
   currency = currency + "unit"
-  # ruleid: number-to-currency
   number_to_currency(1.02, unit: currency)
 end
 
 def ok1()
-  # ok: number-to-currency
   number_to_currency(1.03, unit: h(params[:currency]))
 end
 
