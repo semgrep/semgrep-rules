35c2320a3df40dea456f2bf1b76532c38bf105da
Jacob Brackett <jacob.brackett@merge.dev>
2023-07-20 17:30:23 +0000

    add curl-pipe-bash.sh

---
+++ curl-pipe-bash.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# ruleid: curl-pipe-bash
+bash <(curl -Ls "https://raw.githubusercontent.com/pusox/pusox/main/script/_A.sh")
+
+# ruleid: curl-pipe-bash
+curl http://10.110.1.200/deployment/scripts/SLAVE00-flight-setup.bash | /bin/bash -x | tee -a /tmp/mainscript-default-output
+
+# ruleid: curl-pipe-bash
+curl http://10.110.1.200/deployment/scripts/SLAVE00-flight-setup.bash | sudo /bin/bash
+
+# ruleid: curl-pipe-bash
+sudo bash <(curl -Ls "https://raw.githubusercontent.com/pusox/pusox/main/script/_A.sh")
+
+# ok: curl-pipe-bash
+curl http://10.110.1.200/deployment/scripts/SLAVE00-flight-setup.bash | tee -a /tmp/mainscript-default-output
+
