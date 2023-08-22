77a037a7d41f78802f8dc319e1fb7d4f97ec03c2
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2023-08-22 09:26:09 +0700

    Split rule to separate files
    Signed-off-by: Thanabodee Charoenpiriyakij <wingyminus@gmail.com>

--- elixir/lang/best-practice/enum-map-join.exs
+++ elixir/lang/best-practice/enum-map-join.exs
@@ -1,15 +1,3 @@
-# ruleid: enum_map_into
-Enum.into(Enum.map(%{a: 1, b: 2}, fn {k, v} -> {k, v * 2} end), %{})
-
-# ruleid: enum_map_into
-Enum.map(%{a: 1, b: 2}, fn {k, v} -> {k, v * 2} end)
-|> Enum.into(%{})
-
-# ruleid: enum_map_into
-%{a: 1, b: 2}
-|> Enum.map(fn {k, v} -> {k, v * 2} end)
-|> Enum.into(%{})
-
 # ruleid: enum_map_join
 Enum.join(Enum.map(["a", "b", "c"], fn s -> String.upcase(s) end), ", ")
 
