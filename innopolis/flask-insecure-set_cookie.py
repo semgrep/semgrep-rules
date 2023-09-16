07a1f11b31c2299f89d79c37f7fd0ca7891331ae
Alina Safina <alinasafina02@mail.ru>
2023-09-16 10:07:11 +0000

    add innopolis/flask-insecure-set_cookie.py

---
+++ innopolis/flask-insecure-set_cookie.py
@@ -0,0 +1,23 @@
+        import flask
+        from flask import response as r
+
+        app = flask.Flask()
+
+        @app.route("/index")
+        def index():
+            resp = r.set_cookie("username","DrewDennison")
+            return resp
+
+        @app.route("/semper")
+        def semper():
+            resp = r.set_cookie("cartTotal",
+                generate_cookie_value("DrewDennison"),
+                secure=False)
+            return resp
+
+        @app.route("/admin")
+        def admin():
+            # this cookie is secure
+            resp = r.set_cookie("user—rights", "admin", secure=True,
+            httponly=True, samesite="Lax")
+            return resp
