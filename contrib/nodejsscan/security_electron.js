c4fc7c34018a1464eacac9994820eb6db5c8253f
Prajyot Chemburkar <pachinko.pachinko2@gmail.com>
2023-12-15 02:46:28 -0500

    Add rule for electron - dynamic browserview loadurl

--- contrib/nodejsscan/security_electron.js
+++ contrib/nodejsscan/security_electron.js
@@ -97,3 +97,15 @@ rendition = book.renderTo('epub-viewer', {
     allowScriptedContent: true,
     allowPopups: true
 } as any);
+
+// ruleid: electron_dynamic_loadURL
+const { app, BrowserView, BrowserWindow } = require('electron')
+
+app.whenReady().then(() => {
+  const win = new BrowserWindow({ width: 800, height: 600 })
+
+  const view = new BrowserView()
+  win.setBrowserView(view)
+  view.setBounds({ x: 0, y: 0, width: 300, height: 300 })
+  view.webContents.loadURL(someurl)
+});
