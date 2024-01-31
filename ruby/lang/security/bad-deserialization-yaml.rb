d4f6f8fa49fd1a0ece13382f2f15615f5799193f
navhits <naveen2aug@gmail.com>
2024-01-31 15:34:36 +0530

    Add more negative patterns to exclude cases of reading from a file

--- ruby/lang/security/bad-deserialization-yaml.rb
+++ ruby/lang/security/bad-deserialization-yaml.rb
@@ -4,7 +4,6 @@
     data = YAML.dump(o)
     # ruleid: bad-deserialization-yaml
     obj = YAML.load(data)
-
  end
 
  def ok_deserialization
@@ -20,4 +19,25 @@
 
     # ok: bad-deserialization-yaml
     YAML.load(File.read("test.txt"))
+
+    # ok: bad-deserialization-yaml
+    obj = YAML::load(ERB.new(File.read("test.yml")).result)
+    
+    # ok: bad-deserialization-yaml
+    obj = YAML::load(ERB.new(File.read("test.yml")))
+
+    template = ERB.new(File.read("test.yml"))
+    # ok: bad-deserialization-yaml
+    obj = YAML::load(template)
+
+    template = ERB.new(File.read("test.yml")).result
+    # ok: bad-deserialization-yaml
+    obj = YAML::load(template)
+
+    template = ERB.new(File.read("test.yml"))
+    # ok: bad-deserialization-yaml
+    obj = YAML::load(template.result)
+
+    # ok: bad-deserialization-yaml
+    obj = YAML.load(File.read(File.join(Pathname.pwd, "hello.yml")))
  end
