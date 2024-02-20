ebdcc934aafbc9902a2aea5221133e420a3ef100
Vasilii <inkz@xakep.ru>
2024-02-20 13:44:39 +0700

    update object-deserialization rule

--- java/lang/security/audit/object-deserialization.java
+++ java/lang/security/audit/object-deserialization.java
@@ -4,20 +4,30 @@ import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.IOException;
 import java.lang.ClassNotFoundException;
+import org.apache.commons.io.input.ClassLoaderObjectInputStream;
 
 import com.biz.org.UserData;
 
 public class Cls
 {
-    public UserData deserializeObject(InputStream receivedFile) throws IOException, ClassNotFoundException {
+    public UserData deserializeObject1(InputStream receivedFile) throws IOException, ClassNotFoundException {
         // ruleid:object-deserialization
         ObjectInputStream in = new ObjectInputStream(receivedFile);
         return (UserData) in.readObject();
     }
 
-    public UserData deserializeObject(InputStream receivedFile) throws IOException, ClassNotFoundException {
+    public UserData deserializeObject2(InputStream receivedFile) throws IOException, ClassNotFoundException {
         // ruleid:object-deserialization 
         try (ObjectInputStream in = new ObjectInputStream(receivedFile)) {
+            return (UserData) in.readObject();
+        } catch (IOException e) {
+            throw e;
+        }
+    }
+
+    public UserData deserializeObject3(InputStream receivedFile) throws IOException, ClassNotFoundException {
+        // ruleid:object-deserialization 
+        try (ObjectInputStream in = new ClassLoaderObjectInputStream(Thread.currentThread().getContextClassLoader(), receivedFile)) {
             return (UserData) in.readObject();
         } catch (IOException e) {
             throw e;
