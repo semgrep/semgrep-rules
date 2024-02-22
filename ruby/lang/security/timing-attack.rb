643446c519b6cd254966228f37a688317205405f
LewisArdern <LewisArdern@live.co.uk>
2024-02-22 12:05:12 -0800

    Remove deprecated rules

--- ruby/lang/security/timing-attack.rb
+++
@@ -1,4 +0,0 @@
-class Timing_attack
-  http_basic_authenticate_with name: "Chris", password: "LimpBizkitRules420"
-  http_basic_authenticate_with :name => ENV["NAME"], :password => ENV["PASSWORD"]
-end
--- ruby/rails/security/audit/mime-type-dos.rb
+++
@@ -1,9 +0,0 @@
-def bad(string, symbol, mime_type_synonyms = [], extension_synonyms = [], skip_lookup = false)
-  Mime.const_set(symbol.to_s.upcase, Type.new(string, symbol, mime_type_synonyms))
-end
-
-def ok()
-  Mime.const_set :LOOKUP, Hash.new { |h,k|
-    Mime::Type.new(k) unless k.blank?
-  }
-end
