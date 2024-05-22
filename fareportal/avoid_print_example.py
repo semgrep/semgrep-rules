612efb3ba3ba399465ad0ab52a68d4226b2898f0
Aman <Aman.Kumar@fareportal.com>
2024-05-22 10:45:05 +0000

    add fareportal/avoid_print_example.py

---
+++ fareportal/avoid_print_example.py
@@ -0,0 +1,15 @@
+print("Welcome to Semgrep!" + "Use our Run button to start experimenting -->")
+
+
+print("Example")
+
+# To detect ALL calls to the print() function, change the Semgrep Rule from print("...") to print(...)
+print("Example")
+print(not_a_string)
+
+print(first_var, second_var)
+print("Example")
+print()
+
+# print("This is commented out so it will never be found")
+print("Example")
