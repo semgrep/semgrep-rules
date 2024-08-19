5ba4d9efbc0f9daa808e4714fd9ab4b8e51e5a9a
Yosef Alsuhaibani <yosefalsuhaibani@Yosefs-MacBook-Pro-2.local>
2024-08-19 19:26:00 -0400

    not deep only but alos floss

--- java/spring/security/injection/tainted-system-command.java
+++ java/spring/security/injection/tainted-system-command.java
@@ -41,13 +41,13 @@ public class CommandInjection {
         if (isValid) {
             Process process;
             if (!isWindows) {
-                // deepruleid: tainted-system-command
+                // ruleid: tainted-system-command
                 process =
                         new ProcessBuilder(new String[] {"sh", "-c", "ping -c 2 " + ipAddress})
                                 .redirectErrorStream(true)
                                 .start();
             } else {
-               	// deepruleid: tainted-system-command
+               	// ruleid: tainted-system-command
                 process =
                         new ProcessBuilder(new String[] {"cmd", "/c", "ping -n 2 " + ipAddress})
                                 .redirectErrorStream(true)
