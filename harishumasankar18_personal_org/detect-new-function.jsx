c019a4ba591c0c9aa7507053328538048aceb487
Harish Umasankar <harishumasankar18@gmail.com>
2024-11-27 08:39:34 +0000

    add harishumasankar18_personal_org/detect-new-function.jsx

---
+++ harishumasankar18_personal_org/detect-new-function.jsx
@@ -0,0 +1,21 @@
+
+const calculate = (activity, health, weight, happiness) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            // devine formula :100:
+            let res = `with(a='${activity}', hp=${health}, w=${weight}, hs=${happiness}) {
+                if (a == 'feed') { hp += 1; w += 5; hs += 3; } if (a == 'play') { w -= 5; hp += 2; hs += 3; } if (a == 'sleep') { hp += 2; w += 3; hs += 3; } if ((a == 'feed' || a == 'sleep' ) && w > 70) { hp -= 10; hs -= 10; } else if ((a == 'feed' || a == 'sleep' ) && w < 40) { hp += 10; hs += 5; } else if (a == 'play' && w < 40) { hp -= 10; hs -= 10; } else if ( hs > 70 && (hp < 40 || w < 30)) { hs -= 10; }  if ( hs > 70 ) { m = 'kissy' } else if ( hs < 40 ) { m = 'cry' } else { m = 'awkward'; } if ( hs > 100) { hs = 100; } if ( hs < 5) { hs = 5; } if ( hp < 5) { hp = 5; } if ( hp > 100) { hp = 100; }  if (w < 10) { w = 10 } return {m, hp, w, hs}
+                }`;
+            quickMaths = new Function(res);
+            const {m, hp, w, hs} = quickMaths();
+            resolve({mood: m, health: hp, weight: w, happiness: hs})
+        }
+        catch (e) {
+            reject(e);
+        }
+    });
+}
+
+module.exports = {
+    calculate
+}
