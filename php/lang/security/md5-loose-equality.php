951d9625930fe4a5889009b74a2825ac430b355a
Sjoerd Langkemper <sjoerd-github@linuxonly.nl>
2025-03-01 20:43:48 +0100

    Generically check for loose equal check on hexadecimal strings
    Instead of only checking for comparisons containing `md5` or `md5_file`, make a
    taint mode rule that checks comparisons for all hexadecimal strings.

--- php/lang/security/md5-loose-equality.php
+++ php/lang/security/md5-loose-equality.php
@@ -14,3 +14,12 @@ md5("240610708") == md5_file("file.txt");
 
 // ok: md5-loose-equality
 md5("240610708") === "0";
+
+$hash = hash('sha256', 'hello');
+$something = "32";
+
+// ok: md5-loose-equality
+strlen($hash) == $something;
+
+// ruleid: md5-loose-equality
+$hash == $something;
