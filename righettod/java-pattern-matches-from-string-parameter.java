1cf77635b989c218e7cc5117c5660837dcf7e850
Dominique RIGHETTO <dominique.righetto@gmail.com>
2025-07-03 07:33:18 +0000

    add righettod/java-pattern-matches-from-string-parameter.java

---
+++ righettod/java-pattern-matches-from-string-parameter.java
@@ -0,0 +1,46 @@
+package com.test;
+
+import java.util.regex.Pattern;
+
+public class Cases {
+
+    public void case1(int v, String regex, long x) {
+        String data = "a".repeat(50) + "!";
+        //ruleid: java-pattern-matches-from-string-parameter
+        Pattern.matches(regex,data);
+        //ok: java-pattern-matches-from-string-parameter
+        Pattern.compile(regex);
+        //ok: java-pattern-matches-from-string-parameter
+        data.matches("[a-z]+");
+    }
+
+    public void case2(String regex, long x) {
+        String data = "a".repeat(50) + "!";
+        //ruleid: java-pattern-matches-from-string-parameter
+        Pattern.matches(regex,data);
+        //ok: java-pattern-matches-from-string-parameter
+        Pattern.compile(regex);
+        //ok: java-pattern-matches-from-string-parameter
+        data.matches("[a-z]+");
+    }
+
+    public void case3(int v, String regex) {
+        String data = "a".repeat(50) + "!";
+        //ruleid: java-pattern-matches-from-string-parameter
+        Pattern.matches(regex,data);
+        //ok: java-pattern-matches-from-string-parameter
+        Pattern.compile(regex);
+        //ok: java-pattern-matches-from-string-parameter
+        data.matches("[a-z]+");
+    } 
+
+    public void case4(String regex) {
+        String data = "a".repeat(50) + "!";
+        //ruleid: java-pattern-matches-from-string-parameter
+        Pattern.matches(regex,data);
+        //ok: java-pattern-matches-from-string-parameter
+        Pattern.compile(regex);
+        //ok: java-pattern-matches-from-string-parameter
+        data.matches("[a-z]+");
+    }         
+}
