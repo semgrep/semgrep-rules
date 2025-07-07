c4ce52c5c83d717bc63e996827cec62e899ae22e
Lee Yu-Shin <fjrvldi6@naver.com>
2025-07-07 05:36:15 +0000

    add fjrvldi6_naver_com/hardcoded-conditional.java

---
+++ fjrvldi6_naver_com/hardcoded-conditional.java
@@ -0,0 +1,22 @@
+public class Example {
+
+    public void run() {
+
+        while(true) {
+            // do something
+        }
+
+        for(;;) {
+            // do something else
+        }
+
+        for(int i=0;; i++) {
+            // do something else
+        }
+        
+        int a = 0;
+        while (a < 5) {
+            a++;
+        }
+    }
+}
