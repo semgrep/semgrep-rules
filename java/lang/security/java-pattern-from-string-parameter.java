00fca9005119becf5a0fac73f0655c424e17f5bc
Vasilii Ermilov <ermilov.work@gmail.com>
2025-07-25 13:52:45 -0400

    Update java-pattern-from-string-parameter.java

--- java/lang/security/java-pattern-from-string-parameter.java
+++ java/lang/security/java-pattern-from-string-parameter.java
@@ -6,49 +6,49 @@ public class Cases {
 
     public void case1(int v, String regex, long x) {
         String data = "a".repeat(50) + "!";
-        //ruleid: java-pattern-matches-from-string-parameter
+        //ruleid: java-pattern-from-string-parameter
         Pattern.matches(regex,data);
-        //ruleid: java-pattern-matches-from-string-parameter
+        //ruleid: java-pattern-from-string-parameter
         Pattern.compile(regex);
-        //ruleid: java-pattern-matches-from-string-parameter
+        //ruleid: java-pattern-from-string-parameter
         Pattern.compile(regex,Pattern.CASE_INSENSITIVE);        
-        //ok: java-pattern-matches-from-string-parameter
+        //ok: java-pattern-from-string-parameter
         data.matches("[a-z]+");
     }
 
     public void case2(String regex, long x) {
         String data = "a".repeat(50) + "!";
-        //ruleid: java-pattern-matches-from-string-parameter
+        //ruleid: java-pattern-from-string-parameter
         Pattern.matches(regex,data);
-        //ruleid: java-pattern-matches-from-string-parameter
+        //ruleid: java-pattern-from-string-parameter
         Pattern.compile(regex);
-        //ruleid: java-pattern-matches-from-string-parameter
+        //ruleid: java-pattern-from-string-parameter
         Pattern.compile(regex,Pattern.CASE_INSENSITIVE);        
-        //ok: java-pattern-matches-from-string-parameter
+        //ok: java-pattern-from-string-parameter
         data.matches("[a-z]+");
     }
 
     public void case3(int v, String regex) {
         String data = "a".repeat(50) + "!";
-        //ruleid: java-pattern-matches-from-string-parameter
+        //ruleid: java-pattern-from-string-parameter
         Pattern.matches(regex,data);
-        //ruleid: java-pattern-matches-from-string-parameter
+        //ruleid: java-pattern-from-string-parameter
         Pattern.compile(regex);
-        //ruleid: java-pattern-matches-from-string-parameter
+        //ruleid: java-pattern-from-string-parameter
         Pattern.compile(regex,Pattern.CASE_INSENSITIVE);        
-        //ok: java-pattern-matches-from-string-parameter
+        //ok: java-pattern-from-string-parameter
         data.matches("[a-z]+");
     } 
 
     public void case4(String regex) {
         String data = "a".repeat(50) + "!";
-        //ruleid: java-pattern-matches-from-string-parameter
+        //ruleid: java-pattern-from-string-parameter
         Pattern.matches(regex,data);
-        //ruleid: java-pattern-matches-from-string-parameter
+        //ruleid: java-pattern-from-string-parameter
         Pattern.compile(regex);
-        //ruleid: java-pattern-matches-from-string-parameter
+        //ruleid: java-pattern-from-string-parameter
         Pattern.compile(regex,Pattern.CASE_INSENSITIVE);        
-        //ok: java-pattern-matches-from-string-parameter
+        //ok: java-pattern-from-string-parameter
         data.matches("[a-z]+");
     }            
 }
