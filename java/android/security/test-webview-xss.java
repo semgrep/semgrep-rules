d5a268c6a506728c7be7be17ae8e1a6e29d0a2fc
Mdm <70381337+Mehrdoost@users.noreply.github.com>
2026-06-27 18:54:07 +0330

    Rename methods and add security checks for WebView
    Extend test cases for all WebView security rules

--- java/android/security/test-webview-xss.java
+++ java/android/security/test-webview-xss.java
@@ -2,12 +2,32 @@ import android.webkit.WebView;
 
 public class Test {
     // ruleid: android-webview-javascript-enabled-xss
-    public void vulnerableWebView(WebView view) {
+    public void vulnerableXss(WebView view) {
         view.getSettings().setJavaScriptEnabled(true);
     }
 
     // ok: android-webview-javascript-enabled-xss
-    public void safeWebView(WebView view) {
+    public void safeXss(WebView view) {
         view.getSettings().setJavaScriptEnabled(false);
+    }
+
+    // ruleid: android-webview-file-access
+    public void vulnerableFileAccess(WebView view) {
+        view.getSettings().setAllowFileAccess(true);
+    }
+
+    // ok: android-webview-file-access
+    public void safeFileAccess(WebView view) {
+        view.getSettings().setAllowFileAccess(false);
+    }
+
+    // ruleid: android-webview-universal-access
+    public void vulnerableUniversalAccess(WebView view) {
+        view.getSettings().setAllowUniversalAccessFromFileURLs(true);
+    }
+
+    // ok: android-webview-universal-access
+    public void safeUniversalAccess(WebView view) {
+        view.getSettings().setAllowUniversalAccessFromFileURLs(false);
     }
 }
